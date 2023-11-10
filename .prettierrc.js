1f18bad2739c008e9b60bd957982c4d154d94c1c
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-09 16:50:31 -0800

    chore: Setup monorepo

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    printWidth: 145,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    trailingComma: "es5",
+}
---
+++ packages/ua-utils/src/getDefaultConfig.ts
@@ -0,0 +1,34 @@
+import { ethers } from "ethers"
+import { getProvider, getLayerZeroChainId, getEndpointAddress } from "./utils/crossChainHelper"
+const { ENDPOINT_ABI, MESSAGING_LIBRARY_ABI } = require("./constants/abi")
+
+export default async (taskArgs: any, hre: any) => {
+    const networks = taskArgs.networks.split(",")
+
+    const configByNetwork = await Promise.all(
+        networks.map(async (network: string) => {
+            const provider = getProvider(hre, network)
+            console.log()
+            const endpoint = new ethers.Contract(getEndpointAddress(network), ENDPOINT_ABI, provider)
+            const sendVersion = await endpoint.defaultSendVersion()
+            const receiveVersion = await endpoint.defaultReceiveVersion()
+            const sendLibraryAddress = await endpoint.defaultSendLibrary()
+            const messagingLibrary = new ethers.Contract(sendLibraryAddress, MESSAGING_LIBRARY_ABI, provider)
+            const config = await messagingLibrary.defaultAppConfig(getLayerZeroChainId(network))
+
+            return {
+                network,
+                sendVersion,
+                receiveVersion,
+                inboundProofLibraryVersion: config.inboundProofLibraryVersion,
+                inboundBlockConfirmations: config.inboundBlockConfirmations.toNumber(),
+                relayer: config.relayer,
+                outboundProofType: config.outboundProofType,
+                outboundBlockConfirmations: config.outboundBlockConfirmations.toNumber(),
+                oracle: config.oracle,
+            }
+        })
+    )
+
+    console.table(configByNetwork)
+}
