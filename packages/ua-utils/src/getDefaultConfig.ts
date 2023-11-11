7a025caaaa221c3e04c77ef6202b9d10928952a1
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2023-11-10 18:35:18 -0800

    first pass updating getDefaultConfig

--- packages/ua-utils/src/getDefaultConfig.ts
+++ packages/ua-utils/src/getDefaultConfig.ts
@@ -1,34 +1,72 @@
+import { ActionType } from "hardhat/types"
+import { task, types } from "hardhat/config"
 import { ethers } from "ethers"
-import { getProvider, getLayerZeroChainId, getEndpointAddress } from "./utils/crossChainHelper"
-const { ENDPOINT_ABI, MESSAGING_LIBRARY_ABI } = require("./constants/abi")
+import { createGetNetworkEnvironment } from "@layerzerolabs/hardhat-utils"
 
-export default async (taskArgs: any, hre: any) => {
+const CONFIG_TYPE_MAX_MESSAGE_SIZE = 1
+const CONFIG_TYPE_OUTBOUND_CONFIRMATIONS = 2
+const CONFIG_TYPE_EXECUTOR = 3
+const CONFIG_TYPE_INBOUND_CONFIRMATIONS = 4
+const CONFIG_TYPE_VERIFIERS = 5
+const CONFIG_TYPE_OPTIONAL_VERIFIERS = 6
+
+const action: ActionType<any> = async (taskArgs, hre) => {
+    // TODO add logging
+    // const logger = createLogger()
+
     const networks = taskArgs.networks.split(",")
-
+    const getEnvironment = createGetNetworkEnvironment(hre)
     const configByNetwork = await Promise.all(
         networks.map(async (network: string) => {
-            const provider = getProvider(hre, network)
-            console.log()
-            const endpoint = new ethers.Contract(getEndpointAddress(network), ENDPOINT_ABI, provider)
-            const sendVersion = await endpoint.defaultSendVersion()
-            const receiveVersion = await endpoint.defaultReceiveVersion()
-            const sendLibraryAddress = await endpoint.defaultSendLibrary()
-            const messagingLibrary = new ethers.Contract(sendLibraryAddress, MESSAGING_LIBRARY_ABI, provider)
-            const config = await messagingLibrary.defaultAppConfig(getLayerZeroChainId(network))
+            const environment = await getEnvironment(network)
+            const endpointV2 = await environment.getContract("EndpointV2", environment.provider)
+            const eid = await endpointV2.eid()
+
+            const defaultSendLibrary = await endpointV2.defaultSendLibrary(eid)
+            const defaultReceiveLibrary = await endpointV2.defaultReceiveLibrary(eid)
+
+            const maxMessageSizeEncodedData = await endpointV2.defaultConfig(defaultSendLibrary, eid, CONFIG_TYPE_MAX_MESSAGE_SIZE)
+            const maxMessageSize = ethers.utils.defaultAbiCoder.decode(["uint32"], maxMessageSizeEncodedData)
+
+            const outboundConfirmationsEncodedData = await endpointV2.defaultConfig(defaultSendLibrary, eid, CONFIG_TYPE_OUTBOUND_CONFIRMATIONS)
+            const outboundConfirmations = ethers.utils.defaultAbiCoder.decode(["uint64"], outboundConfirmationsEncodedData)
+
+            const executorEncodedData = await endpointV2.defaultConfig(defaultSendLibrary, eid, CONFIG_TYPE_EXECUTOR)
+            const executor = ethers.utils.defaultAbiCoder.decode(["address"], executorEncodedData)
+
+            const inboundBlockConfirmationsEncodedData = await endpointV2.defaultConfig(
+                defaultReceiveLibrary,
+                eid,
+                CONFIG_TYPE_INBOUND_CONFIRMATIONS
+            )
+            const inboundBlockConfirmations = ethers.utils.defaultAbiCoder.decode(["uint64"], inboundBlockConfirmationsEncodedData)
+
+            const verifiersEncodedData = await endpointV2.defaultConfig(defaultReceiveLibrary, eid, CONFIG_TYPE_VERIFIERS)
+            const verifiers = ethers.utils.defaultAbiCoder.decode(["address[]"], verifiersEncodedData)
+
+            const optionalVerifierEncodedData = await endpointV2.defaultConfig(defaultReceiveLibrary, eid, CONFIG_TYPE_OPTIONAL_VERIFIERS)
+            const [optionalVerifiers, optionalVerifierThreshold] = await ethers.utils.defaultAbiCoder.decode(
+                ["uint8", "uint8"],
+                optionalVerifierEncodedData
+            )
 
             return {
                 network,
-                sendVersion,
-                receiveVersion,
-                inboundProofLibraryVersion: config.inboundProofLibraryVersion,
-                inboundBlockConfirmations: config.inboundBlockConfirmations.toNumber(),
-                relayer: config.relayer,
-                outboundProofType: config.outboundProofType,
-                outboundBlockConfirmations: config.outboundBlockConfirmations.toNumber(),
-                oracle: config.oracle,
+                defaultSendLibrary,
+                defaultReceiveLibrary,
+                maxMessageSize,
+                outboundConfirmations,
+                executor,
+                inboundBlockConfirmations,
+                verifiers,
+                optionalVerifiers,
+                optionalVerifierThreshold,
             }
         })
     )
-
     console.table(configByNetwork)
 }
+
+task("getDefaultConfig", "outputs the default Send and Receive Messaging Library versions and the default application config")
+    .addParam("networks", "comma separated list of networks")
+    .setAction(action)
