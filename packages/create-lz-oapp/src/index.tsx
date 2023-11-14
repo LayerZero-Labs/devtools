ded29d44173f3eefdaa534821e49a4b2c3e72dd4
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-14 09:20:37 -0800

    chore: create-oapp is too similar to an existing package, rename

--- packages/create-lz-oapp/src/index.tsx
+++ packages/create-lz-oapp/src/index.tsx
@@ -3,7 +3,7 @@ import { render } from "ink"
 import { Command } from "commander"
 import { Placeholder } from "./components/placeholder.js"
 
-new Command("create-oapp")
+new Command("create-lz-oapp")
     .description("Create LayerZero OApp with one command")
     .action(async () => {
         const { waitUntilExit } = render(<Placeholder />)
