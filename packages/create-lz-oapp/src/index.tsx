52f3c344307b01d52385ed24fd5ff3bd5398af97
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2023-11-14 14:13:25 -0800

    🚧 Prepare for create-lz-oapp publishing (#12)

---
+++ packages/create-lz-oapp/src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { render } from "ink"
+import { Command } from "commander"
+import { Placeholder } from "./components/placeholder.js"
+import { altScreen } from "./utilities/terminal.js"
+
+new Command("create-lz-oapp")
+    .description("Create LayerZero OApp with one command")
+    .action(async () => {
+        const exitAltScreen = await altScreen()
+
+        try {
+            const { waitUntilExit } = render(<Placeholder />)
+            await waitUntilExit()
+        } finally {
+            await exitAltScreen()
+        }
+    })
+    .parseAsync()
