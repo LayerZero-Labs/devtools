ed1f5f852da591882e03813eb6ea3455349b3c82
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-14 09:57:52 -0800

    chore: Full-screen LayerZero experience

--- packages/create-lz-oapp/src/index.tsx
+++ packages/create-lz-oapp/src/index.tsx
@@ -2,12 +2,18 @@ import React from "react"
 import { render } from "ink"
 import { Command } from "commander"
 import { Placeholder } from "./components/placeholder.js"
+import { altScreen } from "./utilities/terminal.js"
 
 new Command("create-lz-oapp")
     .description("Create LayerZero OApp with one command")
     .action(async () => {
-        const { waitUntilExit } = render(<Placeholder />)
+        const exitAltScreen = await altScreen()
 
-        await waitUntilExit()
+        try {
+            const { waitUntilExit } = render(<Placeholder />)
+            await waitUntilExit()
+        } finally {
+            await exitAltScreen()
+        }
     })
     .parseAsync()
