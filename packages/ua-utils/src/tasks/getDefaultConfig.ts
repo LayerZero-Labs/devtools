824ede6e633d590b39f345e331cf5847fccce167
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-16 15:55:01 -0800

    chore: Revert changes to ua-utils

--- packages/ua-utils/src/tasks/getDefaultConfig.ts
+++ packages/ua-utils/src/tasks/getDefaultConfig.ts
@@ -1,21 +1,34 @@
-import { ActionType } from "hardhat/types"
-import { task } from "hardhat/config"
-import { createGetNetworkEnvironment } from "@layerzerolabs/hardhat-utils"
-import { createGetDefaultConfig } from "@/utils/config"
+import { ethers } from "ethers"
+import { getProvider, getLayerZeroChainId, getEndpointAddress } from "@/utils/crossChainHelper"
+import { ENDPOINT_ABI, MESSAGING_LIBRARY_ABI } from "@/constants/abi"
 
-interface TaskArguments {
-    networks: string
-}
-
-const action: ActionType<TaskArguments> = async (taskArgs, hre) => {
+export default async (taskArgs: any, hre: any) => {
     const networks = taskArgs.networks.split(",")
-    const getEnvironment = createGetNetworkEnvironment(hre)
-    const getDefaultConfig = createGetDefaultConfig(getEnvironment)
-    const configByNetwork = await Promise.all(networks.map(getDefaultConfig))
+
+    const configByNetwork = await Promise.all(
+        networks.map(async (network: string) => {
+            const provider = getProvider(hre, network)
+            console.log()
+            const endpoint = new ethers.Contract(getEndpointAddress(network), ENDPOINT_ABI, provider)
+            const sendVersion = await endpoint.defaultSendVersion()
+            const receiveVersion = await endpoint.defaultReceiveVersion()
+            const sendLibraryAddress = await endpoint.defaultSendLibrary()
+            const messagingLibrary = new ethers.Contract(sendLibraryAddress, MESSAGING_LIBRARY_ABI, provider)
+            const config = await messagingLibrary.defaultAppConfig(getLayerZeroChainId(network))
+
+            return {
+                network,
+                sendVersion,
+                receiveVersion,
+                inboundProofLibraryVersion: config.inboundProofLibraryVersion,
+                inboundBlockConfirmations: config.inboundBlockConfirmations.toNumber(),
+                relayer: config.relayer,
+                outboundProofType: config.outboundProofType,
+                outboundBlockConfirmations: config.outboundBlockConfirmations.toNumber(),
+                oracle: config.oracle,
+            }
+        })
+    )
 
     console.table(configByNetwork)
 }
-
-task("getDefaultConfig", "Outputs the default Send and Receive Messaging Library versions and the default application config")
-    .addParam("networks", "comma separated list of networks")
-    .setAction(action)
--- packages/ua-utils/tsup.config.ts
+++ packages/ua-utils/tsup.config.ts
@@ -1,24 +1,12 @@
 import { defineConfig } from "tsup"
 
-export default defineConfig([
-    {
-        entry: ["src/tasks/index.ts"],
-        outDir: "./dist/tasks",
-        clean: true,
-        dts: true,
-        sourcemap: true,
-        splitting: false,
-        treeshake: true,
-        format: ["esm", "cjs"],
-    },
-    {
-        entry: ["src/index.ts"],
-        outDir: "./dist",
-        clean: true,
-        dts: true,
-        sourcemap: true,
-        splitting: false,
-        treeshake: true,
-        format: ["esm", "cjs"],
-    },
-])
+export default defineConfig({
+    entry: ["src/tasks/index.ts"],
+    outDir: "./dist/tasks",
+    clean: true,
+    dts: true,
+    sourcemap: true,
+    splitting: false,
+    treeshake: true,
+    format: ["esm", "cjs"],
+})
