e35cb87b2a986c064615eaf0cde5371793bf2a42
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2023-11-17 15:02:12 -0800

    🎁 Deploy UltraLightNode302 (#26)

--- packages/ua-utils-test-v2/deploy/001_bootstrap.ts
+++ packages/ua-utils-test-v2/deploy/001_bootstrap.ts
@@ -11,13 +11,20 @@ const deploy: DeployFunction = async ({ getUnnamedAccounts, deployments, network
     assert(network.config.endpointId != null, `Missing endpoint ID for network ${network.name}`)
 
     const [deployer] = await getUnnamedAccounts()
+
     const endpointV2Deployment = await deployments.deploy("EndpointV2", {
         from: deployer,
         args: [network.config.endpointId, AddressZero],
     })
 
+    const uln302Deployment = await deployments.deploy("UltraLightNode302", {
+        from: deployer,
+        args: [endpointV2Deployment.address, 0],
+    })
+
     console.table({
         EndpointV2: endpointV2Deployment.address,
+        UltraLightNode302: uln302Deployment.address,
     })
 }
 
