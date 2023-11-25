b0ae08fe190c1016a02c0a0e40f794ba40a08dcb
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2023-11-24 17:00:02 -0800

    🪚 Add type-extensions export to utils-evm-hardhat (#42)

--- packages/utils-evm-hardhat/tsup.config.ts
+++ packages/utils-evm-hardhat/tsup.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-    entry: ['src/index.ts'],
-    outDir: './dist',
-    clean: true,
-    dts: true,
-    sourcemap: true,
-    splitting: false,
-    treeshake: true,
-    format: ['esm', 'cjs'],
-})
+export default defineConfig([
+    {
+        entry: ['src/index.ts'],
+        outDir: './dist',
+        clean: true,
+        dts: true,
+        sourcemap: true,
+        splitting: false,
+        treeshake: true,
+        format: ['esm', 'cjs'],
+    },
+    {
+        entry: ['src/type-extensions.ts'],
+        outDir: './dist',
+        clean: true,
+        dts: true,
+        sourcemap: true,
+        splitting: false,
+        treeshake: true,
+        format: ['esm', 'cjs'],
+    },
+])
