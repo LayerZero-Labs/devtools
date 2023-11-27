a6f3991f2afd516a2afbabfb8bf74622f68af112
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-27 10:57:48 -0800

    chore: Move EndpointIdBasedfactory up a directory level

--- packages/utils-evm/src/provider/types.ts
+++ packages/utils-evm/src/provider/types.ts
@@ -1,9 +1,7 @@
+import type { EndpointBasedFactory } from '@/types'
 import type { BaseProvider } from '@ethersproject/providers'
-import type { EndpointId } from '@layerzerolabs/lz-definitions'
 
 export type Provider = BaseProvider
-
-export type EndpointBasedFactory<TValue> = (eid: EndpointId) => TValue | Promise<TValue>
 
 export type RpcUrlFactory = EndpointBasedFactory<string>
 
