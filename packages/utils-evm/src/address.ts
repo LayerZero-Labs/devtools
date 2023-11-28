940bd3aad3a7cf0fb1c0b1850778c57c0f0815d4
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-28 13:55:39 -0800

    chore: Add makeZero utility

--- packages/utils-evm/src/address.ts
+++ packages/utils-evm/src/address.ts
@@ -3,3 +3,5 @@ import type { Address } from '@layerzerolabs/ua-utils'
 
 export const ignoreZero = (address: Address | null | undefined): Address | undefined =>
     address === AddressZero ? undefined : address ?? undefined
+
+export const makeZero = (address: Address | null | undefined): Address => address ?? AddressZero
--- packages/utils-evm/test/address.test.ts
+++ packages/utils-evm/test/address.test.ts
@@ -1,7 +1,7 @@
 import fc from 'fast-check'
 import { AddressZero } from '@ethersproject/constants'
 import { evmAddressArbitrary } from '@layerzerolabs/test-utils'
-import { ignoreZero } from '@/address'
+import { ignoreZero, makeZero } from '@/address'
 
 describe('address', () => {
     describe('ignoreZero', () => {
@@ -25,6 +25,30 @@ describe('address', () => {
 
         it('should return undefined with null', () => {
             expect(ignoreZero(null)).toBe(undefined)
+        })
+    })
+
+    describe('makeZero', () => {
+        it('should return address with non-zero address', () => {
+            fc.assert(
+                fc.property(evmAddressArbitrary, (address) => {
+                    fc.pre(address !== AddressZero)
+
+                    expect(makeZero(address)).toBe(address)
+                })
+            )
+        })
+
+        it('should return undefined with zero address', () => {
+            expect(makeZero(AddressZero)).toBe(AddressZero)
+        })
+
+        it('should return undefined with undefined', () => {
+            expect(makeZero(undefined)).toBe(AddressZero)
+        })
+
+        it('should return undefined with null', () => {
+            expect(makeZero(null)).toBe(AddressZero)
         })
     })
 })
