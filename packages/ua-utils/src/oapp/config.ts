0fb644e4892954410f8e6058b25ff0fc55d29114
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-29 13:50:40 -0800

    chore: Add configureOApp utility to ua-utils

---
+++ packages/ua-utils/src/oapp/config.ts
@@ -0,0 +1,21 @@
+import type { OmniGraph, OmniPoint, OmniTransaction } from '@/omnigraph/types'
+import type { IOApp } from './types'
+
+export type OAppFactory = (point: OmniPoint) => IOApp | Promise<IOApp>
+
+export const configureOApp = async (
+    graph: OmniGraph<unknown, unknown>,
+    factory: OAppFactory
+): Promise<OmniTransaction[]> => {
+    const setPeers = await Promise.all(
+        graph.connections.map(async ({ vector: { from, to } }): Promise<OmniTransaction[]> => {
+            const instance = await factory(from)
+            const address = await instance.peers(to.eid)
+
+            if (to.address === address) return []
+            return [await instance.setPeer(to.eid, to.address)]
+        })
+    )
+
+    return [...setPeers].flat()
+}
