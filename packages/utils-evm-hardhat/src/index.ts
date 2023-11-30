7289ab51556ba1e61737f74d0cf4549d507cb796
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-29 16:30:00 -0800

    chore: Move deployment related things to ua-utils-evm-hardhat

--- packages/utils-evm-hardhat/src/index.ts
+++ packages/utils-evm-hardhat/src/index.ts
@@ -3,8 +3,8 @@ import './type-extensions'
 
 // Regular exports
 export * from './config'
+export * from './internal'
 export * from './logger'
-export * from './omnigraph'
 export * from './provider'
 export * from './runtime'
 export * from './signer'
