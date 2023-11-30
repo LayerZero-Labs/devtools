f7b729bca63e7d2ecb16c35325c8e957926d5250
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-11-30 10:12:12 -0800

    chore: Add OmniVector formatter

--- packages/ua-utils/src/omnigraph/format.ts
+++ packages/ua-utils/src/omnigraph/format.ts
@@ -1,6 +1,9 @@
 import { EndpointId } from '@layerzerolabs/lz-definitions'
-import type { OmniPoint } from './types'
+import type { OmniPoint, OmniVector } from './types'
 
 export const formatEid = (eid: EndpointId): string => EndpointId[eid] ?? `Unknown EndpointId (${eid})`
 
-export const formatOmniPoint = ({ eid, address }: OmniPoint): string => `${address} @ ${formatEid(eid)}`
+export const formatOmniPoint = ({ eid, address }: OmniPoint): string => `[${address} @ ${formatEid(eid)}]`
+
+export const formatOmniVector = ({ from, to }: OmniVector): string =>
+    `${formatOmniPoint(from)} → ${formatOmniPoint(to)}`
--- packages/ua-utils/test/omnigraph/format.test.ts
+++ packages/ua-utils/test/omnigraph/format.test.ts
@@ -1,9 +1,19 @@
-import { formatEid, formatOmniPoint } from '@/omnigraph/format'
+import { formatEid, formatOmniPoint, formatOmniVector } from '@/omnigraph/format'
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 import { ENDPOINT_IDS, addressArbitrary, endpointArbitrary } from '@layerzerolabs/test-utils'
 import fc from 'fast-check'
 
 describe('omnigraph/format', () => {
+    const omniPointArbitrary = fc.record({
+        eid: endpointArbitrary,
+        address: addressArbitrary,
+    })
+
+    const omniVectorArbitrary = fc.record({
+        from: omniPointArbitrary,
+        to: omniPointArbitrary,
+    })
+
     describe('formatEid', () => {
         it.each(ENDPOINT_IDS)(`should format %d correctly`, (eid) => {
             expect(formatEid(eid)).toMatchSnapshot()
@@ -22,8 +32,20 @@ describe('omnigraph/format', () => {
     describe('formatOmniPoint', () => {
         it('should just work innit', () => {
             fc.assert(
-                fc.property(addressArbitrary, endpointArbitrary, (address, eid) => {
-                    expect(formatOmniPoint({ eid, address })).toBe(`${address} @ ${formatEid(eid)}`)
+                fc.property(omniPointArbitrary, (point) => {
+                    expect(formatOmniPoint(point)).toBe(`[${point.address} @ ${formatEid(point.eid)}]`)
+                })
+            )
+        })
+    })
+
+    describe('formatOmniVector', () => {
+        it('should just work innit', () => {
+            fc.assert(
+                fc.property(omniVectorArbitrary, (vector) => {
+                    expect(formatOmniVector(vector)).toBe(
+                        `${formatOmniPoint(vector.from)} → ${formatOmniPoint(vector.to)}`
+                    )
                 })
             )
         })
