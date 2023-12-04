5172160e2cdb5df66f6b011fec9ae804f859139e
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-12-04 10:53:13 -0800

    chore: Create test-evm-node package

---
+++ packages/test-evm-node/hardhat.config.ts
@@ -0,0 +1,22 @@
+import assert from 'assert'
+import { HardhatUserConfig } from 'hardhat/types'
+
+// test test test test test test test test test test test test
+const MNEMONIC = process.env.MNEMONIC
+assert(MNEMONIC, `Missing MNEMONIC environment variable`)
+
+/**
+ * This is a dummy hardhat config that enables us to test
+ * hardhat functionality without mocking too much
+ */
+const config: HardhatUserConfig = {
+    networks: {
+        hardhat: {
+            accounts: {
+                mnemonic: MNEMONIC,
+            },
+        },
+    },
+}
+
+export default config
