1c95d8833d05cf65c7205d54e5503f0424947210
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2023-12-05 14:24:04 -0800

    🪚 OmniGraph™ Repeatable setup for Endpoint & Uln302 deployment & configuration (#62)

---
+++ packages/protocol-utils-evm/src/endpoint/sdk.ts
@@ -0,0 +1,62 @@
+import type { IEndpoint } from '@layerzerolabs/protocol-utils'
+import { formatEid, type Address, type OmniTransaction } from '@layerzerolabs/utils'
+import type { EndpointId } from '@layerzerolabs/lz-definitions'
+import { ignoreZero, makeZero, omniContractToPoint, type OmniContract } from '@layerzerolabs/utils-evm'
+
+export class Endpoint implements IEndpoint {
+    constructor(public readonly contract: OmniContract) {}
+
+    async defaultReceiveLibrary(eid: EndpointId): Promise<string | undefined> {
+        return ignoreZero(await this.contract.contract.defaultReceiveLibrary(eid))
+    }
+
+    async setDefaultReceiveLibrary(
+        eid: EndpointId,
+        lib: string | null | undefined,
+        gracePeriod: number = 0
+    ): Promise<OmniTransaction> {
+        const data = this.contract.contract.interface.encodeFunctionData('setDefaultReceiveLibrary', [
+            eid,
+            makeZero(lib),
+            gracePeriod,
+        ])
+
+        return {
+            ...this.createTransaction(data),
+            description: `Setting default receive library for ${formatEid(eid)} to ${makeZero(lib)}`,
+        }
+    }
+
+    async defaultSendLibrary(eid: EndpointId): Promise<string | undefined> {
+        return ignoreZero(await this.contract.contract.defaultSendLibrary(eid))
+    }
+
+    async setDefaultSendLibrary(eid: EndpointId, lib: Address | null | undefined): Promise<OmniTransaction> {
+        const data = this.contract.contract.interface.encodeFunctionData('setDefaultSendLibrary', [eid, makeZero(lib)])
+
+        return {
+            ...this.createTransaction(data),
+            description: `Setting default send library for ${formatEid(eid)} to ${lib}`,
+        }
+    }
+
+    isRegisteredLibrary(lib: Address): Promise<boolean> {
+        return this.contract.contract.isRegisteredLibrary(lib)
+    }
+
+    async registerLibrary(lib: string): Promise<OmniTransaction> {
+        const data = this.contract.contract.interface.encodeFunctionData('registerLibrary', [lib])
+
+        return {
+            ...this.createTransaction(data),
+            description: `Registering library ${lib}`,
+        }
+    }
+
+    protected createTransaction(data: string): OmniTransaction {
+        return {
+            point: omniContractToPoint(this.contract),
+            data,
+        }
+    }
+}
---
+++ packages/ua-utils-evm-hardhat-test/test/endpoint/config.test.ts
@@ -0,0 +1,76 @@
+import { createConnectedContractFactory } from '@layerzerolabs/utils-evm-hardhat'
+import type { OmniPoint } from '@layerzerolabs/utils'
+import { omniContractToPoint } from '@layerzerolabs/utils-evm'
+import { expect } from 'chai'
+import { describe } from 'mocha'
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { defaultUlnConfig, setupDefaultEndpoint } from '../__utils__/endpoint'
+import { Endpoint, Uln302 } from '@layerzerolabs/protocol-utils-evm'
+
+describe('endpoint/config', () => {
+    const ethEndpoint = { eid: EndpointId.ETHEREUM_MAINNET, contractName: 'EndpointV2' }
+    const ethReceiveUln = { eid: EndpointId.ETHEREUM_MAINNET, contractName: 'ReceiveUln302' }
+    const ethSendUln = { eid: EndpointId.ETHEREUM_MAINNET, contractName: 'SendUln302' }
+    const avaxEndpoint = { eid: EndpointId.AVALANCHE_MAINNET, contractName: 'EndpointV2' }
+    const avaxReceiveUln = { eid: EndpointId.AVALANCHE_MAINNET, contractName: 'ReceiveUln302' }
+    const avaxSendUln = { eid: EndpointId.AVALANCHE_MAINNET, contractName: 'SendUln302' }
+
+    beforeEach(async () => {
+        await setupDefaultEndpoint()
+    })
+
+    describe('endpoint', () => {
+        it('should have default libraries configured', async () => {
+            // This is the required tooling we need to set up
+            const connectedContractFactory = createConnectedContractFactory()
+            const sdkFactory = async (point: OmniPoint) => new Endpoint(await connectedContractFactory(point))
+
+            // Now for the purposes of the test, we need to get coordinates of our contracts
+            const ethEndpointPoint = omniContractToPoint(await connectedContractFactory(ethEndpoint))
+            const avaxEndpointPoint = omniContractToPoint(await connectedContractFactory(avaxEndpoint))
+
+            const ethEndpointSdk = await sdkFactory(ethEndpointPoint)
+            const avaxEndpointSdk = await sdkFactory(avaxEndpointPoint)
+
+            // First let's check the send libraries
+            const ethDefaultSendLib = await ethEndpointSdk.defaultSendLibrary(avaxEndpointPoint.eid)
+            const avaxDefaultSendLib = await avaxEndpointSdk.defaultSendLibrary(ethEndpointPoint.eid)
+
+            const ethSendUlnPoint = omniContractToPoint(await connectedContractFactory(ethSendUln))
+            const avaxSendUlnPoint = omniContractToPoint(await connectedContractFactory(avaxSendUln))
+
+            expect(ethDefaultSendLib).to.equal(ethSendUlnPoint.address)
+            expect(avaxDefaultSendLib).to.equal(avaxSendUlnPoint.address)
+
+            // Then let's check the receive libraries
+            const ethDefaultReceiveLib = await ethEndpointSdk.defaultReceiveLibrary(avaxEndpointPoint.eid)
+            const avaxDefaultReceiveLib = await avaxEndpointSdk.defaultReceiveLibrary(ethEndpointPoint.eid)
+
+            const ethReceiveUlnPoint = omniContractToPoint(await connectedContractFactory(ethReceiveUln))
+            const avaxReceiveUlnPoint = omniContractToPoint(await connectedContractFactory(avaxReceiveUln))
+
+            expect(ethDefaultReceiveLib).to.equal(ethReceiveUlnPoint.address)
+            expect(avaxDefaultReceiveLib).to.equal(avaxReceiveUlnPoint.address)
+        })
+    })
+
+    describe('sendUln302', () => {
+        it('should have default executors configured', async () => {
+            // This is the required tooling we need to set up
+            const connectedContractFactory = createConnectedContractFactory()
+            const sdkFactory = async (point: OmniPoint) => new Uln302(await connectedContractFactory(point))
+
+            const ethSendUlnPoint = omniContractToPoint(await connectedContractFactory(ethSendUln))
+            const avaxSendUlnPoint = omniContractToPoint(await connectedContractFactory(avaxSendUln))
+
+            const ethSendUlnSdk = await sdkFactory(ethSendUlnPoint)
+            const avaxSendUlnSdk = await sdkFactory(avaxSendUlnPoint)
+
+            const ethConfig = await ethSendUlnSdk.getUlnConfig(avaxSendUlnPoint.eid, avaxSendUlnPoint.address)
+            const avaxConfig = await avaxSendUlnSdk.getUlnConfig(ethSendUlnPoint.eid, ethSendUlnPoint.address)
+
+            expect(ethConfig).to.eql(defaultUlnConfig)
+            expect(avaxConfig).to.eql(defaultUlnConfig)
+        })
+    })
+})
