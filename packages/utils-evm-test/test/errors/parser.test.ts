a91c67ee9ef595706e814a29bd2606f8b3ec5e78
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-12-05 17:35:38 -0800

    chore: Add more tests

--- packages/utils-evm-test/test/errors/parser.test.ts
+++ packages/utils-evm-test/test/errors/parser.test.ts
@@ -2,7 +2,14 @@ import fc from 'fast-check'
 import hre from 'hardhat'
 import { expect } from 'chai'
 import { Contract } from '@ethersproject/contracts'
-import { OmniContractFactory, createErrorParser, PanicError, RevertError, CustomError } from '@layerzerolabs/utils-evm'
+import {
+    createErrorParser,
+    PanicError,
+    RevertError,
+    CustomError,
+    UnknownError,
+    OmniContractFactory,
+} from '@layerzerolabs/utils-evm'
 import { OmniError } from '@layerzerolabs/utils'
 import { pointArbitrary } from '@layerzerolabs/test-utils'
 
@@ -116,6 +123,38 @@ describe('errors/parser', () => {
                     expect(parsedError.error).to.be.instanceOf(CustomError)
                     expect(parsedError.error.reason).to.eql('CustomErrorWithAnArgument')
                     expect((parsedError.error as CustomError).args).to.eql(['my bad'])
+                })
+            )
+        })
+
+        it('should parse string', async () => {
+            const errorParser = createErrorParser(omniContractFactory)
+
+            await fc.assert(
+                fc.asyncProperty(pointArbitrary, async (point) => {
+                    const omniError: OmniError = { error: 'some weird error', point }
+                    const parsedError = await errorParser(omniError)
+
+                    expect(parsedError.point).to.eql(point)
+                    expect(parsedError.error).to.be.instanceOf(UnknownError)
+                    expect(parsedError.error.reason).to.be.undefined
+                    expect(parsedError.error.message).to.eql('Unknown error: some weird error')
+                })
+            )
+        })
+
+        it('should parse an Error', async () => {
+            const errorParser = createErrorParser(omniContractFactory)
+
+            await fc.assert(
+                fc.asyncProperty(pointArbitrary, async (point) => {
+                    const omniError: OmniError = { error: new Error('some weird error'), point }
+                    const parsedError = await errorParser(omniError)
+
+                    expect(parsedError.point).to.eql(point)
+                    expect(parsedError.error).to.be.instanceOf(UnknownError)
+                    expect(parsedError.error.reason).to.be.undefined
+                    expect(parsedError.error.message).to.eql('Unknown error: Error: some weird error')
                 })
             )
         })
