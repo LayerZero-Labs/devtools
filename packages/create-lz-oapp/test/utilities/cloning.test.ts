71a4b7fc38e31706b0d9a2cbdb74c30edf7b0995
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2023-12-08 14:13:05 -0800

    🪚 OmniGraph™ Use jest in utils-evm-test (#80)

--- packages/create-lz-oapp/test/utilities/cloning.test.ts
+++ packages/create-lz-oapp/test/utilities/cloning.test.ts
@@ -1,31 +1,29 @@
-import { expect } from 'chai'
-import { describe, it } from 'mocha'
-import { createExampleGitURL } from '../../src/utilities/cloning.js'
+import { createExampleGitURL } from '@/utilities/cloning.js'
 
 describe('utilities/cloning', () => {
     describe('createExampleGitURL', () => {
         const REPO_URL = 'git@github.com:LayerZero-Labs/lz-utils'
 
         it('should return the repository field if directory and ref are not specified', () => {
-            expect(createExampleGitURL({ repository: REPO_URL, id: 'dummy', label: 'Dummy' })).to.eql(REPO_URL)
+            expect(createExampleGitURL({ repository: REPO_URL, id: 'dummy', label: 'Dummy' })).toEqual(REPO_URL)
         })
 
         it('should return the repository field with directory if directory is specified', () => {
-            expect(createExampleGitURL({ repository: REPO_URL, directory: 'dir', id: 'dummy', label: 'Dummy' })).to.eql(
-                `${REPO_URL}/dir`
-            )
+            expect(
+                createExampleGitURL({ repository: REPO_URL, directory: 'dir', id: 'dummy', label: 'Dummy' })
+            ).toEqual(`${REPO_URL}/dir`)
             expect(
                 createExampleGitURL({ repository: REPO_URL, directory: '/dir', id: 'dummy', label: 'Dummy' })
-            ).to.eql(`${REPO_URL}/dir`)
+            ).toEqual(`${REPO_URL}/dir`)
             expect(
                 createExampleGitURL({ repository: REPO_URL, directory: 'dir', ref: '', id: 'dummy', label: 'Dummy' })
-            ).to.eql(`${REPO_URL}/dir`)
+            ).toEqual(`${REPO_URL}/dir`)
         })
 
         it('should return the repository field with directory and ref if directory and ref are specified', () => {
             expect(
                 createExampleGitURL({ repository: REPO_URL, directory: 'dir', ref: 'ref', id: 'dummy', label: 'Dummy' })
-            ).to.eql(`${REPO_URL}/dir#ref`)
+            ).toEqual(`${REPO_URL}/dir#ref`)
             expect(
                 createExampleGitURL({
                     repository: REPO_URL,
@@ -34,14 +32,14 @@ describe('utilities/cloning', () => {
                     id: 'dummy',
                     label: 'Dummy',
                 })
-            ).to.eql(`${REPO_URL}/dir#ref`)
+            ).toEqual(`${REPO_URL}/dir#ref`)
         })
 
         it('should return the repository field with ref if only ref specified', () => {
-            expect(createExampleGitURL({ repository: REPO_URL, ref: 'ref', id: 'dummy', label: 'Dummy' })).to.eql(
+            expect(createExampleGitURL({ repository: REPO_URL, ref: 'ref', id: 'dummy', label: 'Dummy' })).toEqual(
                 `${REPO_URL}#ref`
             )
-            expect(createExampleGitURL({ repository: REPO_URL, ref: '#ref', id: 'dummy', label: 'Dummy' })).to.eql(
+            expect(createExampleGitURL({ repository: REPO_URL, ref: '#ref', id: 'dummy', label: 'Dummy' })).toEqual(
                 `${REPO_URL}#ref`
             )
         })
