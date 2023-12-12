6676bd116902a10001fcf4cbcc915ede6080c9db
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-12-11 18:15:36 -0800

    chore: Add logLevel to the wire task, log some stuff

--- packages/ua-utils-evm-hardhat/src/tasks/oapp/wire.ts
+++ packages/ua-utils-evm-hardhat/src/tasks/oapp/wire.ts
@@ -6,10 +6,17 @@ import { OAppOmniGraphHardhat, OAppOmniGraphHardhatSchema } from '@/oapp'
 
 interface TaskArgs {
     oappConfig: string
+    logLevel?: string
 }
 
-const action: ActionType<TaskArgs> = async ({ oappConfig: oappConfigPath }) => {
+const action: ActionType<TaskArgs> = async ({ oappConfig: oappConfigPath, logLevel = 'info' }) => {
+    // We'll set the global logging level to get as much info as needed
+    setDefaultLogLevel(logLevel)
+
+    const logger = createLogger()
+
     // First we check that the config file is indeed there and we can read it
+    logger.debug(`Checking config file '${oappConfigPath}' for existence & readability`)
     const isConfigReadable = isFile(oappConfigPath) && isReadable(oappConfigPath)
     if (!isConfigReadable) {
         throw new Error(
@@ -17,19 +24,27 @@ const action: ActionType<TaskArgs> = async ({ oappConfig: oappConfigPath }) => {
         )
     }
 
+    // Keep talking to the user
+    logger.debug(`Config file '${oappConfigPath}' exists & is readable`)
+
     // Now let's see if we can load the config file
     let rawConfig: unknown
     try {
+        logger.debug(`Loading config file '${oappConfigPath}'`)
+
         rawConfig = require(oappConfigPath)
     } catch (error) {
         throw new Error(`Unable to read config file '${oappConfigPath}': ${error}`)
     }
+
+    logger.debug(`Loaded config file '${oappConfigPath}'`)
 
     // It's time to make sure that the config is not malformed
     //
     // At this stage we are only interested in the shape of the data,
     // we are not checking whether the information makes sense (e.g.
     // whether there are no missing nodes etc)
+    logger.debug(`Validating the structure of config file '${oappConfigPath}'`)
     const configParseResult = OAppOmniGraphHardhatSchema.safeParse(rawConfig)
     if (configParseResult.success === false) {
         // FIXME Error formatting
@@ -60,4 +75,5 @@ const action: ActionType<TaskArgs> = async ({ oappConfig: oappConfigPath }) => {
 }
 task(TASK_LZ_WIRE_OAPP, 'Wire LayerZero OApp')
     .addParam('oappConfig', 'Path to your LayerZero OApp config', './layerzero.config.js', types.string)
+    .addParam('logLevel', 'Logging level. One of: error, warn, info, verbose, debug, silly', 'info', types.string)
     .setAction(action)
