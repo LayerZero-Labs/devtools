0656295a9510b7932d79e01e546e88d6ff7cf206
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2023-12-11 17:06:39 -0800

    🪚 OmniGraph™ Update @layerzerolabs dependencies to 1.5.72 (#100)

--- packages/ua-utils-evm-hardhat-test/contracts/DefaultOApp.sol
+++ packages/ua-utils-evm-hardhat-test/contracts/DefaultOApp.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { OmniCounter as OmniCounterImpl } from "@layerzerolabs/lz-evm-oapp-v2/contracts/examples/OmniCounter.sol";
+import { OApp, Origin } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
 
-contract DefaultOApp is OmniCounterImpl {
-    constructor(address _endpoint) OmniCounterImpl(_endpoint) {}
+contract DefaultOApp is OApp {
+    constructor(address _endpoint, address _owner) OApp(_endpoint, _owner) {}
+
+    function _lzReceive(Origin calldata, bytes32, bytes calldata, address, bytes calldata) internal virtual override {}
 }
