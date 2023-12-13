ae638ad450b4a6b44fef639cccb7ab62abddf404
Ryan Goulding <goulding@layerzerolabs.org>
2023-12-13 14:02:40 -0800

    feat: OmniCounter plus deployment code (#115)
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

---
+++ packages/ua-utils-evm-hardhat-test/deploy/003_omnicounter.ts
@@ -0,0 +1,33 @@
+import { formatEid } from '@layerzerolabs/utils'
+import { type DeployFunction } from 'hardhat-deploy/types'
+import assert from 'assert'
+
+/**
+ * This deploy function will deploy and configure LayerZero OmniCounter
+ *
+ * @param env `HardhatRuntimeEnvironment`
+ */
+const deploy: DeployFunction = async ({ getUnnamedAccounts, deployments, network }) => {
+    assert(network.config.eid != null, `Missing endpoint ID for network ${network.name}`)
+
+    const [deployer] = await getUnnamedAccounts()
+    assert(deployer, 'Missing deployer')
+
+    await deployments.delete('OmniCounter')
+    const endpointV2 = await deployments.get('EndpointV2')
+    const omniCounterDeployment = await deployments.deploy('OmniCounter', {
+        contract: 'contracts/OmniCounter.sol:OmniCounter',
+        from: deployer,
+        args: [endpointV2.address, deployer],
+    })
+
+    console.table({
+        Network: `${network.name} (endpoint ${formatEid(network.config.eid)})`,
+        OmniCounter: omniCounterDeployment.address,
+    })
+}
+
+deploy.tags = ['OmniCounter']
+deploy.dependencies = ['Bootstrap']
+
+export default deploy
