bca05eec1b7e6294dbf945791f5f56e0a1a47eea
Jan Nanista <jan.jakub.nanista@gmail.com>
2023-12-13 18:13:01 -0800

    chore: Add Zod error printer

--- packages/io-utils/test/stdio/printer.test.ts
+++ packages/io-utils/test/stdio/printer.test.ts
@@ -1,4 +1,6 @@
-import { printRecord } from '@/stdio'
+import { printRecord, printZodErrors } from '@/stdio'
+import assert from 'assert'
+import { z } from 'zod'
 
 describe('stdio/printer', () => {
     let CI: string | undefined
@@ -60,6 +62,80 @@ describe('stdio/printer', () => {
 
         it('should return a nested list for a nested object in array', () => {
             expect(printRecord([...list, list, record])).toMatchSnapshot()
+        })
+    })
+
+    describe('printZodErrors', () => {
+        const assertFailedSchema = (schema: z.ZodSchema, value: unknown): z.ZodError => {
+            const result = schema.safeParse(value)
+            assert(result.success === false, `Expected schema to fail for input '${value}' but it passed`)
+
+            return result.error
+        }
+
+        it('should have a good test helper', () => {
+            expect(() => assertFailedSchema(z.number(), 6)).toThrow(
+                /Expected schema to fail for input '6' but it passed/
+            )
+        })
+
+        it('should work for primitives', () => {
+            const numericError = assertFailedSchema(z.number(), 'a string')
+            const stringError = assertFailedSchema(z.string(), {})
+            const bigintError = assertFailedSchema(z.bigint(), [])
+            const booleanError = assertFailedSchema(z.boolean(), [])
+
+            expect(printZodErrors(numericError)).toMatchSnapshot()
+            expect(printZodErrors(stringError)).toMatchSnapshot()
+            expect(printZodErrors(bigintError)).toMatchSnapshot()
+            expect(printZodErrors(booleanError)).toMatchSnapshot()
+        })
+
+        it('should work for arrays', () => {
+            const numericArrayError = assertFailedSchema(z.array(z.number()), 'a string')
+            const stringArrayError = assertFailedSchema(z.array(z.string()), new Date())
+            const bigintArrayError = assertFailedSchema(z.array(z.bigint()), {})
+            const booleanArrayError = assertFailedSchema(z.array(z.boolean()), new RegExp(''))
+
+            expect(printZodErrors(numericArrayError)).toMatchSnapshot()
+            expect(printZodErrors(stringArrayError)).toMatchSnapshot()
+            expect(printZodErrors(bigintArrayError)).toMatchSnapshot()
+            expect(printZodErrors(booleanArrayError)).toMatchSnapshot()
+        })
+
+        it('should work for simple objects', () => {
+            const recordSchema = z.object({ a: z.number() })
+            const typeObjectError = assertFailedSchema(recordSchema, 'a string')
+            const noPropertyObjectError = assertFailedSchema(recordSchema, {})
+            const propertyObjectError1 = assertFailedSchema(recordSchema, { a: 'hello' })
+            const propertyObjectError2 = assertFailedSchema(recordSchema, { a: [], b: 7 })
+
+            expect(printZodErrors(typeObjectError)).toMatchSnapshot()
+            expect(printZodErrors(propertyObjectError1)).toMatchSnapshot()
+            expect(printZodErrors(propertyObjectError2)).toMatchSnapshot()
+            expect(printZodErrors(noPropertyObjectError)).toMatchSnapshot()
+        })
+
+        it('should work for nested objects', () => {
+            const subRecordSchema = z.object({ c: z.number() })
+            const recordSchema = z.object({ a: z.array(subRecordSchema), b: subRecordSchema })
+            const typeObjectError = assertFailedSchema(recordSchema, 'a string')
+            const noPropertyObjectError = assertFailedSchema(recordSchema, {})
+            const propertyObjectError1 = assertFailedSchema(recordSchema, { a: 'hello' })
+            const propertyObjectError2 = assertFailedSchema(recordSchema, { a: [], b: 7 })
+            const propertyObjectError3 = assertFailedSchema(recordSchema, { a: ['hello'], b: 7 })
+            const propertyObjectError4 = assertFailedSchema(recordSchema, { a: [{ c: 'hello' }], b: 7 })
+            const propertyObjectError5 = assertFailedSchema(recordSchema, { a: ['hello'], b: 7 })
+            const propertyObjectError6 = assertFailedSchema(recordSchema, { a: [{ c: [] }], b: { c: true } })
+
+            expect(printZodErrors(typeObjectError)).toMatchSnapshot()
+            expect(printZodErrors(propertyObjectError1)).toMatchSnapshot()
+            expect(printZodErrors(propertyObjectError2)).toMatchSnapshot()
+            expect(printZodErrors(propertyObjectError3)).toMatchSnapshot()
+            expect(printZodErrors(propertyObjectError4)).toMatchSnapshot()
+            expect(printZodErrors(propertyObjectError5)).toMatchSnapshot()
+            expect(printZodErrors(propertyObjectError6)).toMatchSnapshot()
+            expect(printZodErrors(noPropertyObjectError)).toMatchSnapshot()
         })
     })
 })
