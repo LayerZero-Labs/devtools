1b2c4cba5dd4cc100a56bd1fa2bde73bcd782a27
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-01-02 15:51:48 -0800

    chore: Coerce UInt in the schema

--- packages/devtools/src/omnigraph/schema.ts
+++ packages/devtools/src/omnigraph/schema.ts
@@ -2,7 +2,7 @@ import { EndpointId } from '@layerzerolabs/lz-definitions'
 import { z } from 'zod'
 import type { OmniPoint, OmniNode, OmniVector, OmniEdge, OmniGraph } from './types'
 
-export const UIntSchema = z.number().nonnegative().int()
+export const UIntSchema = z.coerce.number().nonnegative().int()
 
 export const AddressSchema = z.string()
 
