d58077f1aa63ccfded7010ce9cce34e9e823c791
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-01-05 16:09:54 -0800

    chore: Small fixes for check config task

--- packages/ua-devtools-evm-hardhat/src/tasks/oapp/checkWire.ts
+++ packages/ua-devtools-evm-hardhat/src/tasks/oapp/checkWire.ts
@@ -1,21 +1,21 @@
 import { ActionType } from 'hardhat/types'
 import { task, types } from 'hardhat/config'
 import { createLogger, setDefaultLogLevel } from '@layerzerolabs/io-devtools'
+import { printRecords } from '@layerzerolabs/io-devtools/swag'
 import { TASK_LZ_CHECK_WIRE_OAPP } from '@/constants/tasks'
 import { printLogo } from '@layerzerolabs/io-devtools/swag'
-import { OAppPeers, OAppOmniGraph } from '@layerzerolabs/ua-devtools'
+import { OAppOmniGraph } from '@layerzerolabs/ua-devtools'
 import { createConnectedContractFactory } from '@layerzerolabs/devtools-evm-hardhat'
 import { createOAppFactory } from '@layerzerolabs/ua-devtools-evm'
 import { checkOAppPeers } from '@layerzerolabs/ua-devtools'
-import { endpointIdToNetwork } from '@layerzerolabs/lz-definitions'
 import { validateAndTransformOappConfig } from '@/utils/taskHelpers'
+import { getNetworkNameForEid } from '@layerzerolabs/devtools-evm-hardhat'
+import { printBoolean } from '@layerzerolabs/io-devtools'
 
 interface TaskArgs {
     oappConfig: string
     logLevel?: string
 }
-
-type ConnectedPeers<TKey extends string | number | symbol> = Record<TKey, Record<TKey, string>>
 
 export const checkWire: ActionType<TaskArgs> = async ({ oappConfig: oappConfigPath, logLevel = 'info' }) => {
     printLogo()
@@ -32,32 +32,21 @@ export const checkWire: ActionType<TaskArgs> = async ({ oappConfig: oappConfigPa
     const contractFactory = createConnectedContractFactory()
     const oAppFactory = createOAppFactory(contractFactory)
 
-    let checkOAppPeersArray: OAppPeers[]
     try {
-        checkOAppPeersArray = await checkOAppPeers(graph, oAppFactory)
+        const peers = await checkOAppPeers(graph, oAppFactory)
+
+        const formattedPeers = peers.map((peer) => ({
+            'From network': getNetworkNameForEid(peer.vector.from.eid),
+            'To network': getNetworkNameForEid(peer.vector.to.eid),
+            Connected: printBoolean(peer.hasPeer),
+        }))
+
+        printRecords(formattedPeers)
+
+        return peers
     } catch (error) {
         throw new Error(`An error occurred while getting the OApp configuration: ${error}`)
     }
-
-    const connectedPeers: ConnectedPeers<string> = {}
-    for (const oappPeer of checkOAppPeersArray) {
-        const fromNetwork = endpointIdToNetwork(oappPeer.vector.from.eid)
-        const toNetwork = endpointIdToNetwork(oappPeer.vector.to.eid)
-
-        if (!connectedPeers[fromNetwork]) {
-            connectedPeers[fromNetwork] = {
-                [fromNetwork]: '',
-            }
-        }
-
-        const connection = connectedPeers[fromNetwork]
-        if (connection && !connection[toNetwork]) {
-            connection[toNetwork] = oappPeer.hasPeer ? '🟩' : '🟥'
-        }
-    }
-
-    console.table(connectedPeers)
-    return connectedPeers
 }
 
 task(
