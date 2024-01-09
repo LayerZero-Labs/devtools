ad8b0bd4133965807d03b8ee741ba6c2abc0a48b
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-01-09 15:47:22 -0800

    chore: Some docs

--- packages/devtools/src/transactions/signer.ts
+++ packages/devtools/src/transactions/signer.ts
@@ -3,8 +3,11 @@ import type { OmniSignerFactory, OmniTransaction, OmniTransactionWithError, Omni
 import { formatOmniPoint } from '@/omnigraph/format'
 
 export type SignAndSendResult = [
+    // All the successful transactions
     successful: OmniTransactionWithReceipt[],
+    // The failed transactions along with the errors
     errors: OmniTransactionWithError[],
+    // All the transactions that have not been executed (including the failed ones)
     pending: OmniTransaction[],
 ]
 
