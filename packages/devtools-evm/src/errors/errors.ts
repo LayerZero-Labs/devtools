5077bf175ea44e605ca9c4c86121dc7f9b0d9b25
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2024-01-15 11:20:02 -0800

    🪚 Decode errors: ground work [1/N] (#204)

--- packages/devtools-evm/src/errors/errors.ts
+++ packages/devtools-evm/src/errors/errors.ts
@@ -1,3 +1,5 @@
+import { printJson } from '@layerzerolabs/io-devtools'
+
 export abstract class ContractError<TReason = unknown> extends Error {
     public abstract readonly reason: TReason
 }
@@ -7,24 +9,35 @@ export class UnknownError extends ContractError<undefined> {
 
     constructor(message = 'Unknown contract error') {
         super(message)
+        this.name = 'UnknownError'
     }
 }
 
 export class PanicError extends ContractError<bigint> {
     constructor(
         public readonly reason: bigint,
-        message: string = `Contract panicked with code ${reason}`
+        message: string = `Contract panicked (assert() has been called)`
     ) {
         super(message)
+        this.name = 'PanicError'
+    }
+
+    override toString(): string {
+        return `${super.toString()}. Error code ${this.reason}`
     }
 }
 
 export class RevertError extends ContractError<string> {
     constructor(
         public readonly reason: string,
-        message: string = `Contract reverted with reason '${reason}'`
+        message: string = `Contract reverted`
     ) {
         super(message)
+        this.name = 'RevertError'
+    }
+
+    override toString(): string {
+        return `${super.toString()}. Error reason '${this.reason}'`
     }
 }
 
@@ -32,8 +45,15 @@ export class CustomError extends ContractError<string> {
     constructor(
         public readonly reason: string,
         public readonly args: unknown[],
-        message: string = `Contract reverted with custom error '${reason}'`
+        message: string = `Contract reverted with custom error`
     ) {
         super(message)
+        this.name = 'CustomError'
+    }
+
+    override toString(): string {
+        const formattedArgs = this.args.map((arg) => printJson(arg, false))
+
+        return `${super.toString()}. Error ${this.reason}(${formattedArgs})`
     }
 }
