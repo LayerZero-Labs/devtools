0cce8624992d6288a5eecf1e4478b52dd62c787e
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2024-01-17 15:45:23 -0800

    🚧 Deduplicate library registration (#213)

---
+++ .changeset/big-lies-eat.md
@@ -0,0 +1,6 @@
+---
+"@layerzerolabs/protocol-devtools": patch
+"@layerzerolabs/devtools": patch
+---
+
+Fixed a problem with duplicate library registration when confgiuring Endpoint
--- packages/protocol-devtools/src/endpoint/config.ts
+++ packages/protocol-devtools/src/endpoint/config.ts
@@ -1,13 +1,38 @@
-import { flattenTransactions, type OmniTransaction } from '@layerzerolabs/devtools'
-import type { EndpointFactory, EndpointOmniGraph } from './types'
+import { flattenTransactions, type OmniTransaction, OmniPointMap, Bytes32 } from '@layerzerolabs/devtools'
+import type { EndpointFactory, EndpointOmniGraph, IEndpoint } from './types'
 
 export type EndpointConfigurator = (graph: EndpointOmniGraph, createSdk: EndpointFactory) => Promise<OmniTransaction[]>
 
 export const configureEndpoint: EndpointConfigurator = async (graph, createSdk) =>
     flattenTransactions([
+        await configureEndpointRegisterLibraries(graph, createSdk),
         await configureEndpointDefaultReceiveLibraries(graph, createSdk),
         await configureEndpointDefaultSendLibraries(graph, createSdk),
     ])
+
+export const configureEndpointRegisterLibraries: EndpointConfigurator = async (graph, createSdk) => {
+    const librariesByEndpoint = graph.connections.reduce(
+        (librariesByEndpoint, { vector: { from }, config }) =>
+            librariesByEndpoint.set(
+                from,
+                librariesByEndpoint
+                    .getOrElse(from, () => new Set<string>())
+                    .add(config.defaultReceiveLibrary)
+                    .add(config.defaultSendLibrary)
+            ),
+        new OmniPointMap<Set<Bytes32>>()
+    )
+
+    return flattenTransactions(
+        await Promise.all(
+            Array.from(librariesByEndpoint).map(async ([from, libraries]) => {
+                const sdk = await createSdk(from)
+
+                return registerLibraries(sdk, Array.from(libraries))
+            })
+        )
+    )
+}
 
 export const configureEndpointDefaultReceiveLibraries: EndpointConfigurator = async (graph, createSdk) =>
     flattenTransactions(
@@ -19,18 +44,13 @@ export const configureEndpointDefaultReceiveLibraries: EndpointConfigurator = as
                 // If the library is already set as default, do nothing
                 if (config.defaultReceiveLibrary === address) return []
 
-                // We need to check whether the library has been registered before we set is as default
-                const isRegistered = await sdk.isRegisteredLibrary(config.defaultReceiveLibrary)
-
-                return flattenTransactions([
-                    // We only want to register the library if it has not been registered yet
-                    isRegistered ? undefined : await sdk.registerLibrary(config.defaultReceiveLibrary),
+                return [
                     await sdk.setDefaultReceiveLibrary(
                         to.eid,
                         config.defaultReceiveLibrary,
                         config.defaultReceiveLibraryGracePeriod
                     ),
-                ])
+                ]
             })
         )
     )
@@ -45,14 +65,19 @@ export const configureEndpointDefaultSendLibraries: EndpointConfigurator = async
                 // If the library is already set as default, do nothing
                 if (config.defaultSendLibrary === address) return []
 
-                // We need to check whether the library has been registered before we set is as default
-                const isRegistered = await sdk.isRegisteredLibrary(config.defaultSendLibrary)
+                return [await sdk.setDefaultSendLibrary(to.eid, config.defaultSendLibrary)]
+            })
+        )
+    )
 
-                return flattenTransactions([
-                    // We only want to register the library if it has not been registered yet
-                    isRegistered ? undefined : await sdk.registerLibrary(config.defaultSendLibrary),
-                    await sdk.setDefaultSendLibrary(to.eid, config.defaultSendLibrary),
-                ])
+const registerLibraries = async (sdk: IEndpoint, libraries: string[]): Promise<OmniTransaction[]> =>
+    flattenTransactions(
+        await Promise.all(
+            libraries.map(async (address) => {
+                const isRegistered = await sdk.isRegisteredLibrary(address)
+
+                if (isRegistered) return []
+                return [await sdk.registerLibrary(address)]
             })
         )
     )
