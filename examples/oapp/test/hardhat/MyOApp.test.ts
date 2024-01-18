e4f40d916d0df5e23fbf16728b6b843eebd3a13f
Matthew Krak <krak@layerzerolabs.org>
2024-01-17 16:38:57 -0800

    hardhat test for MyOApp

---
+++ examples/oapp/test/hardhat/MyOApp.test.ts
@@ -0,0 +1,40 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+describe('MyOApp', function () {
+    beforeEach(async function () {
+        this.eid = 123
+        const signers = await ethers.getSigners()
+        this.owner = signers[0]
+        console.log(this.owner.address)
+
+        const LayerZeroEndpointMock = await ethers.getContractFactory('LZEndpointV2Mock')
+        this.LzEndpointV2Mock = await LayerZeroEndpointMock.deploy(this.eid)
+
+        const MyOApp = await ethers.getContractFactory('MyOApp')
+        this.myOAppA = await MyOApp.deploy(this.LzEndpointV2Mock.address, this.owner.address)
+        this.myOAppB = await MyOApp.deploy(this.LzEndpointV2Mock.address, this.owner.address)
+
+        await this.LzEndpointV2Mock.setDestLzEndpoint(this.myOAppA.address, this.LzEndpointV2Mock.address)
+        await this.LzEndpointV2Mock.setDestLzEndpoint(this.myOAppB.address, this.LzEndpointV2Mock.address)
+
+        await this.myOAppA.setPeer(this.eid, ethers.utils.zeroPad(this.myOAppB.address, 32))
+        await this.myOAppB.setPeer(this.eid, ethers.utils.zeroPad(this.myOAppB.address, 32))
+    })
+
+    it('send a message to each destination OApp', async function () {
+        expect(await this.myOAppA.data()).to.equal('Nothing received yet.')
+        expect(await this.myOAppB.data()).to.equal('Nothing received yet.')
+
+        await this.myOAppA.send(this.eid, 'Test message.', '0x00030100110100000000000000000000000000030d40', {
+            value: ethers.utils.parseEther('0.5'),
+        })
+        expect(await this.myOAppA.data()).to.equal('Nothing received yet.')
+        expect(await this.myOAppB.data()).to.equal('Test message.')
+
+        /*await this.myOAppB.send(this.eid, { value: ethers.utils.parseEther("0.5") });
+        expect(await this.myOAppA.data()).to.equal("Test message.");
+        expect(await this.myOAppB.data()).to.equal("Test message.");
+        */
+    })
+})
