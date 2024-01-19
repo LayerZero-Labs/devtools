f4b2dbc3aec084385c3d0ef356a4ff24ab458c98
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-01-19 10:51:20 -0800

    chore: Fix hardhat tests in OApp

---
+++ examples/oapp/test/hardhat/OApp.test.ts
@@ -0,0 +1,11 @@
+import { before, beforeEach, describe } from 'mocha'
+
+describe('OApp: ', function () {
+    before(async function () {
+        //TODO
+    })
+
+    beforeEach(async function () {
+        //TODO
+    })
+})
