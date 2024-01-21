462566cc672411d93453ae0ba249902df8ea52e5
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2024-01-21 10:10:04 -0800

    🧹 Use @swc/jest to speed up test runs (#224)

--- packages/ua-devtools-evm-hardhat/jest.config.js
+++ packages/ua-devtools-evm-hardhat/jest.config.js
@@ -1,9 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
     cache: false,
     testEnvironment: 'node',
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    transform: {
+        '^.+\\.(t|j)sx?$': '@swc/jest',
     },
 };
--- tests/ua-devtools-evm-hardhat-test/jest.config.js
+++ tests/ua-devtools-evm-hardhat-test/jest.config.js
@@ -1,10 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
     cache: false,
     testEnvironment: 'node',
     testTimeout: 300_000,
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    transform: {
+        '^.+\\.(t|j)sx?$': '@swc/jest',
     },
 };
