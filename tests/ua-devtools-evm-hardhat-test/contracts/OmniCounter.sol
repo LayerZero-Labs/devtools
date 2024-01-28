07d58dae4055a3f009c55c3cc038c6cb8fd433bb
lz.wΔrlock() <trileyschwarz@gmail.com>
2024-01-28 14:14:06 -0800

    rename variables to delegate

--- tests/ua-devtools-evm-hardhat-test/contracts/OmniCounter.sol
+++ tests/ua-devtools-evm-hardhat-test/contracts/OmniCounter.sol
@@ -5,5 +5,5 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OmniCounter as OmniCounterImpl } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/examples/OmniCounter.sol";
 
 contract OmniCounter is OmniCounterImpl {
-    constructor(address _endpoint, address _owner) OmniCounterImpl(_endpoint, _owner) Ownable(_owner) {}
+    constructor(address _endpoint, address _delegate) OmniCounterImpl(_endpoint, _delegate) Ownable(_delegate) {}
 }
