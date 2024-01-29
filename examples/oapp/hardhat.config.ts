4aee52a057225ffa472d668e3a71fbe04f989e8d
Matthew Krak <krak@layerzerolabs.org>
2024-01-29 13:31:01 -0800

    added setup configs

--- examples/oapp/hardhat.config.ts
+++ examples/oapp/hardhat.config.ts
@@ -4,6 +4,10 @@ import '@nomiclabs/hardhat-ethers'
 import '@layerzerolabs/toolbox-hardhat'
 import { HardhatUserConfig } from 'hardhat/types'
 
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+
+const MNEMONIC = process.env.MNEMONIC ?? ''
+
 const config: HardhatUserConfig = {
     solidity: {
         compilers: [
@@ -18,7 +22,29 @@ const config: HardhatUserConfig = {
             },
         ],
     },
-
+    networks: {
+        sepolia: {
+            eid: EndpointId.ETHEREUM_V2_TESTNET,
+            url: process.env.NETWORK_URL_ETHEREUM_SEPOLIA ?? 'https://rpc.sepolia.org/',
+            accounts: {
+                mnemonic: MNEMONIC,
+            },
+        },
+        fuji: {
+            eid: EndpointId.AVALANCHE_V2_TESTNET,
+            url: process.env.NETWORK_URL_AVALANCHE_FUJI ?? 'https://api.avax-test.network/',
+            accounts: {
+                mnemonic: MNEMONIC,
+            },
+        },
+        mumbai: {
+            eid: EndpointId.POLYGON_V2_TESTNET,
+            url: process.env.NETWORK_URL_POLYGON_MUMBAI ?? 'https://rpc-mumbai.matic.today',
+            accounts: {
+                mnemonic: MNEMONIC,
+            },
+        },
+    },
     namedAccounts: {
         deployer: {
             default: 0, // wallet address of index[0], of the mnemonic in .env
