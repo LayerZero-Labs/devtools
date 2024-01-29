4b3d4ee2a91e9f6253244bb2c28f39c6ca0ff996
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2024-01-29 11:08:44 -0800

    refactored getting networks in getDefaultTask

--- packages/ua-devtools-evm-hardhat/src/tasks/oapp/config.get.default.ts
+++ packages/ua-devtools-evm-hardhat/src/tasks/oapp/config.get.default.ts
@@ -1,25 +1,27 @@
 import { ActionType } from 'hardhat/types'
-import { task, types } from 'hardhat/config'
+import { task } from 'hardhat/config'
 import { printRecord } from '@layerzerolabs/io-devtools'
 import { getReceiveConfig, getSendConfig } from '@/utils/taskHelpers'
 import { TASK_LZ_OAPP_CONFIG_GET_DEFAULT } from '@/constants'
 import { setDefaultLogLevel } from '@layerzerolabs/io-devtools'
-import { getEidsByNetworkName } from '@layerzerolabs/devtools-evm-hardhat'
+import { getEidsByNetworkName, types } from '@layerzerolabs/devtools-evm-hardhat'
 
 interface TaskArgs {
     logLevel?: string
-    networks?: string
+    networks?: string[]
 }
 
-export const getDefaultConfig: ActionType<TaskArgs> = async (taskArgs, hre) => {
+export const getDefaultConfig: ActionType<TaskArgs> = async (
+    { logLevel = 'info', networks: networksArgument },
+    hre
+) => {
     // We'll set the global logging level to get as much info as needed
-    setDefaultLogLevel(taskArgs.logLevel ?? 'info')
-    let networks
-    if (taskArgs?.networks != null) {
-        networks = new Set(taskArgs.networks.split(','))
-    } else {
-        networks = Object.keys(getEidsByNetworkName(hre)).filter((key) => getEidsByNetworkName(hre)[key] !== undefined)
-    }
+    setDefaultLogLevel(logLevel)
+
+    const networks =
+        networksArgument ??
+        Object.entries(getEidsByNetworkName(hre)).flatMap(([networkName, eid]) => (eid == null ? [] : [networkName]))
+
     const configs: Record<string, Record<string, unknown>> = {}
     for (const localNetworkName of networks) {
         configs[localNetworkName] = {}
@@ -61,5 +63,5 @@ task(
     'Outputs the default Send and Receive Messaging Library versions and the default application config'
 )
     .addParam('logLevel', 'Logging level. One of: error, warn, info, verbose, debug, silly', 'info', types.string)
-    .addOptionalParam('networks', 'comma separated list of networks', undefined, types.string)
+    .addOptionalParam('networks', 'comma separated list of networks', undefined, types.networks)
     .setAction(getDefaultConfig)
