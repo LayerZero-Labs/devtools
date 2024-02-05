c3cea65c33ec0da9586dff1c3a0672c5e1c9918b
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-02-05 09:24:05 -0800

    chore: Add jest-extended to devtools-evm-hardhat

--- tests/devtools-evm-hardhat-test/jest.config.js
+++ tests/devtools-evm-hardhat-test/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     cache: false,
     testEnvironment: 'node',
     testTimeout: 150_000,
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
