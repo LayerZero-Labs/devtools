3235d21e0bfab4b79b473f119c2b3bf7e3a52ba9
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-02-06 09:34:37 -0800

    chore: Track stdout/stderr in create-lz-oapp

--- packages/create-lz-oapp/src/utilities/installation.ts
+++ packages/create-lz-oapp/src/utilities/installation.ts
@@ -1,9 +1,22 @@
 import type { Config, PackageManager } from '@/types'
+import { createModuleLogger } from '@layerzerolabs/io-devtools'
 import { spawn } from 'child_process'
 import which from 'which'
 
 export const installDependencies = (config: Config) =>
     new Promise<void>((resolve, reject) => {
+        const logger = createModuleLogger('installation')
+
+        // We'll store combined stdout and stderr in this variable
+        const std: string[] = []
+
+        // This function will handle stdout/stderr streams from the child process
+        const handleStd = (chunk: string) => {
+            std.push(chunk)
+
+            logger.verbose(chunk)
+        }
+
         /**
          * Spawn the installation process.
          */
@@ -19,16 +32,35 @@ export const installDependencies = (config: Config) =>
             },
         })
 
+        child.stdout.setEncoding('utf8')
+        child.stderr.setEncoding('utf8')
+        child.stdout.on('data', handleStd)
+        child.stderr.on('data', handleStd)
+
         child.on('close', (code) => {
-            if (code !== 0) {
-                reject(
-                    new Error(
-                        `Failed to install dependencies: ${config.packageManager.label} install exited with code ${code}`
-                    )
-                )
-            } else resolve()
+            switch (code) {
+                case null:
+                    return reject(new Error(`Failed to install dependencies: Installation interrupted`))
+
+                case 0:
+                    return resolve()
+
+                default:
+                    return reject(new InstallationError(config.packageManager, code, std.join('')))
+            }
         })
     })
 
 export const isPackageManagerAvailable = ({ executable }: PackageManager): boolean =>
     !!which.sync(executable, { nothrow: true })
+
+export class InstallationError extends Error {
+    constructor(
+        public readonly packageManager: PackageManager,
+        public readonly exitCode: number,
+        public readonly stdout: string,
+        message: string = `Failed to install dependencies: ${packageManager.label} exited with code ${exitCode}`
+    ) {
+        super(message)
+    }
+}
