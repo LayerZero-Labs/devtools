e9d45858f6e98963c5248a76b9fcf08fb6133ef3
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-02-07 10:51:24 -0800

    chore: Split off the configuration part of wire task into a subtask

---
+++ packages/ua-devtools-evm-hardhat/src/tasks/oapp/wire/subtask.configure.ts
@@ -0,0 +1,38 @@
+import { SUBTASK_LZ_OAPP_WIRE_CONFIGURE } from '@/constants'
+import { OmniTransaction } from '@layerzerolabs/devtools'
+import { createConnectedContractFactory } from '@layerzerolabs/devtools-evm-hardhat'
+import { createModuleLogger, printJson } from '@layerzerolabs/io-devtools'
+import { configureOApp, type OAppConfigurator, type OAppFactory, type OAppOmniGraph } from '@layerzerolabs/ua-devtools'
+import { createOAppFactory } from '@layerzerolabs/ua-devtools-evm'
+import { subtask } from 'hardhat/config'
+import { ActionType } from 'hardhat/types'
+
+export interface TaskArgs {
+    graph: OAppOmniGraph
+    configurator?: OAppConfigurator
+    oappFactory?: OAppFactory
+}
+
+const action: ActionType<TaskArgs> = async ({
+    graph,
+    configurator = configureOApp,
+    oappFactory = createOAppFactory(createConnectedContractFactory()),
+}: TaskArgs): Promise<OmniTransaction[]> => {
+    const logger = createModuleLogger(SUBTASK_LZ_OAPP_WIRE_CONFIGURE)
+
+    logger.verbose(`Running with graph:\n\n${printJson(graph)}`)
+
+    try {
+        return await configurator(graph, oappFactory)
+    } catch (error) {
+        logger.verbose(`Encountered an error: ${error}`)
+
+        throw new Error(`An error occurred while getting the OApp configuration: ${error}`)
+    }
+}
+
+subtask(SUBTASK_LZ_OAPP_WIRE_CONFIGURE, 'Create a list of OmniTransactions that configure your OApp')
+    .addParam('graph', 'Configuration of you OApp of type OAppOmniGraph')
+    .addParam('configurator', 'Configuration function of type OAppConfigurator', undefined, undefined, true)
+    .addParam('oappFactory', 'SDK factory for OApp SDK of type OAppFactory', undefined, undefined, true)
+    .setAction(action)
