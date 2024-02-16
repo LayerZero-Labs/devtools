f0392c50c54c66c8f0b40dfa44697c9ce6943f19
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2024-02-15 16:50:39 -0800

    🗞️ Add lz:export:deployments:typescript hardhat task (#412)

---
+++ .changeset/silent-fishes-relax.md
@@ -0,0 +1,6 @@
+---
+"@layerzerolabs/devtools-evm-hardhat-test": patch
+"@layerzerolabs/devtools-evm-hardhat": patch
+---
+
+Add export deployments hardhat task
--- tests/devtools-evm-hardhat-test/jest.setup.js
+++ tests/devtools-evm-hardhat-test/jest.setup.js
@@ -7,5 +7,13 @@ expect.extend(jestExtended);
 // clear all deployments before & after every test
 const clearDeployments = () => rmSync('./deployments', { force: true, recursive: true });
 
-beforeEach(clearDeployments);
-afterEach(clearDeployments);
+// clear all generated files before & after every test
+const clearGenerated = () => rmSync('./generated', { force: true, recursive: true });
+
+const clean = () => {
+    clearDeployments();
+    clearGenerated();
+};
+
+beforeEach(clean);
+afterEach(clean);
