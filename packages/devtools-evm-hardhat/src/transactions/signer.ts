94de06b4b515a8503264e8680fc492a12e855b99
Ryan Goulding <goulding@layerzerolabs.org>
2024-02-21 09:09:58 -0800

    chore:  clean up based on combination of Jan / Riley's feedback
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- packages/devtools-evm-hardhat/src/transactions/signer.ts
+++ packages/devtools-evm-hardhat/src/transactions/signer.ts
@@ -6,17 +6,30 @@ import { createGetHreByEid } from '@/runtime'
 
 export const createSignerFactory = (
     addressOrIndex?: string | number,
+    providerFactory = createProviderFactory()
+): OmniSignerFactory<OmniSignerEVM> => {
+    return pMemoize(async (eid) => {
+        const provider = await providerFactory(eid)
+        const signer = provider.getSigner(addressOrIndex)
+
+        return new OmniSignerEVM(eid, signer)
+    })
+}
+
+export const createGnosisSignerFactory = (
+    addressOrIndex?: string | number,
     providerFactory = createProviderFactory(),
     networkEnvironmentFactory = createGetHreByEid()
-): OmniSignerFactory => {
+): OmniSignerFactory<GnosisOmniSignerEVM> => {
     return pMemoize(async (eid) => {
         const provider = await providerFactory(eid)
         const signer = provider.getSigner(addressOrIndex)
 
         const env = await networkEnvironmentFactory(eid)
         const safeConfig = env.network.config.safeConfig
-        return safeConfig
-            ? new GnosisOmniSignerEVM(eid, signer, safeConfig.safeUrl, safeConfig)
-            : new OmniSignerEVM(eid, signer)
+        if (!safeConfig) {
+            throw new Error('No safe config found for the current network')
+        }
+        return new GnosisOmniSignerEVM(eid, signer, safeConfig.safeUrl, safeConfig)
     })
 }
