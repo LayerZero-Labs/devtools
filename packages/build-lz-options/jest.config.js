18a91eee02ce328546e62d4a08e1dce556f027d2
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-03-12 09:44:59 -0700

    chore: Add github actions reporter to jest

--- packages/build-lz-options/jest.config.js
+++ packages/build-lz-options/jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
+    reporters: [['github-actions', { silent: false }], 'summary'],
     testEnvironment: 'node',
     testTimeout: 15000,
     moduleNameMapper: {
