9386821ef26d6339c17bcee877a2fac8b6b052c7
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-03-18 13:58:59 -0700

    chore: Add task names

--- packages/devtools-evm-hardhat/src/constants/tasks.ts
+++ packages/devtools-evm-hardhat/src/constants/tasks.ts
@@ -3,3 +3,9 @@ export const TASK_LZ_DEPLOY = 'lz:deploy'
 export const TASK_LZ_EXPORT_DEPLOYMENTS_TYPESCRIPT = 'lz:export:deployments:typescript'
 
 export const SUBTASK_LZ_SIGN_AND_SEND = '::lz:sign-and-send'
+
+export const TASK_LZ_TEST_SIMULATION_START = 'lz:test:simulation:start'
+
+export const TASK_LZ_TEST_SIMULATION_LOGS = 'lz:test:simulation:logs'
+
+export const TASK_LZ_TEST_SIMULATION_STOP = 'lz:test:simulation:stop'
