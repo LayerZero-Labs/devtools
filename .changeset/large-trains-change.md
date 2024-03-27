a6f2fef4b7a30d3ca483fdc2c066b5eef7c3591f
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2024-03-27 10:05:08 -0700

    🧹 Re-export useful types from toolbox-hardhat; Fix small type & export issues (#518)

---
+++ .changeset/large-trains-change.md
@@ -0,0 +1,9 @@
+---
+"@layerzerolabs/ua-devtools-evm-hardhat": patch
+"@layerzerolabs/toolbox-hardhat": patch
+"@layerzerolabs/devtools": patch
+"@layerzerolabs/oapp-example": patch
+"@layerzerolabs/oft-example": patch
+---
+
+Re-export useful types from toolbox-hardhat
--- packages/toolbox-hardhat/types/index.d.ts
+++ packages/toolbox-hardhat/types/index.d.ts
@@ -3,3 +3,10 @@
 //
 // To fix this, we will create a d.ts file instead of having one created during the build
 import '@layerzerolabs/devtools-evm-hardhat/type-extensions'
+
+// We re-export all the relevant types from devtools
+export type { OmniPointHardhat, OmniEdgeHardhat, OmniDeployment } from '@layerzerolabs/devtools-evm-hardhat'
+export type { OmniPoint, OmniVector, OmniNode, OmniEdge, OmniGraph } from '@layerzerolabs/devtools'
+
+// We also re-export all the relevant types from ua-devtools
+export type { OAppOmniGraphHardhat } from '@layerzerolabs/ua-devtools-evm-hardhat'
