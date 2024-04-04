719eacc7aa80a0b887a8b12528523fb074fc8ebf
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-04-04 16:17:39 +0000

    chore: Serialize executor config in the SDK

--- packages/protocol-devtools-evm/src/executor/sdk.ts
+++ packages/protocol-devtools-evm/src/executor/sdk.ts
@@ -1,5 +1,10 @@
 import type { EndpointId } from '@layerzerolabs/lz-definitions'
-import type { IExecutor, ExecutorDstConfig } from '@layerzerolabs/protocol-devtools'
+import {
+    type IExecutor,
+    type ExecutorDstConfig,
+    ExecutorDstConfigPre2_1_27Schema,
+    ExecutorDstConfigPost2_1_27Schema,
+} from '@layerzerolabs/protocol-devtools'
 import { formatEid, type OmniTransaction } from '@layerzerolabs/devtools'
 import { OmniSDK } from '@layerzerolabs/devtools-evm'
 import { printJson } from '@layerzerolabs/io-devtools'
@@ -18,20 +23,36 @@ export class Executor extends OmniSDK implements IExecutor {
 
     async setDstConfig(eid: EndpointId, value: ExecutorDstConfig): Promise<OmniTransaction> {
         const data = this.contract.contract.interface.encodeFunctionData('setDstConfig', [
-            [
-                {
-                    dstEid: eid,
-                    baseGas: value.baseGas,
-                    multiplierBps: value.multiplierBps,
-                    floorMarginUSD: value.floorMarginUSD,
-                    nativeCap: value.nativeCap,
-                },
-            ],
+            [this.serializeExecutorConfig(eid, value)],
         ])
 
         return {
             ...this.createTransaction(data),
             description: `Setting dstConfig for ${formatEid(eid)}: ${printJson(value)}`,
         }
+    }
+
+    private serializeExecutorConfig(eid: EndpointId, value: ExecutorDstConfig) {
+        if (ExecutorDstConfigPre2_1_27Schema.safeParse(value)) {
+            return {
+                dstEid: eid,
+                baseGas: value.baseGas,
+                multiplierBps: value.multiplierBps,
+                floorMarginUSD: value.floorMarginUSD,
+                nativeCap: value.nativeCap,
+            }
+        }
+
+        if (ExecutorDstConfigPost2_1_27Schema.safeParse(value)) {
+            return {
+                dstEid: eid,
+                baseGas: value.baseGas,
+                multiplierBps: value.multiplierBps,
+                floorMarginUSD: value.floorMarginUSD,
+                nativeCap: value.nativeCap,
+            }
+        }
+
+        throw new Error(`Failed to serialize ExecutorConfig for ${this.label}: ${printJson(value)}`)
     }
 }
