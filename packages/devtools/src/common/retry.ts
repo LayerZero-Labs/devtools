cb14bd8e28c8454046e77ebf019d8478b3245fc7
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-04-10 18:49:43 +0000

    feat: Feature-flag the AsyncRetriable

--- packages/devtools/src/common/retry.ts
+++ packages/devtools/src/common/retry.ts
@@ -2,16 +2,55 @@ import assert from 'assert'
 import { backOff } from 'exponential-backoff'
 
 export interface RetriableConfig<TInstance = unknown> {
+    /**
+     * Enable / disable the retry behavior
+     */
+    enabled?: boolean
+    /**
+     * The maximum delay, in milliseconds, between two consecutive attempts.
+     *
+     * @default Infinity
+     */
+    maxDelay?: number
+    /**
+     * Number of time the method call will be retried. The default is 3
+     *
+     * @default 3
+     */
     numAttempts?: number
-    onRetry?: (attempt: number, error: unknown, target: TInstance) => boolean | void | undefined
+    /**
+     * Callback called on every failed attempt.
+     *
+     * @param {number} attempt
+     * @param {unknown} error
+     * @param {this} target
+     * @returns {boolean | undefined} This function can stop the retry train by returning false
+     */
+    onRetry?: <TArgs extends unknown[] = unknown[]>(
+        attempt: number,
+        error: unknown,
+        target: TInstance,
+        args: TArgs
+    ) => boolean | void | undefined
 }
 
-export const AsyncRetriable = ({ numAttempts = 3, onRetry }: RetriableConfig = {}) => {
+export const AsyncRetriable = ({
+    // We'll feature flag this functionality for the time being
+    enabled = !!process.env.LZ_EXPERIMENTAL_ENABLE_RETRY,
+    maxDelay,
+    numAttempts = 3,
+    onRetry,
+}: RetriableConfig = {}) => {
     return function AsyncRetriableDecorator<TArgs extends unknown[], TResult>(
         target: unknown,
         propertyKey: string,
         descriptor: TypedPropertyDescriptor<(...args: TArgs) => Promise<TResult>>
     ) {
+        // If we are disabled, we are disabled
+        if (!enabled) {
+            return descriptor
+        }
+
         // Grab the original method and ensure that we are decorating a method
         const originalMethod = descriptor.value
         assert(
@@ -24,11 +63,23 @@ export const AsyncRetriable = ({ numAttempts = 3, onRetry }: RetriableConfig = {
         //
         // - We'll put the attempt as the first argument
         // - We'll add the decorator target as the last argument
-        const handleRetry = (error: unknown, attempt: number): boolean => onRetry?.(attempt, error, target) ?? true
+        //
+        // We'll curry this function so that it can pass the arguments to onRetry
+        const handleRetry =
+            (args: TArgs) =>
+            (error: unknown, attempt: number): boolean =>
+                onRetry?.(attempt, error, target, args) ?? true
 
         // Create the retried method
         const retriedMethod = (...args: TArgs): Promise<TResult> =>
-            backOff(() => originalMethod.apply(target, args), { numOfAttempts: numAttempts, retry: handleRetry })
+            backOff(() => originalMethod.apply(target, args), {
+                // A typical problem in our case is 429 Too many requests
+                // which would still happen if we didn't introduce a bit of randomness into the delay
+                jitter: 'full',
+                maxDelay,
+                numOfAttempts: numAttempts,
+                retry: handleRetry(args),
+            })
 
         // return our new descriptor
         return (descriptor.value = retriedMethod), descriptor
