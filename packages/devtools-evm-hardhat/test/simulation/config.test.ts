097f58672f0e13046ebbef9388ebfff35041e893
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-06-14 09:21:00 -0700

    chore: Only owerwrite simulation network accounts if specified

--- packages/devtools-evm-hardhat/test/simulation/config.test.ts
+++ packages/devtools-evm-hardhat/test/simulation/config.test.ts
@@ -110,42 +110,129 @@ describe('simulation/config', () => {
             ).toStrictEqual({})
         })
 
-        it('should return an object with networks mapped to anvil options if there are http networks', () => {
-            const localhost = hre.config.networks.localhost
-            const networkA: HttpNetworkConfig = { ...localhost, url: 'http://network.a', accounts: [] }
-            const networkB: HttpNetworkConfig = {
-                ...localhost,
-                url: 'http://network.b',
-                accounts: {
-                    count: 10,
-                    initialIndex: 0,
-                    passphrase: '',
-                    path: "m/44'/60'/0'/0/",
-                    mnemonic: 'tomato potato',
-                },
-            }
-            const networkC: HttpNetworkConfig = { ...localhost, url: 'http://network.c', accounts: 'remote' }
-            const simulationConfig = resolveSimulationConfig({}, hre.config)
+        describe('if overrideAccounts is set to false', () => {
+            it('should return an object with networks mapped to anvil options if there are http networks', () => {
+                const localhost = hre.config.networks.localhost
+                const networkA: HttpNetworkConfig = { ...localhost, url: 'http://network.a', accounts: [] }
+                const networkB: HttpNetworkConfig = {
+                    ...localhost,
+                    url: 'http://network.b',
+                    accounts: {
+                        count: 10,
+                        initialIndex: 0,
+                        passphrase: '',
+                        path: "m/44'/60'/0'/0/",
+                        mnemonic: 'tomato potato',
+                    },
+                }
+                const networkC: HttpNetworkConfig = { ...localhost, url: 'http://network.c', accounts: 'remote' }
+                const simulationConfig = resolveSimulationConfig({ overwriteAccounts: false }, hre.config)
 
-            expect(
-                getHardhatNetworkOverrides(simulationConfig, {
-                    networkA,
-                    networkB,
-                    networkC,
+                expect(
+                    getHardhatNetworkOverrides(simulationConfig, {
+                        networkA,
+                        networkB,
+                        networkC,
+                    })
+                ).toStrictEqual({
+                    networkA: {
+                        ...networkA,
+                        url: `http://localhost:${simulationConfig.port}/networkA`,
+                    },
+                    networkB: {
+                        ...networkB,
+                        url: `http://localhost:${simulationConfig.port}/networkB`,
+                    },
+                    networkC: {
+                        ...networkC,
+                        url: `http://localhost:${simulationConfig.port}/networkC`,
+                    },
                 })
-            ).toStrictEqual({
-                networkA: {
-                    ...networkA,
-                    url: `http://localhost:${simulationConfig.port}/networkA`,
-                },
-                networkB: {
-                    ...networkB,
-                    url: `http://localhost:${simulationConfig.port}/networkB`,
-                },
-                networkC: {
-                    ...networkC,
-                    url: `http://localhost:${simulationConfig.port}/networkC`,
-                },
+            })
+        })
+
+        describe('if overrideAccounts is set to true', () => {
+            it('should return an object with networks mapped to anvil options if there are http networks', () => {
+                const localhost = hre.config.networks.localhost
+                const networkA = { ...localhost, url: 'http://network.a' }
+                const networkB = { ...localhost, url: 'http://network.b' }
+                const networkC = { ...localhost, url: 'http://network.c' }
+                const simulationConfig = resolveSimulationConfig({ overwriteAccounts: true }, hre.config)
+
+                expect(
+                    getHardhatNetworkOverrides(simulationConfig, {
+                        networkA,
+                        networkB,
+                        networkC,
+                    })
+                ).toStrictEqual({
+                    networkA: {
+                        ...networkA,
+                        url: `http://localhost:${simulationConfig.port}/networkA`,
+                        accounts: {
+                            count: 10,
+                            initialIndex: 0,
+                            passphrase: '',
+                            path: "m/44'/60'/0'/0/",
+                            mnemonic: simulationConfig.anvil.mnemonic,
+                        },
+                    },
+                    networkB: {
+                        ...networkB,
+                        url: `http://localhost:${simulationConfig.port}/networkB`,
+                        accounts: {
+                            count: 10,
+                            initialIndex: 0,
+                            passphrase: '',
+                            path: "m/44'/60'/0'/0/",
+                            mnemonic: simulationConfig.anvil.mnemonic,
+                        },
+                    },
+                    networkC: {
+                        ...networkC,
+                        url: `http://localhost:${simulationConfig.port}/networkC`,
+                        accounts: {
+                            count: 10,
+                            initialIndex: 0,
+                            passphrase: '',
+                            path: "m/44'/60'/0'/0/",
+                            mnemonic: simulationConfig.anvil.mnemonic,
+                        },
+                    },
+                })
+            })
+
+            it('should respect anvil accounts options', () => {
+                const localhost = hre.config.networks.localhost
+                const networkA = { ...localhost, url: 'http://network.a' }
+                const simulationConfig = resolveSimulationConfig(
+                    {
+                        overwriteAccounts: true,
+                        anvil: {
+                            count: 20,
+                            derivationPath: "m/44'/60'/0'/16/",
+                        },
+                    },
+                    hre.config
+                )
+
+                expect(
+                    getHardhatNetworkOverrides(simulationConfig, {
+                        networkA,
+                    })
+                ).toStrictEqual({
+                    networkA: {
+                        ...networkA,
+                        url: `http://localhost:${simulationConfig.port}/networkA`,
+                        accounts: {
+                            count: simulationConfig.anvil.count,
+                            initialIndex: 0,
+                            passphrase: '',
+                            path: simulationConfig.anvil.derivationPath,
+                            mnemonic: simulationConfig.anvil.mnemonic,
+                        },
+                    },
+                })
             })
         })
     })
