22ad74f241fa804191ff2a50d3ed439927aeb7b6
Ryan Goulding <goulding@layerzerolabs.org>
2024-06-25 13:36:10 -0400

    chore:  default settings for enforced options
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- examples/onft721/layerzero.config.ts
+++ examples/onft721/layerzero.config.ts
@@ -1,6 +1,7 @@
 import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
 
-import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import type { OAppEdgeConfig, OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
 
 const sepoliaContract: OmniPointHardhat = {
     eid: EndpointId.SEPOLIA_V2_TESTNET,
@@ -15,6 +16,24 @@ const fujiContract: OmniPointHardhat = {
 const amoyContract: OmniPointHardhat = {
     eid: EndpointId.AMOY_V2_TESTNET,
     contractName: 'MyONFT721',
+}
+
+const DEFAULT_EDGE_CONFIG: OAppEdgeConfig = {
+    enforcedOptions: [
+        {
+            msgType: 1,
+            optionType: ExecutorOptionType.LZ_RECEIVE,
+            gas: 100_000,
+            value: 0,
+        },
+        {
+            msgType: 2,
+            optionType: ExecutorOptionType.COMPOSE,
+            index: 0,
+            gas: 100_000,
+            value: 0,
+        },
+    ],
 }
 
 const config: OAppOmniGraphHardhat = {
@@ -33,26 +52,32 @@ const config: OAppOmniGraphHardhat = {
         {
             from: fujiContract,
             to: sepoliaContract,
+            config: DEFAULT_EDGE_CONFIG,
         },
         {
             from: fujiContract,
             to: amoyContract,
+            config: DEFAULT_EDGE_CONFIG,
         },
         {
             from: sepoliaContract,
             to: fujiContract,
+            config: DEFAULT_EDGE_CONFIG,
         },
         {
             from: sepoliaContract,
             to: amoyContract,
+            config: DEFAULT_EDGE_CONFIG,
         },
         {
             from: amoyContract,
             to: sepoliaContract,
+            config: DEFAULT_EDGE_CONFIG,
         },
         {
             from: amoyContract,
             to: fujiContract,
+            config: DEFAULT_EDGE_CONFIG,
         },
     ],
 }
