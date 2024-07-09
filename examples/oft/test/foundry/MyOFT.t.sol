c21c3f2f30310e778b7c94a7412ee5103a1f5a16
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2024-07-09 15:29:33 -0700

    🧹 Clean up examples (#682)

--- examples/oft/test/foundry/MyOFT.t.sol
+++ examples/oft/test/foundry/MyOFT.t.sol
@@ -28,15 +28,15 @@ import { TestHelperOz5 } from "@layerzerolabs/test-devtools-evm-foundry/contract
 contract MyOFTTest is TestHelperOz5 {
     using OptionsBuilder for bytes;
 
-    uint32 aEid = 1;
-    uint32 bEid = 2;
+    uint32 private aEid = 1;
+    uint32 private bEid = 2;
 
-    OFTMock aOFT;
-    OFTMock bOFT;
+    OFTMock private aOFT;
+    OFTMock private bOFT;
 
-    address public userA = address(0x1);
-    address public userB = address(0x2);
-    uint256 public initialBalance = 100 ether;
+    address private userA = address(0x1);
+    address private userB = address(0x2);
+    uint256 private initialBalance = 100 ether;
 
     function setUp() public virtual override {
         vm.deal(userA, 1000 ether);
