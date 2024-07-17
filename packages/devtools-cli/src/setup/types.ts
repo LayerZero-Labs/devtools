059fbc84e5ca8c5101994bd521cd8ba0bb6ab0f8
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2024-07-17 10:40:15 -0700

    🧹 CLI: Add type exports to CLI package [2/N] (#704)

--- packages/devtools-cli/src/setup/types.ts
+++
@@ -1,6 +0,0 @@
-import type { OmniSDKFactory, OmniSignerFactory } from '@layerzerolabs/devtools'
-
-export interface CLISetup {
-    createSdk: OmniSDKFactory
-    createSigner: OmniSignerFactory
-}
