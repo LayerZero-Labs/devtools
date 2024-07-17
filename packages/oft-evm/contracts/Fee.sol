04ee9336ef41e82d551a76d582167bd0b9104c7e
Ryan Goulding <goulding@layerzerolabs.org>
2024-07-17 16:53:58 -0400

    Riley/oft fee refactor (#711)
    Co-authored-by: ChadChillBro7 <trileyschwarz@gmail.com>

--- packages/oft-evm/contracts/Fee.sol
+++ packages/oft-evm/contracts/Fee.sol
@@ -4,33 +4,28 @@ pragma solidity ^0.8.20;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-import { IFee } from "./interfaces/IFee.sol";
+import { IFee, FeeConfig } from "./interfaces/IFee.sol";
 
 abstract contract Fee is Ownable {
     uint16 public constant BPS_DENOMINATOR = 10_000;
     uint16 public defaultFeeBps;
     mapping(uint32 dstEid => FeeConfig config) public feeBps;
 
-    struct FeeConfig {
-        uint16 feeBps;
-        bool enabled;
-    }
-
     function setDefaultFeeBps(uint16 _feeBps) external onlyOwner {
         if (_feeBps >= BPS_DENOMINATOR) revert IFee.InvalidBps();
         defaultFeeBps = _feeBps;
+        emit DefaultFeeBpsSet(_feeBps);
     }
 
     function setFeeBps(uint32 _dstEid, uint16 _feeBps, bool _enabled) external onlyOwner {
         if (_feeBps >= BPS_DENOMINATOR) revert IFee.InvalidBps();
         feeBps[_dstEid] = FeeConfig(_feeBps, _enabled);
+        emit FeeBpsSet(_dstEid, _feeBps, _enabled);
     }
 
-    function withdrawFees(address _to) external virtual onlyOwner {
-        uint256 balance = _feeBalance();
-        if (balance > 0) {
-            _transferFrom(address(this), _to, balance);
-        }
+    function getFee(uint32 _dstEid, uint256 _amount) external view virtual returns (uint256) {
+        uint16 feeBps = _getFeeBps(_dstEid);
+        return feeBps == 0 ? 0 : (_amount * feeBps) / BPS_DENOMINATOR;
     }
 
     function _getFeeBps(uint32 _dstEid) internal view returns (uint16) {
@@ -42,8 +37,4 @@ abstract contract Fee is Ownable {
         }
         return 0;
     }
-
-    function _transferFrom(address _from, address _to, uint256 _amount) internal virtual returns (uint256);
-
-    function _feeBalance() internal virtual returns (uint256);
 }
--- packages/oft-evm/contracts/OFTFee.sol
+++ packages/oft-evm/contracts/OFTFee.sol
@@ -13,13 +13,6 @@ import { Fee } from "./Fee.sol";
  * @dev OFT is an ERC-20 token that extends the functionality of the OFTCore contract.
  */
 abstract contract OFTFee is OFT, Fee {
-    /**
-     * @dev Constructor for the OFT contract.
-     * @param _name The name of the OFT.
-     * @param _symbol The symbol of the OFT.
-     * @param _lzEndpoint The LayerZero endpoint address.
-     * @param _delegate The delegate capable of making OApp configurations inside of the endpoint.
-     */
     constructor(
         string memory _name,
         string memory _symbol,
@@ -27,25 +20,32 @@ abstract contract OFTFee is OFT, Fee {
         address _delegate
     ) OFT(_name, _symbol, _lzEndpoint, _delegate) {}
 
-    /**
-     * @notice Indicates whether the OFT contract requires approval of the 'token()' to send.
-     * @return requiresApproval Needs approval of the underlying token implementation.
-     *
-     * @dev In the case of OFT where the contract IS the token, approval is NOT required.
-     */
-    function approvalRequired() external pure virtual override returns (bool) {
-        return true;
+    function withdrawFees(address _to, uint256 _amountLD) public virtual onlyOwner {
+        _transfer(address(this), _to, _amountLD);
     }
 
-    /**
-     * @dev Burns tokens from the sender's specified balance.
-     * @param _from The address to debit the tokens from.
-     * @param _amountLD The amount of tokens to send in local decimals.
-     * @param _minAmountLD The minimum amount to send in local decimals.
-     * @param _dstEid The destination chain ID.
-     * @return amountSentLD The amount sent in local decimals.
-     * @return amountReceivedLD The amount received in local decimals on the remote.
-     */
+    function feeBalance() public virtual returns (uint256) {
+        return IERC20(this).balanceOf(address(this));
+    }
+
+    function _debitView(
+        uint256 _amountLD,
+        uint256 _minAmountLD,
+        uint32 _dstEid
+    ) internal view virtual override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
+        amountSentLD = _amountLD;
+
+        // @dev Apply the fee, then de-dust the amount afterwards.
+        // This means the fee is taken from the amount before the dust is removed.
+        uint256 fee = getFee(_dstEid, _amountLD);
+        amountReceivedLD = _removeDust(_amountLD - fee);
+
+        // @dev Check for slippage.
+        if (amountReceivedLD < _minAmountLD) {
+            revert SlippageExceeded(amountReceivedLD, _minAmountLD);
+        }
+    }
+
     function _debit(
         address _from,
         uint256 _amountLD,
@@ -55,41 +55,8 @@ abstract contract OFTFee is OFT, Fee {
         (amountSentLD, amountReceivedLD) = _debitView(_amountLD, _minAmountLD, _dstEid);
         uint256 fee = amountSentLD - amountReceivedLD;
         if (fee > 0) {
-            _transferFrom(_from, address(this), fee);
+            _transfer(_from, address(this), fee);
         }
         _burn(_from, amountReceivedLD);
-    }
-
-    /**
-     * @dev Internal function to mock the amount mutation from a OFT debit() operation.
-     * @param _amountLD The amount to send in local decimals.
-     * @param _minAmountLD The minimum amount to send in local decimals.
-     * @dev _dstEid The destination LayerZero endpoint ID.
-     * @return amountSentLD The amount sent, in local decimals.
-     * @return amountReceivedLD The amount to be received on the remote chain, in local decimals.
-     */
-    function _debitView(
-        uint256 _amountLD,
-        uint256 _minAmountLD,
-        uint32 _dstEid
-    ) internal view virtual override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
-        uint16 feeBps = _getFeeBps(_dstEid);
-        // @dev Calculate the amount to be received on the remote chain after applying fees and removing dust.
-        amountReceivedLD = _removeDust((_amountLD * (BPS_DENOMINATOR - feeBps)) / BPS_DENOMINATOR);
-        if (amountReceivedLD < _minAmountLD) {
-            revert SlippageExceeded(amountReceivedLD, _minAmountLD);
-        }
-        amountSentLD = (amountReceivedLD * BPS_DENOMINATOR) / (BPS_DENOMINATOR - feeBps);
-    }
-
-    function _feeBalance() internal virtual override returns (uint256) {
-        return IERC20(this).balanceOf(address(this));
-    }
-
-    function _transferFrom(address _from, address _to, uint256 _amount) internal virtual override returns (uint256) {
-        address spender = _msgSender();
-        if (_from != address(this) && _from != spender) _spendAllowance(_from, spender, _amount);
-        _transfer(_from, _to, _amount);
-        return _amount;
     }
 }
