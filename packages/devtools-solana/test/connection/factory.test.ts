01f8d4f271c6d7b61af6848a401c4ea14d25cbc8
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-07-17 14:44:36 -0700

    chore: Add default Solana RPC URL factory

--- packages/devtools-solana/test/connection/factory.test.ts
+++ packages/devtools-solana/test/connection/factory.test.ts
@@ -1,7 +1,9 @@
 import fc from 'fast-check'
-import { createConnectionFactory } from '@/connection/factory'
+import { createConnectionFactory, defaultRpcUrlFactory } from '@/connection/factory'
 import { endpointArbitrary } from '@layerzerolabs/test-devtools'
 import { Connection } from '@solana/web3.js'
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { formatEid } from '@layerzerolabs/devtools'
 
 // We'll use the fake timers to work around the fact that JsonRpcProvider uses setTimeout
 // to schedule a task in the queue in its constructor
@@ -58,6 +60,39 @@ describe('provider/factory', () => {
 
                     expect(connection).toBeInstanceOf(Connection)
                     expect(connection.rpcEndpoint).toBe(url)
+                })
+            )
+        })
+    })
+
+    describe('defaultRpcUrlFactory', () => {
+        it('should return https://api.mainnet-beta.solana.com for SOLANA_V2_MAINNET', () => {
+            expect(defaultRpcUrlFactory(EndpointId.SOLANA_V2_MAINNET)).toBe(`https://api.mainnet-beta.solana.com`)
+        })
+
+        it('should return https://api.mainnet-beta.solana.com for SOLANA_MAINNET', () => {
+            expect(defaultRpcUrlFactory(EndpointId.SOLANA_MAINNET)).toBe(`https://api.mainnet-beta.solana.com`)
+        })
+
+        it('should return https://api.testnet.solana.com for SOLANA_V2_TESTNET', () => {
+            expect(defaultRpcUrlFactory(EndpointId.SOLANA_V2_TESTNET)).toBe(`https://api.testnet.solana.com`)
+        })
+
+        it('should return https://api.testnet.solana.com for SOLANA_TESTNET', () => {
+            expect(defaultRpcUrlFactory(EndpointId.SOLANA_TESTNET)).toBe(`https://api.testnet.solana.com`)
+        })
+
+        it('should throw an error for other endpoints', () => {
+            fc.assert(
+                fc.property(endpointArbitrary, (eid) => {
+                    fc.pre(eid !== EndpointId.SOLANA_V2_MAINNET)
+                    fc.pre(eid !== EndpointId.SOLANA_V2_TESTNET)
+                    fc.pre(eid !== EndpointId.SOLANA_MAINNET)
+                    fc.pre(eid !== EndpointId.SOLANA_TESTNET)
+
+                    expect(() => defaultRpcUrlFactory(eid)).toThrow(
+                        `Could not find a default Solana RPC URL for eid ${eid} (${formatEid(eid)})`
+                    )
                 })
             )
         })
