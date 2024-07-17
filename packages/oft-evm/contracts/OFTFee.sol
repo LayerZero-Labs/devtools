b4db2e085df3970b370c3b26d8a77ef7f9b49ee4
ChadChillBro7 <trileyschwarz@gmail.com>
2024-07-17 15:55:51 -0700

    Fix

--- packages/oft-evm/contracts/OFTFee.sol
+++ packages/oft-evm/contracts/OFTFee.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.20;
 
-import { ERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IFee } from "./interfaces/IFee.sol";
 import { OFTCore } from "./OFTCore.sol";
 import { OFT } from "./OFT.sol";
--- packages/oft-evm/contracts/OFTFeeAdapter.sol
+++ packages/oft-evm/contracts/OFTFeeAdapter.sol
@@ -12,19 +12,42 @@ abstract contract OFTFeeAdapter is OFTAdapter, Fee {
 
     uint256 public feeBalance;
 
-    constructor(address _token, address _lzEndpoint, address _delegate) OFTAdapter(_token, _lzEndpoint, _delegate) {}
+    error NoFeesToWithdraw();
+    event FeeWithdrawn(address indexed to, uint256 amountLD);
 
-    /**
-     * @dev Locks tokens from the sender's specified balance in this contract.
-     * @param _from The address to debit from.
-     * @param _amountLD The amount of tokens to send in local decimals.
-     * @param _minAmountLD The minimum amount to send in local decimals.
-     * @param _dstEid The destination chain ID.
-     * @return amountSentLD The amount sent in local decimals.
-     * @return amountReceivedLD The amount received in local decimals on the remote.
-     *
-     * @dev msg.sender will need to approve this _amountLD of tokens to be locked inside of the contract.
-     */
+    constructor(
+        address _token,
+        address _lzEndpoint,
+        address _delegate
+    ) OFTAdapter(_token, _lzEndpoint, _delegate) {}
+
+    function withdrawFees(address _to) external override onlyOwner {
+        uint256 balance = feeBalance;
+        if (balance == 0) revert NoFeesToWithdraw();
+
+        feeBalance = 0;
+        innerToken.safeTransferFrom(address(this), _to, balance);
+        emit FeeWithdrawn(_to, balance);
+    }
+
+    function _debitView(
+        uint256 _amountLD,
+        uint256 _minAmountLD,
+        uint32 _dstEid
+    ) internal view virtual override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
+        amountSentLD = _amountLD;
+
+        // @dev Apply the fee, then de-dust the amount afterwards.
+        // This means the fee is taken from the amount before the dust is removed.
+        uint256 fee = getFee(_dstEid, _amountLD);
+        amountReceivedLD = _removeDust(_amountLD - fee);
+
+        // @dev Check for slippage.
+        if (amountReceivedLD < _minAmountLD) {
+            revert SlippageExceeded(amountReceivedLD, _minAmountLD);
+        }
+    }
+
     function _debit(
         address _from,
         uint256 _amountLD,
@@ -33,38 +56,9 @@ abstract contract OFTFeeAdapter is OFTAdapter, Fee {
     ) internal virtual override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
         (amountSentLD, amountReceivedLD) = _debitView(_amountLD, _minAmountLD, _dstEid);
         innerToken.safeTransferFrom(_from, address(this), amountSentLD);
-        feeBalance += (amountSentLD - amountReceivedLD);
-    }
 
-    function _debitView(
-        uint256 _amountLD,
-        uint256 _minAmountLD,
-        uint32 _dstEid
-    ) internal view virtual override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
-        uint16 feeBps = _getFeeBps(_dstEid);
-        // @dev Calculate the amount to be received on the remote chain after applying fees and removing dust.
-        amountReceivedLD = _removeDust((_amountLD * (BPS_DENOMINATOR - feeBps)) / BPS_DENOMINATOR);
-        if (amountReceivedLD < _minAmountLD) {
-            revert SlippageExceeded(amountReceivedLD, _minAmountLD);
+        if (amountSentLD > amountReceivedLD) {
+            feeBalance += (amountSentLD - amountReceivedLD);
         }
-        amountSentLD = (amountReceivedLD * BPS_DENOMINATOR) / (BPS_DENOMINATOR - feeBps);
-    }
-
-    function withdrawFees(address _to) external virtual onlyOwner {
-        uint256 balance = feeBalance;
-        feeBalance = 0;
-        if (balance > 0) {
-            _transferFrom(address(this), _to, balance);
-        }
-    }
-
-    function _transferFrom(address _from, address _to, uint256 _amount) internal virtual returns (uint256) {
-        uint256 before = innerToken.balanceOf(_to);
-        if (_from == address(this)) {
-            innerToken.safeTransfer(_to, _amount);
-        } else {
-            innerToken.safeTransferFrom(_from, _to, _amount);
-        }
-        return innerToken.balanceOf(_to) - before;
     }
 }
