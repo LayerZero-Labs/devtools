a1373f51788835dda391f3f5bbe386fedc10cf45
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-07-18 15:42:06 -0700

    chore: Add fee payer & latest block hash to the transaction

--- packages/devtools-solana/src/transactions/signer.ts
+++ packages/devtools-solana/src/transactions/signer.ts
@@ -34,9 +34,6 @@ export class OmniSignerSolana extends OmniSignerBase implements OmniSigner {
 
         const solanaTransaction = deserializeTransactionMessage(transaction.data)
 
-        // Transactions in Solana require a fee payer
-        solanaTransaction.feePayer = this.signer.publicKey
-
         const signature = await sendAndConfirmTransaction(
             this.connection,
             solanaTransaction,
@@ -52,59 +49,3 @@ export class OmniSignerSolana extends OmniSignerBase implements OmniSigner {
         }
     }
 }
-
-// export const createSignerFactory = (
-//     definition?: SignerDefinition,
-//     connectionFactory = createConnectionFactory()
-// ): OmniSignerFactory<OmniSignerEVM> => {
-//     return pMemoize(async (eid) => {
-//         const provider = await providerFactory(eid)
-//         const addressOrIndex = await signerAddressorIndexFactory(eid)
-//         const signer = provider.getSigner(addressOrIndex)
-
-//         return new OmniSignerEVM(eid, signer)
-//     })
-// }
-
-// /**
-//  * Factory for signer address/index for a specific eid.
-//  *
-//  * Will take an optional signer definition and either:
-//  *
-//  * - Return static signer address or index for static signer configuration
-//  * - Look up named signer account in hardhat config and return its address
-//  *
-//  * @param {SignerDefinition} [definition]
-//  * @param {EndpointBasedFactory<HardhatRuntimeEnvironment>} [networkEnvironmentFactory]
-//  * @returns
-//  */
-// export const createSignerAddressOrIndexFactory =
-//     (
-//         definition?: SignerDefinition,
-//         networkEnvironmentFactory = createGetHreByEid()
-//     ): EndpointBasedFactory<string | number | undefined> =>
-//     async (eid) => {
-//         // If there is no definition provided, we return nothing
-//         if (definition == null) {
-//             return undefined
-//         }
-
-//         // The hardcoded address and/or index definitions are easy,
-//         // they need no resolution and can be used as they are
-//         if (definition.type === 'address') {
-//             return definition.address
-//         }
-
-//         if (definition.type === 'index') {
-//             return definition.index
-//         }
-
-//         // The named definitions need to be resolved using hre
-//         const hre = await networkEnvironmentFactory(eid)
-//         const accounts = await hre.getNamedAccounts()
-//         const account = accounts[definition.name]
-
-//         assert(account != null, `Missing named account '${definition.name}' for eid ${formatEid(eid)}`)
-
-//         return account
-//     }
