e7dede53302aa4fe2388360509e5322c2ef4045d
Matthew Krak <krak@layerzerolabs.org>
2024-08-09 11:08:32 -0700

    added index to hardhat

--- examples/oft-solana/hardhat.config.ts
+++ examples/oft-solana/hardhat.config.ts
@@ -14,6 +14,8 @@ import { HardhatUserConfig, HttpNetworkAccountsUserConfig } from 'hardhat/types'
 
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 
+import './tasks/index'
+
 // Set your preferred authentication method
 //
 // If you prefer using a mnemonic, set a MNEMONIC environment variable
