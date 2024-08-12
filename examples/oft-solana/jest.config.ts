80f69e3aec837d32b55812844508f8a0a0cc5414
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-08-12 08:37:39 -0700

    chore: Move test regex from jest config to anchor.toml

--- examples/oft-solana/jest.config.ts
+++ examples/oft-solana/jest.config.ts
@@ -2,7 +2,6 @@
 module.exports = {
     reporters: [['github-actions', { silent: false }], 'default'],
     testEnvironment: 'node',
-    testRegex: '(/test/anchor/.*\\.test\\.(ts|tsx)$)',
     testTimeout: 15000,
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
