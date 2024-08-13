d5e6dc37cf4b6cb16756723110bb770c6f5c4368
ravinagill15 <ravina@layerzerolabs.org>
2024-08-13 15:54:15 -0700

    added token and approvalRequired, added comment, reverted super for now

--- packages/oft-evm/contracts/NativeOFTAdapter.sol
+++ packages/oft-evm/contracts/NativeOFTAdapter.sol
@@ -14,6 +14,8 @@ import { MessagingFee, MessagingReceipt, OFTCore, OFTReceipt, SendParam } from "
  */
 abstract contract NativeOFTAdapter is OFTCore {
 
+    address public constant NATIVE_TOKEN_ADDRESS = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+
     error InsufficientMessageValue(uint256 provided, uint256 required);
     error CreditFailed();
 
@@ -31,7 +33,6 @@ abstract contract NativeOFTAdapter is OFTCore {
 
     /**
      * @dev Locks native sent by the sender as msg.value
-     * @param _from The address to debit from.
      * @param _amountLD The amount of native to send in local decimals.
      * @param _minAmountLD The minimum amount to send in local decimals.
      * @param _dstEid The destination chain ID.
@@ -39,7 +40,7 @@ abstract contract NativeOFTAdapter is OFTCore {
      * @return amountReceivedLD The amount received in local decimals on the remote.
      */
     function _debit(
-        address _from,
+        address /*_from*/,
         uint256 _amountLD,
         uint256 _minAmountLD,
         uint32 _dstEid
@@ -75,15 +76,52 @@ abstract contract NativeOFTAdapter is OFTCore {
         SendParam calldata _sendParam,
         MessagingFee calldata _fee,
         address _refundAddress
-    ) external payable override returns (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt) {
+    ) external payable virtual override returns (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt) {
         if (_fee.nativeFee + _sendParam.amountLD != msg.value) {
             revert NotEnoughNative(msg.value);
         }
 
-        return super.send(_sendParam, _fee, _refundAddress);
+        // return super.send(_sendParam, _fee, _refundAddress); // TODO fix this later -- doesn't seem to recognize OFTCore.send
+
+        // @dev Applies the token transfers regarding this send() operation.
+        // - amountSentLD is the amount in local decimals that was ACTUALLY sent/debited from the sender.
+        // - amountReceivedLD is the amount in local decimals that will be received/credited to the recipient on the remote OFT instance.
+        (uint256 amountSentLD, uint256 amountReceivedLD) = _debit(
+            msg.sender,
+            _sendParam.amountLD,
+            _sendParam.minAmountLD,
+            _sendParam.dstEid
+        );
+
+        // @dev Builds the options and OFT message to quote in the endpoint.
+        (bytes memory message, bytes memory options) = _buildMsgAndOptions(_sendParam, amountReceivedLD);
+
+        // @dev Sends the message to the LayerZero endpoint and returns the LayerZero msg receipt.
+        msgReceipt = _lzSend(_sendParam.dstEid, message, options, _fee, _refundAddress);
+        // @dev Formulate the OFT receipt.
+        oftReceipt = OFTReceipt(amountSentLD, amountReceivedLD);
+
+        emit OFTSent(msgReceipt.guid, _sendParam.dstEid, msg.sender, amountSentLD, amountReceivedLD);
     }
 
-    function _payNative(uint256 _nativeFee) internal override returns (uint256 nativeFee) {
-        return _nativeFee;
+    /**
+     * @dev Returns the address of the native token
+     * @return The address of the native token.
+     */
+    function token() public pure returns (address) {
+        return NATIVE_TOKEN_ADDRESS;
     }
+
+    /**
+     * @notice Indicates whether the OFT contract requires approval of the 'token()' to send.
+     * @return requiresApproval Needs approval of the underlying token implementation.
+     *
+     * @dev In the case of default NativeOFTAdapter, approval is not required.
+     */
+    function approvalRequired() external pure virtual returns (bool) {
+        return false;
+    }
+
+    // @dev Overridden to be empty as this assertion is done higher up on the send function.
+    function _payNative(uint256 _nativeFee) internal override returns (uint256 nativeFee) {}
 }
