0ccd6cda8b6b183145187771c0d956491fbe8489
Ryan Goulding <goulding@layerzerolabs.org>
2024-08-15 14:41:07 -0400

    🪲  sk decoding is corrected (#850)
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- examples/oft-solana/tasks/common/types.ts
+++ examples/oft-solana/tasks/common/types.ts
@@ -1,11 +1,11 @@
-import bs58 from '@coral-xyz/anchor/dist/cjs/utils/bytes/bs58'
+import { decode } from '@coral-xyz/anchor/dist/cjs/utils/bytes/bs58'
 import { Keypair, PublicKey } from '@solana/web3.js'
 import { CLIArgumentType } from 'hardhat/types'
 
 export const keyPair: CLIArgumentType<Keypair> = {
     name: 'keyPair',
     parse(name: string, value: string) {
-        return Keypair.fromSecretKey(bs58.decode(value))
+        return Keypair.fromSecretKey(decode(value))
     },
     validate() {},
 }
