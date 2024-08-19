6fff5950395192a37a9a51fbd9f323713ffc0e97
ravinagill15 <ravina@layerzerolabs.org>
2024-08-19 11:59:47 -0700

    test package

---
+++ tests/ua-devtools-evm-foundry-test/test/ReceiveConfig.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+// Forge imports
+import "forge-std/console.sol";
+import { Test } from "forge-std/Test.sol";
+
+contract ReceiveConfigTest is Test {
+
+    function test_receiveConfig() public {
+        console.log("ReceiveConfigTest.test_receiveConfig");
+    }
+}
