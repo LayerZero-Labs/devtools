ad8383a3b33ed97ca83906e44a64c47a8015361b
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-08-20 13:55:13 -0700

    fix: Tests

--- packages/ua-devtools-evm/test/erc20/sdk.test.ts
+++ packages/ua-devtools-evm/test/erc20/sdk.test.ts
@@ -1,41 +1,21 @@
 import fc from 'fast-check'
-import { endpointArbitrary, evmAddressArbitrary } from '@layerzerolabs/test-devtools'
-import { Contract } from '@ethersproject/contracts'
+import { evmAddressArbitrary, pointArbitrary } from '@layerzerolabs/test-devtools'
 import { ERC20 } from '@/erc20/sdk'
-import { OmniContract } from '@layerzerolabs/devtools-evm'
-import { BigNumber } from '@ethersproject/bignumber/lib/bignumber'
+import { JsonRpcProvider } from '@ethersproject/providers'
 
 describe('erc20/sdk', () => {
-    const jestFunctionArbitrary = fc.anything().map(() => jest.fn())
-
-    const erc20ContractArbitrary = fc.record({
-        address: evmAddressArbitrary,
-        name: jestFunctionArbitrary,
-        decimals: jestFunctionArbitrary,
-        symbol: jestFunctionArbitrary,
-        allowance: jestFunctionArbitrary,
-        balanceOf: jestFunctionArbitrary,
-        interface: fc.record({
-            encodeFunctionData: jestFunctionArbitrary,
-        }),
-    }) as fc.Arbitrary<unknown> as fc.Arbitrary<Contract>
-
-    const omniContractArbitrary: fc.Arbitrary<OmniContract> = fc.record({
-        eid: endpointArbitrary,
-        contract: erc20ContractArbitrary,
-    })
-
     describe('getName', () => {
         it('should return the contract name', async () => {
             await fc.assert(
-                fc.asyncProperty(omniContractArbitrary, fc.string(), async (omniContract, name) => {
-                    omniContract.contract.name.mockResolvedValue(name)
+                fc.asyncProperty(pointArbitrary, fc.string(), async (point, name) => {
+                    const provider = new JsonRpcProvider()
+                    const sdk = new ERC20(provider, point)
 
-                    const sdk = new ERC20(omniContract)
+                    jest.spyOn(provider, 'call').mockResolvedValue(
+                        sdk.contract.contract.interface.encodeFunctionResult('name', [name])
+                    )
 
                     await expect(sdk.getName()).resolves.toBe(name)
-
-                    expect(omniContract.contract.name).toHaveBeenCalledTimes(1)
                 })
             )
         })
@@ -44,14 +24,15 @@ describe('erc20/sdk', () => {
     describe('getSymbol', () => {
         it('should return the contract symbol', async () => {
             await fc.assert(
-                fc.asyncProperty(omniContractArbitrary, fc.string(), async (omniContract, symbol) => {
-                    omniContract.contract.symbol.mockResolvedValue(symbol)
+                fc.asyncProperty(pointArbitrary, fc.string(), async (point, symbol) => {
+                    const provider = new JsonRpcProvider()
+                    const sdk = new ERC20(provider, point)
 
-                    const sdk = new ERC20(omniContract)
+                    jest.spyOn(provider, 'call').mockResolvedValue(
+                        sdk.contract.contract.interface.encodeFunctionResult('symbol', [symbol])
+                    )
 
                     await expect(sdk.getSymbol()).resolves.toBe(symbol)
-
-                    expect(omniContract.contract.symbol).toHaveBeenCalledTimes(1)
                 })
             )
         })
@@ -60,14 +41,15 @@ describe('erc20/sdk', () => {
     describe('getDecimals', () => {
         it('should return the contract decimals', async () => {
             await fc.assert(
-                fc.asyncProperty(omniContractArbitrary, fc.integer({ min: 1 }), async (omniContract, decimals) => {
-                    omniContract.contract.decimals.mockResolvedValue(decimals)
+                fc.asyncProperty(pointArbitrary, fc.integer({ min: 1, max: 255 }), async (point, decimals) => {
+                    const provider = new JsonRpcProvider()
+                    const sdk = new ERC20(provider, point)
 
-                    const sdk = new ERC20(omniContract)
+                    jest.spyOn(provider, 'call').mockResolvedValue(
+                        sdk.contract.contract.interface.encodeFunctionResult('decimals', [decimals])
+                    )
 
                     await expect(sdk.getDecimals()).resolves.toBe(decimals)
-
-                    expect(omniContract.contract.decimals).toHaveBeenCalledTimes(1)
                 })
             )
         })
@@ -77,18 +59,18 @@ describe('erc20/sdk', () => {
         it('should return the user balance', async () => {
             await fc.assert(
                 fc.asyncProperty(
-                    omniContractArbitrary,
+                    pointArbitrary,
                     evmAddressArbitrary,
                     fc.bigInt({ min: BigInt(0) }),
-                    async (omniContract, user, balance) => {
-                        omniContract.contract.balanceOf.mockResolvedValue(BigNumber.from(balance))
+                    async (point, user, balance) => {
+                        const provider = new JsonRpcProvider()
+                        const sdk = new ERC20(provider, point)
 
-                        const sdk = new ERC20(omniContract)
+                        jest.spyOn(provider, 'call').mockResolvedValue(
+                            sdk.contract.contract.interface.encodeFunctionResult('balanceOf', [balance])
+                        )
 
                         await expect(sdk.getBalanceOf(user)).resolves.toBe(balance)
-
-                        expect(omniContract.contract.balanceOf).toHaveBeenCalledTimes(1)
-                        expect(omniContract.contract.balanceOf).toHaveBeenCalledWith(user)
                     }
                 )
             )
@@ -99,19 +81,19 @@ describe('erc20/sdk', () => {
         it('should return the user balance', async () => {
             await fc.assert(
                 fc.asyncProperty(
-                    omniContractArbitrary,
+                    pointArbitrary,
                     evmAddressArbitrary,
                     evmAddressArbitrary,
                     fc.bigInt({ min: BigInt(0) }),
-                    async (omniContract, owner, spender, allowance) => {
-                        omniContract.contract.allowance.mockResolvedValue(BigNumber.from(allowance))
+                    async (point, owner, spender, allowance) => {
+                        const provider = new JsonRpcProvider()
+                        const sdk = new ERC20(provider, point)
 
-                        const sdk = new ERC20(omniContract)
+                        jest.spyOn(provider, 'call').mockResolvedValue(
+                            sdk.contract.contract.interface.encodeFunctionResult('allowance', [allowance])
+                        )
 
                         await expect(sdk.getAllowance(owner, spender)).resolves.toBe(allowance)
-
-                        expect(omniContract.contract.allowance).toHaveBeenCalledTimes(1)
-                        expect(omniContract.contract.allowance).toHaveBeenCalledWith(owner, spender)
                     }
                 )
             )
