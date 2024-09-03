b0ec17702427d43d17e4efa1342bcfb071f9a3e2
ravinagill15 <ravina@layerzerolabs.org>
2024-09-03 13:53:13 -0700

    adding ethers as external to fix build

--- packages/devtools-evm-hardhat/tsup.config.ts
+++ packages/devtools-evm-hardhat/tsup.config.ts
@@ -10,6 +10,7 @@ export default defineConfig([
         splitting: false,
         treeshake: true,
         format: ['esm', 'cjs'],
+        external: ['ethers'],
         loader: {
             '.conf': 'text',
         },
