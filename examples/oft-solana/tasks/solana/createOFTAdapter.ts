42b0a89ffe29458e5f26a6fc538ecdaa2dd40854
Ryan Goulding <goulding@layerzerolabs.org>
2024-10-16 14:57:43 -0400

    feat: support token2022 standard
    Also significantly cleans by factoring out common code, deleting unneeded
    code, and centralizing on UMI for transaction processing.
    
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- examples/oft-solana/tasks/solana/createOFTAdapter.ts
+++ examples/oft-solana/tasks/solana/createOFTAdapter.ts
@@ -1,152 +1,79 @@
-import assert from 'assert'
-import { existsSync, mkdirSync, writeFileSync } from 'node:fs'
-
-import { mplToolbox } from '@metaplex-foundation/mpl-toolbox'
-import { EddsaInterface, createSignerFromKeypair, publicKey, signerIdentity } from '@metaplex-foundation/umi'
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
-import { createWeb3JsEddsa } from '@metaplex-foundation/umi-eddsa-web3js'
-import { toWeb3JsInstruction, toWeb3JsKeypair, toWeb3JsPublicKey } from '@metaplex-foundation/umi-web3js-adapters'
-import {
-    TOKEN_PROGRAM_ID,
-    createInitializeMintInstruction,
-    createMintToInstruction,
-    createMultisig,
-    getMintLen,
-    getOrCreateAssociatedTokenAccount,
-} from '@solana/spl-token'
-import { SystemProgram } from '@solana/web3.js'
+import { createSignerFromKeypair, publicKey, transactionBuilder } from '@metaplex-foundation/umi'
+import { toWeb3JsKeypair, toWeb3JsPublicKey } from '@metaplex-foundation/umi-web3js-adapters'
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import bs58 from 'bs58'
-import { sha256 } from 'ethereumjs-util'
 import { task } from 'hardhat/config'
 
 import { types as devtoolsTypes } from '@layerzerolabs/devtools-evm-hardhat'
-import { EndpointId, endpointIdToNetwork } from '@layerzerolabs/lz-definitions'
-import { OFT_DECIMALS, OftPDA, oft, types } from '@layerzerolabs/oft-v2-solana-sdk'
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { OFT_DECIMALS, oft, types } from '@layerzerolabs/oft-v2-solana-sdk'
 
-import { createSolanaConnectionFactory } from '../common/utils'
+import { createMintAuthorityMultisig } from './multisig'
 
-import { sendAndConfirmTx } from './index'
+import { deriveConnection, deriveKeys, getExplorerTxLink, output } from './index'
 
-const LOCAL_DECIMALS = 9
-
-interface Args {
+interface CreateOFTAdapterTaskArgs {
+    /**
+     * The endpoint ID for the Solana network.
+     */
     eid: EndpointId
+
+    /**
+     * The token mint public key.
+     */
+    mint: string
+
+    /**
+     * The OFT Program id.
+     */
     programId: string
-    oftName: string
-    amount: bigint
+
+    /**
+     * The Token Program public key.
+     */
+    tokenProgram: string
 }
 
+// Define a Hardhat task for creating OFTAdapter on Solana
 task('lz:oft-adapter:solana:create', 'Mints new SPL Token, Lockbox, and new OFT Adapter Store account')
+    .addParam('mint', 'The Token Mint public key')
     .addParam('programId', 'The OFT Program id')
     .addParam('eid', 'Solana mainnet or testnet', undefined, devtoolsTypes.eid)
-    .addParam('oftName', 'oft name', undefined, devtoolsTypes.string, true)
-    .addParam('amount', 'amount', undefined, devtoolsTypes.bigint)
-    .setAction(async (taskArgs: Args) => {
-        const privateKey = process.env.SOLANA_PRIVATE_KEY
-        assert(!!privateKey, 'SOLANA_PRIVATE_KEY is not defined in the environment variables.')
-        const connectionFactory = createSolanaConnectionFactory()
-        const connection = await connectionFactory(taskArgs.eid)
-        const umi = createUmi(connection.rpcEndpoint).use(mplToolbox())
-        const umiWalletKeyPair = umi.eddsa.createKeypairFromSecretKey(bs58.decode(privateKey))
-        const umiWalletSigner = createSignerFromKeypair(umi, umiWalletKeyPair)
-        umi.use(signerIdentity(umiWalletSigner))
-        const web3WalletKeyPair = toWeb3JsKeypair(umiWalletKeyPair)
-
-        const programId = publicKey(taskArgs.programId)
-        const eddsa: EddsaInterface = createWeb3JsEddsa()
-        const oftDeriver = new OftPDA(programId)
-        const oftName = taskArgs.oftName ?? taskArgs.programId
-
-        const lockBox = eddsa.createKeypairFromSeed(sha256(Buffer.from(`${oftName}-v2-lockbox`, 'utf-8')))
-        const escrowPK = lockBox.publicKey
-        const [oftStorePda] = oftDeriver.oftStore(escrowPK)
-        const mintKp = eddsa.createKeypairFromSeed(sha256(Buffer.from(`${oftName}-v2`, 'utf-8')))
-        const mintPK = mintKp.publicKey
-
-        const multiSigKey = await createMultisig(
-            connection,
-            toWeb3JsKeypair(umiWalletKeyPair),
-            [toWeb3JsPublicKey(oftStorePda), toWeb3JsPublicKey(umiWalletKeyPair.publicKey)],
-            1,
-            undefined,
-            undefined,
-            TOKEN_PROGRAM_ID
-        )
-
-        const createMintIxs = [
-            SystemProgram.createAccount({
-                fromPubkey: toWeb3JsPublicKey(umiWalletKeyPair.publicKey),
-                newAccountPubkey: toWeb3JsPublicKey(mintPK),
-                space: getMintLen([]),
-                lamports: await connection.getMinimumBalanceForRentExemption(getMintLen([])),
-                programId: TOKEN_PROGRAM_ID,
-            }),
-            createInitializeMintInstruction(
-                toWeb3JsPublicKey(mintPK),
-                LOCAL_DECIMALS,
-                multiSigKey,
-                multiSigKey,
-                TOKEN_PROGRAM_ID
-            ),
-        ]
-        const initOftIx = oft.initOft(
-            {
-                payer: createSignerFromKeypair({ eddsa: eddsa }, umiWalletKeyPair),
-                admin: umiWalletKeyPair.publicKey,
-                mint: mintPK,
-                escrow: createSignerFromKeypair({ eddsa: eddsa }, lockBox),
-            },
-            types.OFTType.Adapter,
-            OFT_DECIMALS,
-            {
-                oft: programId,
-            }
-        )
-        const ixs = [...createMintIxs, toWeb3JsInstruction(initOftIx.instruction)]
-        const signers = [umiWalletKeyPair, mintKp, lockBox]
-
-        const txResult = await sendAndConfirmTx(connection, signers, ixs)
-        console.log(`initOFT transaction hash: ${txResult.hash}`)
-
-        const ata = await getOrCreateAssociatedTokenAccount(
-            connection, // Solana connection
-            web3WalletKeyPair, // The payer for the transaction (typically the wallet's keypair)
-            toWeb3JsPublicKey(mintPK), // SPL Token mint address
-            web3WalletKeyPair.publicKey // The wallet for which you're getting or creating the ATA
-        )
-
-        if (taskArgs.amount && taskArgs.amount > 0) {
-            const mintIx = createMintToInstruction(
-                toWeb3JsPublicKey(mintPK),
-                toWeb3JsPublicKey(umiWalletSigner.publicKey),
-                multiSigKey,
-                taskArgs.amount,
-                [toWeb3JsKeypair(umiWalletKeyPair)]
+    .addParam('tokenProgram', 'The Token Program public key', TOKEN_PROGRAM_ID.toBase58(), devtoolsTypes.string, true)
+    .setAction(
+        async ({
+            eid,
+            mint: mintStr,
+            programId: programIdStr,
+            tokenProgram: tokenProgramStr,
+        }: CreateOFTAdapterTaskArgs) => {
+            const { connection, umi, umiWalletKeyPair } = await deriveConnection(eid)
+            const { programId, lockBox, escrowPK, oftStorePda, eddsa } = deriveKeys(programIdStr)
+            const tokenProgram = publicKey(tokenProgramStr)
+            const mint = publicKey(mintStr)
+            const mintAuthorityPublicKey = await createMintAuthorityMultisig(
+                connection,
+                toWeb3JsKeypair(umiWalletKeyPair),
+                toWeb3JsPublicKey(oftStorePda),
+                toWeb3JsPublicKey(tokenProgram)
             )
-            const txResult = await sendAndConfirmTx(connection, umiWalletKeyPair, [mintIx])
-            console.log(`Minted ${taskArgs.amount} tokens to ${ata.address.toBase58()} ${txResult.hash}`)
-        }
-
-        const outputDir = `./deployments/${endpointIdToNetwork(taskArgs.eid)}`
-        if (!existsSync(outputDir)) {
-            mkdirSync(outputDir, { recursive: true })
-        }
-
-        // Write the JSON file to the specified directory
-        writeFileSync(
-            `${outputDir}/OFT.json`,
-            JSON.stringify(
+            const initOftIx = oft.initOft(
                 {
-                    oftName,
-                    programId: taskArgs.programId,
-                    mint: mintPK,
-                    escrow: escrowPK,
-                    oftStore: oftStorePda,
-                    ata: ata.address,
+                    payer: createSignerFromKeypair({ eddsa: eddsa }, umiWalletKeyPair),
+                    admin: umiWalletKeyPair.publicKey,
+                    mint: mint,
+                    escrow: createSignerFromKeypair({ eddsa: eddsa }, lockBox),
                 },
-                null,
-                4
+                types.OFTType.Adapter,
+                OFT_DECIMALS,
+                {
+                    oft: programId,
+                    token: tokenProgram ? publicKey(tokenProgram) : undefined,
+                }
             )
-        )
-        console.log(`Accounts have been saved to ${outputDir}/OFT.json`)
-    })
+            const { signature } = await transactionBuilder().add(initOftIx).sendAndConfirm(umi)
+            console.log(`initOftTx: ${getExplorerTxLink(bs58.encode(signature), eid == EndpointId.SOLANA_V2_TESTNET)}`)
+
+            output(eid, programIdStr, mint, mintAuthorityPublicKey.toBase58(), escrowPK, oftStorePda)
+        }
+    )
