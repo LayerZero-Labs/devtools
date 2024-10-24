dc22d8e7cdfdc35542bd579c07e7cc535121568e
alexanderliteplo <alexanderliteplo@gmail.com>
2024-10-24 22:55:28 +0000

    setting up skeleton for ua-devtools-initia package

---
+++ packages/ua-devtools-initia/src/index.ts
@@ -0,0 +1,3 @@
+export const hello = () => {
+    return 'Hello from ua-devtools-initia'
+}
---
+++ packages/ua-devtools-initia/test/index.test.ts
@@ -0,0 +1,7 @@
+import { hello } from '../src'
+
+describe('ua-devtools-initia', () => {
+    it('should work', () => {
+        expect(hello()).toBe('Hello from ua-devtools-initia')
+    })
+})
