6e19b0c3e419aed4185942575813ff1443edb475
Ryan Goulding <goulding@layerzerolabs.org>
2024-10-29 15:39:23 -0400

    fix:  cache inspector to avoid a double storage read
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- packages/oft-evm-upgradeable/contracts/oft/OFTCoreUpgradeable.sol
+++ packages/oft-evm-upgradeable/contracts/oft/OFTCoreUpgradeable.sol
@@ -251,7 +251,8 @@ abstract contract OFTCoreUpgradeable is
 
         // @dev Optionally inspect the message and options depending if the OApp owner has set a msg inspector.
         // @dev If it fails inspection, needs to revert in the implementation. ie. does not rely on return boolean
-        if ($.msgInspector != address(0)) IOAppMsgInspector($.msgInspector).inspect(message, options);
+        address inspector = $.msgInspector; // caches the msgInspector to avoid potential double storage read
+        if (inspector != address(0)) IOAppMsgInspector(inspector).inspect(message, options);
     }
 
     /**
