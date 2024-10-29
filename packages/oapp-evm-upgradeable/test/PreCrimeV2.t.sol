aa8f3b1026b56e27b2debcbbbdaca9057c177b25
Ryan Goulding <goulding@layerzerolabs.org>
2024-10-29 15:29:28 -0400

    fix:  ensure packets are sorted
    Also updates the tests to ensure this constraint is upheld.
    
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- packages/oapp-evm-upgradeable/test/PreCrimeV2.t.sol
+++ packages/oapp-evm-upgradeable/test/PreCrimeV2.t.sol
@@ -17,6 +17,16 @@ import { PreCrimeV2SimulatorUpgradeableMock } from "./mocks/PreCrimeV2SimulatorU
 import "forge-std/console.sol";
 
 contract PreCrimeV2Test is TestHelperOz5 {
+    uint8 constant DEFAULT_NUM_PEERS = 3;
+    uint64 constant DEFAULT_MAX_BATCH_SIZE = 4;
+
+    uint32 constant EID_B = 2;
+    uint32 constant EID_C = 3;
+    bytes32 constant SENDER_B = 0x0000000000000000000000000000000000000000000000000000000000000002;
+    bytes32 constant SENDER_C = 0x0000000000000000000000000000000000000000000000000000000000000003;
+
+    uint32 constant DST_EID = 1;
+
     uint16 constant CONFIG_VERSION = 2;
     uint64 constant MAX_BATCH_SIZE = 4;
     address constant OFF_CHAIN = address(0xDEAD);
@@ -27,33 +37,6 @@ contract PreCrimeV2Test is TestHelperOz5 {
     PreCrimePeer[] preCrimePeers;
 
     address public proxyAdmin = makeAddr("proxyAdmin");
-
-    function setUp() public override {
-        super.setUp();
-
-        setUpEndpoints(1, LibraryType.SimpleMessageLib);
-
-        simulator = PreCrimeV2SimulatorUpgradeableMock(
-            _deployContractAndProxy(
-                type(PreCrimeV2SimulatorUpgradeableMock).creationCode,
-                new bytes(0),
-                abi.encodeWithSelector(PreCrimeV2SimulatorUpgradeableMock.initialize.selector, address(this))
-            )
-        );
-        preCrime = PreCrimeV2UpgradeableMock(
-            _deployContractAndProxy(
-                type(PreCrimeV2UpgradeableMock).creationCode,
-                abi.encode(address(endpoints[1]), address(simulator)),
-                abi.encodeWithSelector(PreCrimeV2UpgradeableMock.initialize.selector, address(this))
-            )
-        );
-
-        preCrimePeers.push(PreCrimePeer(2, bytes32(uint256(22)), bytes32(uint256(2))));
-        preCrimePeers.push(PreCrimePeer(3, bytes32(uint256(33)), bytes32(uint256(3))));
-
-        preCrime.setPreCrimePeers(preCrimePeers);
-        preCrime.setMaxBatchSize(MAX_BATCH_SIZE);
-    }
 
     function _deployContractAndProxy(
         bytes memory _oappBytecode,
@@ -71,44 +54,185 @@ contract PreCrimeV2Test is TestHelperOz5 {
         return address(new TransparentUpgradeableProxy(addr, proxyAdmin, _initializeArgs));
     }
 
-    function test_getConfig() public {
-        uint256[] memory packetMsgValues = new uint256[](1);
+    function setUpPreCrime(uint8 _numPeers, uint64 _maxBatchSize) private {
+        setUpEndpoints(_numPeers, LibraryType.SimpleMessageLib);
+
+        simulator = PreCrimeV2SimulatorUpgradeableMock(
+            _deployContractAndProxy(
+                type(PreCrimeV2SimulatorUpgradeableMock).creationCode,
+                new bytes(0),
+                abi.encodeWithSelector(PreCrimeV2SimulatorUpgradeableMock.initialize.selector, address(this))
+            )
+        );
+        preCrime = PreCrimeV2UpgradeableMock(
+            _deployContractAndProxy(
+                type(PreCrimeV2UpgradeableMock).creationCode,
+                abi.encode(address(endpoints[1]), address(simulator)),
+                abi.encodeWithSelector(PreCrimeV2UpgradeableMock.initialize.selector, address(this))
+            )
+        );
+
+        for (uint8 i = 2; i <= _numPeers; i++) {
+            preCrimePeers.push(PreCrimePeer(uint32(i), bytes32(uint256(i)), bytes32(uint256(i))));
+        }
+
+        preCrime.setPreCrimePeers(preCrimePeers);
+        preCrime.setMaxBatchSize(_maxBatchSize);
+    }
+
+    function checkPreCrimeParams(uint8 _numPeers, uint64 _maxBatchSize) private pure {
+        vm.assume(_numPeers > 0 && _numPeers <= 10);
+        vm.assume(_maxBatchSize >= 1 && _maxBatchSize <= 50);
+    }
+
+    function checkAndSetupPreCrime(uint8 _numPeers, uint64 _maxBatchSize) private {
+        checkPreCrimeParams(_numPeers, _maxBatchSize);
+        setUpPreCrime(_numPeers, _maxBatchSize);
+    }
+
+    function setUpDefaultPreCrime() private {
+        setUpPreCrime(DEFAULT_NUM_PEERS, DEFAULT_MAX_BATCH_SIZE);
+    }
+
+    function test_getConfig_no_packets(uint256[] calldata _packetMsgValues) public {
+        setUpDefaultPreCrime();
 
         // return config with all peers if no packet
         vm.startPrank(OFF_CHAIN);
-        bytes memory config = preCrime.getConfig(new bytes[](0), packetMsgValues);
+        bytes memory config = preCrime.getConfig(new bytes[](0), _packetMsgValues);
         bytes memory expectedConfig = abi.encodePacked(CONFIG_VERSION, MAX_BATCH_SIZE, _encodePeers(preCrimePeers));
-        assertEq(config, expectedConfig);
-
-        // return config without peers if packet from untrusted peer
-        bytes[] memory packets = _buildPacket(2, bytes32(0), 1, 1); // untrusted peer
-        config = preCrime.getConfig(packets, packetMsgValues);
-        expectedConfig = abi.encodePacked(CONFIG_VERSION, MAX_BATCH_SIZE);
-        assertEq(config, expectedConfig);
-
-        // return config with peers if packet from trusted peer
-        packets = _buildPacket(2, bytes32(uint256(2)), 1, 1); // trusted peer
-        config = preCrime.getConfig(packets, packetMsgValues);
-        expectedConfig = abi.encodePacked(CONFIG_VERSION, MAX_BATCH_SIZE, _encodePeers(preCrimePeers));
         assertEq(config, expectedConfig);
     }
 
-    function test_simulate_packetOverSize() public {
-        uint256[] memory packetMsgValues = new uint256[](5);
-        bytes[] memory packets = _buildPacket(2, bytes32(uint256(2)), 1, 5); // too many packets
+    function test_getConfig_untrusted_peer(uint8 _numPeers, uint64 _maxBatchSize, bytes32 _untrustedPeer) public {
+        checkAndSetupPreCrime(_numPeers, _maxBatchSize);
+        // ensure the fuzzed address is not a multiple of ADDRESS_MULTIPLIER
+        vm.assume(uint256(_untrustedPeer) == 0 || uint256(_untrustedPeer) > _numPeers);
+
+        // return config without peers if packet from untrusted peer
+        bytes[] memory packets = _buildPacket(2, _untrustedPeer, 1, 1); // untrusted peer
         vm.startPrank(OFF_CHAIN);
-        bytes memory expectedError = abi.encodeWithSelector(IPreCrime.PacketOversize.selector, 4, 5);
+        bytes memory config = preCrime.getConfig(packets, new uint256[](1));
+        bytes memory expectedConfig = abi.encodePacked(CONFIG_VERSION, _maxBatchSize);
+        assertEq(config, expectedConfig);
+    }
+
+    function test_getConfig_trusted_peer(uint8 _numPeers, uint64 _maxBatchSize) public {
+        vm.assume(_numPeers > 1);
+        checkAndSetupPreCrime(_numPeers, _maxBatchSize);
+
+        // return config with peers if packet from trusted peer
+        for (uint8 i = 2; i <= _numPeers; i++) {
+            bytes[] memory packets = _buildPacket(i, bytes32(uint256(i)), 1, 1); // trusted peer
+            vm.startPrank(OFF_CHAIN);
+            bytes memory config = preCrime.getConfig(packets, new uint256[](1));
+            bytes memory expectedConfig = abi.encodePacked(CONFIG_VERSION, _maxBatchSize, _encodePeers(preCrimePeers));
+            assertEq(config, expectedConfig);
+        }
+    }
+
+    function test_simulate_packetOverSize(uint64 _batchSize) public {
+        vm.assume(_batchSize > 0 && _batchSize < 1000);
+        setUpPreCrime(DEFAULT_NUM_PEERS, _batchSize);
+
+        uint256[] memory packetMsgValues = new uint256[](_batchSize + 1);
+        bytes[] memory packets = _buildPacket(2, bytes32(uint256(2)), 1, _batchSize + 1); // too many packets
+        vm.startPrank(OFF_CHAIN);
+        bytes memory expectedError = abi.encodeWithSelector(
+            IPreCrime.PacketOversize.selector,
+            _batchSize,
+            _batchSize + 1
+        );
         vm.expectRevert(expectedError);
         preCrime.simulate(packets, packetMsgValues);
     }
 
-    function test_simulate_packetUnsorted() public {
-        uint256[] memory packetMsgValues = new uint256[](4);
-        bytes[] memory unsortedPackets = new bytes[](4);
-        unsortedPackets[0] = _buildPacket(2, bytes32(uint256(2)), 1, 1)[0];
-        unsortedPackets[1] = _buildPacket(3, bytes32(uint256(3)), 1, 1)[0]; // unsorted
-        unsortedPackets[2] = _buildPacket(2, bytes32(uint256(2)), 2, 1)[0];
-        unsortedPackets[3] = _buildPacket(3, bytes32(uint256(4)), 1, 1)[0]; // untrested peer, but skipped
+    // Sets up a test scenario to ensure input packets are ordered and not interleaved. There are 4 peers:
+    // A: dstEid = 1
+    // B: srcEid = 2, sender = 0x0000000000000000000000000000000000000000000000000000000000000002
+    // C: srcEid = 3, sender = 0x0000000000000000000000000000000000000000000000000000000000000003
+    // The test then calls simulate() with:
+    // 1. numPacketsB from B to A starting at startingNonceB
+    // 2. numPacketsC from C to A starting at startingNonceC
+    // 3. One packet from B to A starting at startingNonceB + numPacketsB + 1
+    // PacketUnsorted is expected.
+    function test_simulate_packetUnsorted_interleaving_peers(
+        uint8 _numPacketsB,
+        uint64 _startingNonceB,
+        uint8 _numPacketsC,
+        uint64 _startingNonceC
+    ) public {
+        // numPacketsA and numPacketsB must be positive in order to cause interleaving
+        vm.assume(_numPacketsB > 0);
+        vm.assume(_numPacketsC > 0);
+
+        // calculate the last nonce sent by B and C, ensuring that the container does not overflow
+        uint128 endingNonceB = (uint128(_startingNonceB) + uint128(_numPacketsB));
+        uint128 endingNonceC = (uint128(_startingNonceC) + uint128(_numPacketsC));
+        vm.assume(endingNonceB < type(uint64).max);
+        vm.assume(endingNonceC < type(uint64).max);
+
+        uint64 totalPackets = uint64(_numPacketsB) + uint64(_numPacketsC) + 1; // 1 extra for the interleave at the end
+
+        setUpPreCrime(DEFAULT_NUM_PEERS, totalPackets);
+
+        uint256[] memory packetMsgValues = new uint256[](totalPackets);
+        bytes[] memory unsortedPackets = new bytes[](totalPackets);
+
+        // B -> A
+        bytes[] memory sortedPacketsB = _buildPacket(EID_B, SENDER_B, _startingNonceB, _numPacketsB);
+        assertEq(sortedPacketsB.length, _numPacketsB);
+        uint256 j = 0;
+        for (uint256 i = 0; i < _numPacketsB; i++) {
+            unsortedPackets[j++] = sortedPacketsB[i];
+        }
+        // C -> A
+        bytes[] memory sortedPacketsC = _buildPacket(EID_C, SENDER_C, _startingNonceC, _numPacketsC);
+        assertEq(sortedPacketsC.length, _numPacketsC);
+        for (uint256 i = 0; i < _numPacketsC; i++) {
+            unsortedPackets[j++] = sortedPacketsC[i];
+        }
+        // B -> A (interleaved)
+        unsortedPackets[j++] = _buildPacket(EID_B, SENDER_B, _startingNonceB + _numPacketsB + 1, 1)[0]; // unsorted
+
+        vm.startPrank(OFF_CHAIN);
+        bytes memory expectedError = abi.encodeWithSelector(IPreCrime.PacketUnsorted.selector);
+        vm.expectRevert(expectedError);
+        preCrime.simulate(unsortedPackets, packetMsgValues);
+    }
+
+    function test_simulate_packetUnsorted_one_unordered_peer(
+        uint8 _numPacketsB,
+        uint64 _startingNonceB,
+        uint8 _replaceIndexOffset // added to _startingNonceB to get the index of the packet to replace
+    ) public {
+        // 1. numPacketsB must be at least 3 to ensure that the replaceIndex is within bounds
+        // 2. replaceIndexOffset must be less than numPacketsB to ensure that the replaceIndex is within bounds
+        vm.assume(_numPacketsB >= 3);
+        vm.assume(_replaceIndexOffset < _numPacketsB - 2);
+        // 3. calculate the last nonce sent by B and C, ensuring that the container does not overflow
+        uint128 endingNonceB = (uint128(_startingNonceB) + uint128(_numPacketsB));
+        vm.assume(endingNonceB < type(uint64).max);
+        // 4. calculate the index of the packet to replace
+        uint64 replaceIndex = _replaceIndexOffset + _startingNonceB;
+        vm.assume(replaceIndex > _startingNonceB && replaceIndex < endingNonceB - 2);
+
+        setUpPreCrime(DEFAULT_NUM_PEERS, _numPacketsB);
+
+        uint256[] memory packetMsgValues = new uint256[](_numPacketsB);
+        bytes[] memory unsortedPackets = _buildPacket(EID_B, SENDER_B, _startingNonceB, _numPacketsB);
+        unsortedPackets[replaceIndex - _startingNonceB] = _encodePacket(
+            InboundPacket(
+                Origin(EID_B, SENDER_B, 0),
+                DST_EID,
+                address(uint160(uint256(SENDER_B))), //preCrime.oApp(),
+                bytes32(0), // guid
+                0, // value
+                address(0), // executor
+                "", // message
+                "" // extraData
+            )
+        );
 
         vm.startPrank(OFF_CHAIN);
         bytes memory expectedError = abi.encodeWithSelector(IPreCrime.PacketUnsorted.selector);
@@ -117,9 +241,12 @@ contract PreCrimeV2Test is TestHelperOz5 {
     }
 
     function test_simulate_failed() public {
+        uint32 invalidPeerEid = 0; // see PrecrimeV2SimulatorMock, which hardcodes eid=0 as InvalidEid
+        setUpDefaultPreCrime();
+
         // empty packetMsgValues to be reused
         uint256[] memory packetMsgValues = new uint256[](1);
-        bytes[] memory packets = _buildPacket(0, bytes32(0), 1, 1); // invalid packet and simulation failed
+        bytes[] memory packets = _buildPacket(invalidPeerEid, bytes32(0), 1, 1); // invalid packet and simulation failed
         vm.startPrank(OFF_CHAIN);
         bytes memory expectedError = abi.encodeWithSelector(
             IPreCrime.SimulationFailed.selector,
@@ -129,18 +256,25 @@ contract PreCrimeV2Test is TestHelperOz5 {
         preCrime.simulate(packets, packetMsgValues);
     }
 
-    function test_simulate() public {
-        uint256[] memory packetMsgValues = new uint256[](4);
-        bytes[] memory packets = _buildPacket(2, bytes32(uint256(2)), 1, 2);
-        packets = _appendPackets(packets, _buildPacket(3, bytes32(uint256(3)), 1, 2));
+    function test_simulate(uint8 _numPacketsB, uint8 _numPacketsC) public {
+        vm.assume(_numPacketsB > 0 && _numPacketsC > 0);
+        uint64 totalPackets = uint32(_numPacketsB) + uint32(_numPacketsC);
+        setUpPreCrime(DEFAULT_NUM_PEERS, totalPackets);
+
+        uint256[] memory packetMsgValues = new uint256[](totalPackets);
+        bytes[] memory packets = _buildPacket(EID_B, SENDER_B, 1, _numPacketsB);
+        packets = _appendPackets(packets, _buildPacket(EID_C, SENDER_C, 1, _numPacketsC));
+        assertEq(packets.length, totalPackets);
 
         vm.startPrank(OFF_CHAIN);
         bytes memory result = preCrime.simulate(packets, packetMsgValues);
-        bytes memory expectedResult = abi.encodePacked(uint32(1), uint256(4)); // receive 4 packets
+        bytes memory expectedResult = abi.encodePacked(DST_EID, uint256(totalPackets));
         assertEq(result, expectedResult);
     }
 
     function test_preCrime_simulationResultNotFound() public {
+        setUpDefaultPreCrime();
+
         uint256[] memory packetMsgValues = new uint256[](1);
         bytes[] memory packets = _buildPacket(2, bytes32(uint256(2)), 1, 1);
 
@@ -164,6 +298,8 @@ contract PreCrimeV2Test is TestHelperOz5 {
     }
 
     function test_preCrime() public {
+        setUpDefaultPreCrime();
+
         uint256[] memory packetMsgValues = new uint256[](1);
         bytes[] memory packets = _buildPacket(2, bytes32(uint256(2)), 1, 1);
 
@@ -188,19 +324,19 @@ contract PreCrimeV2Test is TestHelperOz5 {
         uint32 _srcEid,
         bytes32 _sender,
         uint64 _nonce,
-        uint256 _packetNum
+        uint256 _numPackets
     ) internal view returns (bytes[] memory) {
-        bytes[] memory packets = new bytes[](_packetNum);
-        for (uint256 i = 0; i < _packetNum; ++i) {
+        bytes[] memory packets = new bytes[](_numPackets);
+        for (uint256 i = 0; i < _numPackets; ++i) {
             InboundPacket memory packet = InboundPacket(
                 Origin(_srcEid, _sender, _nonce + uint64(i)),
-                1,
+                DST_EID,
                 preCrime.oApp(),
-                bytes32(0),
-                0,
-                address(0),
-                "",
-                ""
+                bytes32(0), // guid
+                0, // value
+                address(0), // executor
+                "", // message
+                "" // extraData
             );
             packets[i] = _encodePacket(packet);
         }
