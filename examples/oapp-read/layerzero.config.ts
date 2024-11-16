a937c6a74652c4fd8145220073bf543edc5be220
Matthew Krak <krak@layerzerolabs.org>
2024-11-17 00:51:36 +0700

    chore: update layerzero.config.ts

--- examples/oapp-read/layerzero.config.ts
+++ examples/oapp-read/layerzero.config.ts
@@ -1,31 +1,31 @@
-import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { ChannelId, EndpointId } from '@layerzerolabs/lz-definitions'
 
 import type { OAppReadOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
-const sepoliaContract: OmniPointHardhat = {
+
+const arbitrumContract: OmniPointHardhat = {
     eid: EndpointId.SEPOLIA_V2_TESTNET,
-    contractName: 'MyOAppRead',
-}
-
-const fujiContract: OmniPointHardhat = {
-    eid: EndpointId.AVALANCHE_V2_TESTNET,
-    contractName: 'MyOAppRead',
-}
-
-const amoyContract: OmniPointHardhat = {
-    eid: EndpointId.AMOY_V2_TESTNET,
-    contractName: 'MyOAppRead',
+    contractName: 'ReadPublic',
 }
 
 const config: OAppReadOmniGraphHardhat = {
     contracts: [
         {
-            contract: fujiContract,
-        },
-        {
-            contract: sepoliaContract,
-        },
-        {
-            contract: amoyContract,
+            contract: arbitrumContract,
+            config: {
+                readLibrary: '0xbcd4CADCac3F767C57c4F402932C4705DF62BEFf',
+                readChannels: [
+                    {
+                        channelId: ChannelId.READ_CHANNEL_1,
+                        active: true,
+                    },
+                ],
+                readConfig: {
+                    ulnConfig: {
+                        requiredDVNs: ['0x1308151a7ebac14f435d3ad5ff95c34160d539a5'],
+                        executor: '0x31CAe3B7fB82d847621859fb1585353c5720660D',
+                    },
+                },
+            },
         },
     ],
     connections: [],
