a64ece85e411ce7e843854828d6575effe262fd6
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-11-20 13:08:40 -0800

    fix: Bump test timeout

--- packages/devtools-ton/jest.config.ts
+++ packages/devtools-ton/jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
     cache: false,
     reporters: [['github-actions', { silent: false }], 'default'],
     testEnvironment: 'node',
+    testTimeout: 60_000,
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
