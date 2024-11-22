25e4b0977df5d8fbed8a00119d6e084d426743e6
Matthew Krak <krak@layerzerolabs.org>
2024-11-22 14:58:16 +0700

    feat: add full examples to CLI scaffolding

---
+++ examples/oft-adapter-aptos/contracts/mocks/MyOFTMock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+
+// @dev WARNING: This is for testing purposes only
+contract MyOFTMock is OFT {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _lzEndpoint,
+        address _delegate
+    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
+
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+}
