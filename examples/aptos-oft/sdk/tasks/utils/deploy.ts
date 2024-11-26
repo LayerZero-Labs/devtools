d4860678bc21c0e4826343181cc0fccac565872e
shankar <shankar@layerzerolabs.org>
2024-11-26 14:15:13 -0800

    feat: deployment script
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/aptos-oft/sdk/tasks/utils/deploy.ts
+++ examples/aptos-oft/sdk/tasks/utils/deploy.ts
@@ -1,31 +1,97 @@
-import { Account, Aptos, AptosConfig, InputViewFunctionData, Network } from '@aptos-labs/ts-sdk'
+import { ArgumentParser } from 'argparse'
+import { spawn } from 'child_process'
+import { assert } from 'console'
+import { deploymentFile } from './types'
 
-import { loadAptosYamlConfig, createAccountFromPrivateKey } from '@/utils/config'
+import fs from 'fs'
 
-async function main() {
-    const aptosYamlConfig = await loadAptosYamlConfig()
+let stdOut = ''
+let stdErr = ''
 
-    const account_address = aptosYamlConfig.profiles.default.account
-    const public_key = aptosYamlConfig.profiles.default.public_key
-    const private_key = aptosYamlConfig.profiles.default.private_key
+const parser = new ArgumentParser({
+    description: 'A simple CLI tool built with argparse in TypeScript',
+})
+async function createDeployment(deployedAddress: string, file_name: string = 'oft.json') {
+    fs.mkdirSync('deployments', { recursive: true })
+    fs.mkdirSync('deployments/aptos-mainnet', { recursive: true })
 
-    const account: Account = createAccountFromPrivateKey(private_key, account_address)
-
-    const network_type = aptosYamlConfig.profiles.default.network as Network
-    const full_node_url = aptosYamlConfig.profiles.default.rest_url
-    const faucet_url = aptosYamlConfig.profiles.default.faucet_url
-
-    const aptosConfig = new AptosConfig({ network: network_type, fullnode: full_node_url, faucet: faucet_url })
-
-    const aptos = new Aptos(aptosConfig)
-
-    const payload: InputViewFunctionData = {
-        function: `::router::optimal_liquidity_amounts`,
-        functionArguments: [],
+    const deployment: deploymentFile = {
+        address: deployedAddress,
+        abi: [],
+        transactionHash: '',
+        receipt: {},
+        args: [],
+        numDeployments: 1,
+        solcInputHash: '',
+        metadata: '',
+        bytecode: '',
+        deployedBytecode: '',
+        devdoc: {},
+        storageLayout: {},
     }
 
-    const result = await aptos.view({ payload })
-    console.log('Result:', result)
+    fs.writeFileSync(`deployments/aptos-mainnet/${file_name}.json`, JSON.stringify(deployment, null, 2))
+    console.log(`Deployment file created at deployments/aptos-mainnet/${file_name}.json`)
+}
+
+async function main() {
+    // read in the first arg passed via the command line
+    parser.add_argument('--package-dir', { type: 'str', help: 'Directory of the OFT you want to deploy (oft)' })
+    parser.add_argument('--address-name', { type: 'str', help: 'Module name of the OFT (oft)' })
+
+    const parserArgs = parser.parse_args()
+
+    const package_dir = parserArgs.package_dir
+    const address_name = parserArgs.address_name
+
+    const cmd = 'aptos'
+    const args = [
+        'move',
+        'create-object-and-publish-package',
+        `--package-dir=${package_dir}`,
+        `--address-name=${address_name}`,
+    ]
+
+    return new Promise<void>((resolve, reject) => {
+        const childProcess = spawn(cmd, args, {
+            stdio: ['inherit', 'pipe', 'pipe'], // Inherit stdin, pipe stdout and stderr
+        })
+
+        // Capture stdout which contains our deployed address
+        childProcess.stdout?.on('data', (data) => {
+            const dataStr = data.toString()
+            stdOut += dataStr
+            process.stdout.write(`${dataStr}`)
+        })
+
+        // Capture stderr (this is actually NOT the error output but the interactive prompt)
+        childProcess.stderr?.on('data', (data) => {
+            const dataStr = data.toString()
+            stdErr += dataStr
+            process.stderr.write(`${dataStr}`)
+        })
+
+        // Handle process close
+        childProcess.on('close', (code) => {
+            if (code === 0) {
+                const addresses = stdOut.match(/0x[0-9a-fA-F]{64}/g)
+                assert(addresses[0] == addresses[1], 'Addresses do not match')
+                createDeployment(addresses[0], address_name)
+
+                resolve()
+            } else {
+                console.error(`Command failed with code ${code}`)
+                console.error('Captured stderr:', stdErr)
+                reject(new Error(`Process exited with code ${code}`))
+            }
+        })
+
+        // Handle errors
+        childProcess.on('error', (err) => {
+            console.error('Error spawning the process:', err)
+            reject(err)
+        })
+    })
 }
 
 main().catch((error) => {
