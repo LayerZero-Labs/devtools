14fa43b0f362a3801bb9c9b5571b91c51694ddab
shankar <shankar@layerzerolabs.org>
2024-11-26 18:45:40 -0800

    evm wiring
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/aptos-oft/tasks/utils/utils.ts
+++ examples/aptos-oft/tasks/utils/utils.ts
@@ -1,4 +1,7 @@
 import hardhatConfig from '../../hardhat.config'
+import lzConfig from '../../aptos.layerzero.config'
+import type { OAppOmniGraphHardhat } from '@layerzerolabs/toolbox-hardhat'
+import { HttpNetworkAccountsUserConfig } from 'hardhat/types'
 
 export function createEidToNetworkMapping() {
     const networks = hardhatConfig.networks
@@ -8,6 +11,33 @@ export function createEidToNetworkMapping() {
         eidNetworkNameMapping[networkConfig.eid] = networkName
     }
 
-    console.log(eidNetworkNameMapping)
+    eidNetworkNameMapping[50008] = 'aptos-mainnet'
+
     return eidNetworkNameMapping
+}
+
+export function createEidToNetworkUrlMapping() {
+    const networks = hardhatConfig.networks
+
+    const eidNetworkNameMapping: Record<number, string> = {}
+    for (const [networkName, networkConfig] of Object.entries(networks)) {
+        eidNetworkNameMapping[networkConfig.eid] = networkConfig.url as HttpNetworkUserConfig
+    }
+
+    eidNetworkNameMapping[50008] = 'aptos-mainnet'
+
+    return eidNetworkNameMapping
+}
+
+export function getConfigConnections(_key: string, _eid: number): OAppOmniGraphHardhat['connections'] {
+    const conns = lzConfig.connections
+    const connections: OAppOmniGraphHardhat['connections'] = []
+
+    for (const conn of conns) {
+        if (conn[_key].eid == _eid) {
+            connections.push(conn)
+        }
+    }
+
+    return connections
 }
