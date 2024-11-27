7b7d605806e00694e19f7ae74cf6573140344432
shankar <shankar@layerzerolabs.org>
2024-11-26 19:50:56 -0800

    todo: fetch the aptos oft address dynamicaly
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/aptos-oft/tasks/wireEVMOFTs.ts
+++ examples/aptos-oft/tasks/wireEVMOFTs.ts
@@ -1,6 +1,6 @@
-import { createEidToNetworkMapping, getConfigConnections } from './utils/utils'
+import { createEidToNetworkMapping, createEidToNetworkUrlMapping, getConfigConnections } from './utils/utils'
 import fs from 'fs'
-import { ContractFactory, ethers, providers } from 'ethers'
+import { ContractFactory, ethers } from 'ethers'
 
 // Ensure environment variable is loaded
 if (!process.env.PRIVATE_KEY) {
@@ -9,18 +9,16 @@ if (!process.env.PRIVATE_KEY) {
 }
 
 // Initialize signer with provider
-let provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
-let signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+let provider = null
+let signer = null
 
 const eid_aptos = 50008
 
-// Get connections and networks mapping
 const connsToWire = getConfigConnections('to', eid_aptos)
-console.log(connsToWire)
 
 const networks = createEidToNetworkMapping()
+const urls = createEidToNetworkUrlMapping()
 
-// Iterate through connections and interact with contracts
 ;(async () => {
     for (const conn of connsToWire) {
         const fromNetwork = networks[conn.from.eid]
@@ -32,22 +30,18 @@ const networks = createEidToNetworkMapping()
         const path = `deployments/${fromNetwork}/${conn.from.contractName}.json`
         const fromNetworkDeployments = JSON.parse(fs.readFileSync(path, 'utf8'))
 
-        new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
-        new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+        provider = new ethers.providers.JsonRpcProvider(urls[conn.to.eid])
+        signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 
         const address = fromNetworkDeployments.address
         const abi = fromNetworkDeployments.abi
         const bytecode = fromNetworkDeployments.bytecode
 
-        console.log('Wiring with address: ', address)
-
-        // Attach the deployed contract
         const factory = new ContractFactory(abi, bytecode, signer)
         const contract = factory.attach(address)
 
         console.log(`Owner of contract at ${address}:`, await callContractFunction(contract))
 
-        // Uncomment to set a peer (example of state-changing function)
         await contract.setPeer(conn.to.eid, '0x8401fa82eea1096b32fd39207889152f947d78de1b65976109493584636622a8')
 
         console.log(`Owner of contract at ${address}:`, await callContractFunction(contract))
