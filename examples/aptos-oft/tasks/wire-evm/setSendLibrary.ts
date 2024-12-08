0baac9bd094806666037d20f2a429f83f07ed9d5
shankar <shankar@layerzerolabs.org>
2024-12-08 14:27:22 +0530

    feat: setSendLibrary
    Signed-off-by: shankar <shankar@layerzerolabs.org>

---
+++ examples/aptos-oft/tasks/wire-evm/setSendLibrary.ts
@@ -0,0 +1,55 @@
+import { AptosOFTMetadata, ContractMetadataMapping, eid, EidTxMap } from '../utils/types'
+import { diffPrinter } from '../utils/utils'
+import { Contract, utils } from 'ethers'
+
+const error_LZ_DefaultSendLibUnavailable = '0x6c1ccdb5'
+
+/**
+ * Sets peer information for connections to wire.
+ */
+export async function createSetSendLibraryTransactions(
+    eidDataMapping: ContractMetadataMapping,
+    aptosOft: AptosOFTMetadata
+): Promise<EidTxMap> {
+    const txTypePool: EidTxMap = {}
+
+    for (const [eid, { address, contract, configOapp }] of Object.entries(eidDataMapping)) {
+        if (configOapp?.sendLibrary === undefined) {
+            console.log(`\x1b[43m Skipping: No sendLibrary has been set for ${eid} @ ${address.oapp} \x1b[0m`)
+            continue
+        }
+
+        const fromSendLibrary = await getSendLibrary(contract.epv2, address.oapp, aptosOft.eid)
+
+        const toSendLibrary = utils.getAddress(configOapp.sendLibrary)
+
+        if (fromSendLibrary === toSendLibrary) {
+            console.log(`\x1b[43m Skipping: The same send library has been set for ${eid} @ ${address.oapp} \x1b[0m`)
+            continue
+        }
+
+        diffPrinter(`Setting Send Library on ${eid}`, { sendLibrary: fromSendLibrary }, { sendLibrary: toSendLibrary })
+
+        const tx = await contract.epv2.populateTransaction.setSendLibrary(address.oapp, aptosOft.eid, toSendLibrary)
+
+        if (!txTypePool[eid]) {
+            txTypePool[eid] = []
+        }
+
+        txTypePool[eid].push(tx)
+    }
+
+    return txTypePool
+}
+
+export async function getSendLibrary(epv2Contract: Contract, evmAddress: string, aptosEid: eid): Promise<string> {
+    const sendLib = await epv2Contract.getSendLibrary(evmAddress, aptosEid)
+
+    if (sendLib === error_LZ_DefaultSendLibUnavailable) {
+        return '0x0000000000000000000000000000000000000000'
+    }
+
+    const sendLibAddress = utils.getAddress(sendLib)
+
+    return sendLibAddress
+}
