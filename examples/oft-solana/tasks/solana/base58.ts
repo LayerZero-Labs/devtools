b76a41aea1ae312f5e1ba2e5594aeb916a75997b
nazreen <nazreen.mohamad@layerzerolabs.org>
2024-12-11 17:14:17 +0800

    base 58 hardhat task

---
+++ examples/oft-solana/tasks/solana/base58.ts
@@ -0,0 +1,32 @@
+import fs from 'fs'
+import path from 'path'
+
+import { Keypair } from '@solana/web3.js'
+import bs58 from 'bs58'
+import { task } from 'hardhat/config'
+
+import { types as devtoolsTypes } from '@layerzerolabs/devtools-evm-hardhat'
+
+interface Base58FeesTaskArgs {
+    /**
+     * The path to the keypair file to be used.
+     */
+    keypairFile: string
+}
+
+const defaultKeypairFile = path.resolve(process.env.HOME!, '.config/solana/id.json')
+
+task('lz:solana:base-58', 'Outputs the base58 string for a keypair')
+    .addParam(
+        'keypairFile',
+        'The path to the keypair file to be used. Defaults to ~/.config/solana/id.json',
+        defaultKeypairFile,
+        devtoolsTypes.string
+    )
+    .setAction(async ({ keypairFile }: Base58FeesTaskArgs) => {
+        const data = fs.readFileSync(keypairFile, 'utf8')
+        const keypairJson = JSON.parse(data)
+        const keypair = Keypair.fromSecretKey(Uint8Array.from(keypairJson))
+        const base58EncodedPrivateKey = bs58.encode(keypair.secretKey)
+        console.log(base58EncodedPrivateKey)
+    })
