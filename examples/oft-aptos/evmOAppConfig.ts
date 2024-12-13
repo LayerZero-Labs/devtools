cadd28857bb29230ade35298a26c6d02f225d042
shankar <shankar@layerzerolabs.org>
2024-12-13 07:04:23 +0000

    sample evmOAppConfig
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-aptos/evmOAppConfig.ts
+++ examples/oft-aptos/evmOAppConfig.ts
@@ -40,21 +40,21 @@ const bscOAppConfig: OAppEdgeConfig = {
     sendConfig: {
         executorConfig: {
             maxMessageSize: 65536,
-            executor: '',
+            executor: '0x3ebD570ed38B1b3b4BC886999fcF507e9D584859',
         },
         ulnConfig: {
             confirmations: BigInt(5),
-            requiredDVNs: [''],
-            optionalDVNs: [''],
-            optionalDVNThreshold: 0,
+            requiredDVNs: ['0x0eE552262f7B562eFcED6DD4A7e2878AB897d405', '0x6f99eA3Fc9206E2779249E15512D7248dAb0B52e'],
+            optionalDVNs: ['0x2dDf08e397541721acD82E5b8a1D0775454a180B', '0x6F978ee5bfd7b1A8085A3eA9e54eB76e668E195a'],
+            optionalDVNThreshold: 1,
         },
     },
     receiveConfig: {
         ulnConfig: {
             confirmations: BigInt(5),
-            requiredDVNs: [''],
-            optionalDVNs: [''],
-            optionalDVNThreshold: 0,
+            requiredDVNs: ['0x0eE552262f7B562eFcED6DD4A7e2878AB897d405', '0x6f99eA3Fc9206E2779249E15512D7248dAb0B52e'],
+            optionalDVNs: ['0x2dDf08e397541721acD82E5b8a1D0775454a180B', '0x6F978ee5bfd7b1A8085A3eA9e54eB76e668E195a'],
+            optionalDVNThreshold: 1,
         },
     },
 }
