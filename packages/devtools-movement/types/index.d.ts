742462d0ed4982b19480d0ab49c1cd043efa583c
shankar <shankar@layerzerolabs.org>
2024-12-30 23:07:03 +0000

    feat: core devtools movement
    Signed-off-by: shankar <shankar@layerzerolabs.org>

---
+++ packages/devtools-movement/types/index.d.ts
@@ -0,0 +1,5 @@
+import { build } from '../tasks/move/build'
+import { deploy } from '../tasks/move/deploy'
+import { executeSetDelegate } from '../tasks/move/setDelegate'
+
+export { build, deploy, executeSetDelegate }
