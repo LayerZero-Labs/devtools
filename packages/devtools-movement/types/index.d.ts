3ab9bdf125b87146e92529fa9fd812d59e82b550
shankar <shankar@layerzerolabs.org>
2025-01-02 20:33:13 +0000

    feat: cli + exports
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/devtools-movement/types/index.d.ts
+++ packages/devtools-movement/types/index.d.ts
@@ -1,5 +1,8 @@
 import { build } from '../tasks/move/build'
 import { deploy } from '../tasks/move/deploy'
-import { executeSetDelegate } from '../tasks/move/setDelegate'
+import { setDelegate } from '../tasks/move/setDelegate'
 
-export { build, deploy, executeSetDelegate }
+import { wireMove } from '../tasks/move/wire'
+import { wireEvm } from '../tasks/evm/wire-evm'
+
+export { build, deploy, setDelegate, wireMove, wireEvm }
