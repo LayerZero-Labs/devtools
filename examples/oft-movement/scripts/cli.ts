8a298b1abffe8c94781ddda928b266fe38e0b80b
shankar <shankar@layerzerolabs.org>
2025-01-03 21:25:39 +0000

    using devtools-extensible-cli
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-movement/scripts/cli.ts
+++ examples/oft-movement/scripts/cli.ts
@@ -1,3 +1,13 @@
-import { AptosEVMCLI } from '@layerzerolabs/oft-movement/cli/AptosEVMCli'
+import { sdk } from '@layerzerolabs/devtools-extensible-cli/cli/AptosEVMCli'
+import { init_evm, init_move } from '@layerzerolabs/devtools-movement/cli/init'
+import { oft_move } from '@layerzerolabs/oft-movement/cli/init'
 
-new AptosEVMCLI().cli()
+async function lzSdk() {
+    await init_move(sdk)
+    await init_evm(sdk)
+    await oft_move(sdk)
+
+    await sdk.execute()
+}
+
+lzSdk()
