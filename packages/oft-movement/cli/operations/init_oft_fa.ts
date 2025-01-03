7d5df8591c204c0dabacf82bd0c1782ff4fb5c64
shankar <shankar@layerzerolabs.org>
2025-01-03 21:24:50 +0000

    move-oft-fa operations
    Signed-off-by: shankar <shankar@layerzerolabs.org>

---
+++ packages/oft-movement/cli/operations/init_oft_fa.ts
@@ -0,0 +1,37 @@
+import { INewOperation } from '@layerzerolabs/devtools-extensible-cli'
+import path from 'path'
+
+import { initOFTFA } from '../../tasks/initOFTFA'
+
+class InitOFTFA implements INewOperation {
+    vm = 'move'
+    operation = 'init'
+    reqArgs = ['move_deploy_script']
+
+    async impl(args: any): Promise<void> {
+        const fullPathOFTConfig = path.resolve(path.join(args.rootDir, args.move_deploy_script))
+        const oftConfig = await import(fullPathOFTConfig)
+
+        const oftType = oftConfig.oftType
+        const oftMetadata = oftConfig.oftMetadata
+        if (!oftMetadata) {
+            throw new Error(`${fullPathOFTConfig} does not contain an oftMetadata object`)
+        }
+
+        switch (oftType) {
+            case 'OFT_FA':
+                await initOFTFA(
+                    oftMetadata.token_name,
+                    oftMetadata.token_symbol,
+                    oftMetadata.icon_uri,
+                    oftMetadata.project_uri
+                )
+                break
+            default:
+                throw new Error(`Unsupported OFT type: ${oftType}`)
+        }
+    }
+}
+
+const NewOperation = new InitOFTFA()
+export { NewOperation }
