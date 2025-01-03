c55d99badd853c11c7f2f2267f173229607cbe22
shankar <shankar@layerzerolabs.org>
2025-01-03 21:18:35 +0000

    devtools-cli operations to wire, build, and deploy
    Signed-off-by: shankar <shankar@layerzerolabs.org>

---
+++ packages/devtools-movement/cli/operations/move-wire.ts
@@ -0,0 +1,15 @@
+import { INewOperation } from '@layerzerolabs/devtools-extensible-cli'
+import { wireMove } from '../../tasks/move/wire'
+
+class MoveWireOperation implements INewOperation {
+    vm = 'move'
+    operation = 'wire'
+    reqArgs = ['lz_config']
+
+    async impl(args: any): Promise<void> {
+        await wireMove(args)
+    }
+}
+
+const NewOperation = new MoveWireOperation()
+export { NewOperation }
