9f414c04290ee78471a3983c13d949e5ee4bcbbc
nazreen <nazreen.mohamad@layerzerolabs.org>
2025-01-03 18:52:02 +0800

    remove usage of program_id_from_env

--- examples/oft-solana/programs/oft/src/lib.rs
+++ examples/oft-solana/programs/oft/src/lib.rs
@@ -14,13 +14,9 @@ use oapp::{
     endpoint::{MessagingFee, MessagingReceipt},
     LzReceiveParams,
 };
-use solana_helper::program_id_from_env;
 use state::*;
 
-declare_id!(Pubkey::new_from_array(program_id_from_env!(
-    "OFT_ID",
-    "9UovNrJD8pQyBLheeHNayuG1wJSEAoxkmM14vw5gcsTT"
-)));
+declare_id!("9UovNrJD8pQyBLheeHNayuG1wJSEAoxkmM14vw5gcsTT");
 
 pub const OFT_SEED: &[u8] = b"OFT";
 pub const PEER_SEED: &[u8] = b"Peer";
