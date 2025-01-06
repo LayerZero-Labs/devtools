76389a07458dae011353197d26331b1f46b51826
shankar <shankar@layerzerolabs.org>
2025-01-06 22:08:03 +0000

    extensible cli takes in help
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-movement/deploy/MyMovementOFTFA.ts
+++ examples/oft-movement/deploy/MyMovementOFTFA.ts
@@ -2,8 +2,8 @@ const contractName = 'oft'
 
 const oftType = 'OFT_FA'
 const oftMetadata = {
-    token_name: 'MyMovementOFT',
-    token_symbol: 'MMOFT',
+    token_name: 'MyMoveVMOFT',
+    token_symbol: 'MMVMOFT',
     icon_uri: '',
     project_uri: '',
 }
