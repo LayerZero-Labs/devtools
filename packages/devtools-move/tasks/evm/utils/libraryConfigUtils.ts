9248c2772b42fead1d275866f77ee55022f30699
alexanderliteplo <alexanderliteplo@gmail.com>
2025-01-06 16:41:43 -0800

    renaming, refactoring, bug fixing

---
+++ packages/devtools-move/tasks/evm/utils/libraryConfigUtils.ts
@@ -0,0 +1,108 @@
+import { Contract, PopulatedTransaction, utils } from 'ethers'
+
+import { Uln302ExecutorConfig, Uln302UlnUserConfig } from '@layerzerolabs/toolbox-hardhat'
+
+import { returnChecksum, returnChecksums } from './types'
+
+import type { SetConfigParam, address, eid } from './types'
+
+export async function getConfig(
+    epv2Contract: Contract,
+    evmAddress: address,
+    libraryAddress: address,
+    aptosEid: eid,
+    configType: number
+): Promise<string> {
+    const config = await epv2Contract.getConfig(evmAddress, libraryAddress, aptosEid, configType)
+    // const toDecode: SetConfigParam = {
+    //     eid: aptosEid,
+    //     configType: configType,
+    //     config: config,
+    // }
+    // decodeConfig([toDecode])
+    return config
+}
+
+export function setConfig(
+    epv2Contract: Contract,
+    evmAddress: address,
+    libraryAddress: address,
+    config: SetConfigParam[]
+): Promise<PopulatedTransaction> {
+    return epv2Contract.populateTransaction.setConfig(evmAddress, libraryAddress, config)
+}
+
+export function buildConfig(
+    ulnConfig: Uln302UlnUserConfig,
+    executorConfig: Uln302ExecutorConfig | null = null
+): { executorConfigBytes: string; ulnConfigBytes: string } {
+    if (!ulnConfig.optionalDVNs) {
+        ulnConfig.optionalDVNs = []
+    }
+    const _optionalDVNs = returnChecksums(ulnConfig.optionalDVNs)
+    const _requiredDVNs = returnChecksums(ulnConfig.requiredDVNs)
+
+    const ulnConfigBytes = utils.defaultAbiCoder.encode(
+        [
+            'tuple(uint64 confirmations, uint8 requiredDVNCount, uint8 optionalDVNCount, uint8 optionalDVNThreshold, address[] requiredDVNs, address[] optionalDVNs)',
+        ],
+        [
+            {
+                confirmations: ulnConfig.confirmations,
+                requiredDVNCount: _requiredDVNs.length,
+                optionalDVNCount: _optionalDVNs.length,
+                optionalDVNThreshold: ulnConfig.optionalDVNThreshold,
+                requiredDVNs: _requiredDVNs.sort(),
+                optionalDVNs: _optionalDVNs.sort(),
+            },
+        ]
+    )
+
+    if (executorConfig !== null) {
+        const _executor = returnChecksum(executorConfig.executor)
+        const executorConfigBytes = utils.defaultAbiCoder.encode(
+            ['uint32', 'address'],
+            [executorConfig.maxMessageSize, _executor]
+        )
+
+        return { executorConfigBytes, ulnConfigBytes }
+    }
+    return { executorConfigBytes: '0x', ulnConfigBytes }
+}
+
+export function decodeConfig(configParam: SetConfigParam[]) {
+    const decodedConfig: {
+        executorConfig: Uln302ExecutorConfig
+        ulnConfig: Uln302UlnUserConfig
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } = {} as any
+    try {
+        for (const { configType, config } of configParam) {
+            if (configType === 1) {
+                const decoded = utils.defaultAbiCoder.decode(['uint32', 'address'], config)
+                decodedConfig.executorConfig = {
+                    maxMessageSize: decoded[0],
+                    executor: decoded[1],
+                }
+            } else if (configType === 2) {
+                const decoded = utils.defaultAbiCoder.decode(
+                    [
+                        'tuple(uint64 confirmations, uint8 requiredDVNCount, uint8 optionalDVNCount, uint8 optionalDVNThreshold, address[] requiredDVNs, address[] optionalDVNs)',
+                    ],
+                    config
+                )
+                decodedConfig.ulnConfig = {
+                    confirmations: decoded[0]['confirmations'],
+                    requiredDVNs: decoded[0]['requiredDVNs'],
+                    optionalDVNs: decoded[0]['optionalDVNs'],
+                    optionalDVNThreshold: decoded[0]['optionalDVNThreshold'],
+                }
+            }
+        }
+    } catch (e) {
+        console.log(decodeConfig)
+        return
+    }
+
+    return decodedConfig
+}
---
+++ packages/devtools-move/tasks/move/setDelegate.ts
@@ -0,0 +1,36 @@
+import path from 'path'
+import { getChain, getConnection } from '../../sdk/moveVMConnectionBuilder'
+import { OFT } from '../../sdk/oft'
+
+import { getEidFromAptosNetwork, getLzNetworkStage, parseYaml } from './utils/aptosNetworkParser'
+import { setDelegate } from './utils/moveVMOftConfigOps'
+import { getDelegateFromLzConfig, getMoveVMOftAddress, sendAllTxs } from './utils/utils'
+
+async function executeSetDelegate(args: any, useAccountAddress: boolean = false) {
+    const configPath = args.lz_config
+    const lzConfigPath = path.resolve(path.join(process.cwd(), configPath))
+    const lzConfigFile = await import(lzConfigPath)
+    const lzConfig = lzConfigFile.default
+
+    const { account_address, private_key, network, fullnode, faucet } = await parseYaml()
+
+    const chain = getChain(fullnode)
+    const aptos = getConnection(chain, network, fullnode, faucet)
+
+    const lzNetworkStage = getLzNetworkStage(network)
+    const oftAddress = getMoveVMOftAddress(lzNetworkStage)
+
+    console.log(`\n🔧 Setting ${chain}-${lzNetworkStage} OFT Delegate`)
+    console.log(`\tFor: ${oftAddress}\n`)
+
+    const oft = new OFT(aptos, oftAddress, account_address, private_key)
+
+    const eid = getEidFromAptosNetwork('aptos', network)
+    const delegate = useAccountAddress ? account_address : getDelegateFromLzConfig(eid, lzConfig)
+
+    const setDelegatePayload = await setDelegate(oft, delegate, eid)
+
+    sendAllTxs(aptos, oft, account_address, [setDelegatePayload])
+}
+
+export { executeSetDelegate as setDelegate }
