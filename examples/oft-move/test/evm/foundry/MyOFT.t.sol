48a7aad4d880b116267693bff50ee63fc4a78f7d
shankar <shankar@layerzerolabs.org>
2025-01-07 00:47:50 +0000

    mergemerge

--- examples/oft-move/test/evm/foundry/MyOFT.t.sol
+++ examples/oft-move/test/evm/foundry/MyOFT.t.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.20;
 
 // Mock imports
-import { OFTMock } from "../mocks/OFTMock.sol";
-import { ERC20Mock } from "../mocks/ERC20Mock.sol";
-import { OFTComposerMock } from "../mocks/OFTComposerMock.sol";
+import { MyOFT } from "../../../contracts/OFT.sol";
 
 // OApp imports
 import { IOAppOptionsType3, EnforcedOptionParam } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
@@ -28,8 +26,8 @@ import { TestHelperOz5 } from "@layerzerolabs/test-devtools-evm-foundry/contract
 contract MyOFTTest is TestHelperOz5 {
     using OptionsBuilder for bytes;
 
-    uint32 private aEid = 1;
-    uint32 private bEid = 2;
+    uint32 private bscEid = ;
+    uint32 private aptosEid = 2;
 
     OFTMock private aOFT;
     OFTMock private bOFT;
@@ -57,7 +55,6 @@ contract MyOFTTest is TestHelperOz5 {
         address[] memory ofts = new address[](2);
         ofts[0] = address(aOFT);
         ofts[1] = address(bOFT);
-        this.wireOApps(ofts);
 
         // mint tokens
         aOFT.mint(userA, initialBalance);
@@ -99,62 +96,4 @@ contract MyOFTTest is TestHelperOz5 {
         assertEq(aOFT.balanceOf(userA), initialBalance - tokensToSend);
         assertEq(bOFT.balanceOf(userB), initialBalance + tokensToSend);
     }
-
-    function test_send_oft_compose_msg() public {
-        uint256 tokensToSend = 1 ether;
-
-        OFTComposerMock composer = new OFTComposerMock();
-
-        bytes memory options = OptionsBuilder
-            .newOptions()
-            .addExecutorLzReceiveOption(200000, 0)
-            .addExecutorLzComposeOption(0, 500000, 0);
-        bytes memory composeMsg = hex"1234";
-        SendParam memory sendParam = SendParam(
-            bEid,
-            addressToBytes32(address(composer)),
-            tokensToSend,
-            tokensToSend,
-            options,
-            composeMsg,
-            ""
-        );
-        MessagingFee memory fee = aOFT.quoteSend(sendParam, false);
-
-        assertEq(aOFT.balanceOf(userA), initialBalance);
-        assertEq(bOFT.balanceOf(address(composer)), 0);
-
-        vm.prank(userA);
-        (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt) = aOFT.send{ value: fee.nativeFee }(
-            sendParam,
-            fee,
-            payable(address(this))
-        );
-        verifyPackets(bEid, addressToBytes32(address(bOFT)));
-
-        // lzCompose params
-        uint32 dstEid_ = bEid;
-        address from_ = address(bOFT);
-        bytes memory options_ = options;
-        bytes32 guid_ = msgReceipt.guid;
-        address to_ = address(composer);
-        bytes memory composerMsg_ = OFTComposeMsgCodec.encode(
-            msgReceipt.nonce,
-            aEid,
-            oftReceipt.amountReceivedLD,
-            abi.encodePacked(addressToBytes32(userA), composeMsg)
-        );
-        this.lzCompose(dstEid_, from_, options_, guid_, to_, composerMsg_);
-
-        assertEq(aOFT.balanceOf(userA), initialBalance - tokensToSend);
-        assertEq(bOFT.balanceOf(address(composer)), tokensToSend);
-
-        assertEq(composer.from(), from_);
-        assertEq(composer.guid(), guid_);
-        assertEq(composer.message(), composerMsg_);
-        assertEq(composer.executor(), address(this));
-        assertEq(composer.extraData(), composerMsg_); // default to setting the extraData to the message as well to test
-    }
-
-    // TODO import the rest of oft tests?
 }
