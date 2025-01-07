26859cfad95a9ba8647d740614b8a0bc1290fc47
alexanderliteplo <alexanderliteplo@gmail.com>
2025-01-06 16:41:43 -0800

    renaming, refactoring, bug fixing

---
+++ examples/oft-move/README.md
@@ -0,0 +1,98 @@
+### connecting to aptos via cli
+To install aptos cli, run the following command:
+```
+brew install aptos
+```
+If you need to generate a new key, run the following command:
+```
+aptos key generate --output-file my_key.pub
+```
+Then initialize the aptos cli and connect to the aptos network:
+```
+aptos init --network=testnet --private-key=<your-private-key>
+```
+You can then verify that your initialization was successful by running the following command:
+```
+cat .aptos/config.yaml 
+```
+If successful the config will be populated with the RPC links and your account private key, account address, and network.
+
+Note: Your private key is stored in the .aptos/config.yaml file and will be extracted from there.
+## Setup
+Create a `.env` file with the following variables:
+
+```bash
+ACCOUNT_ADDRESS=<your-aptos-account-address>
+EVM_PRIVATE_KEY=<your-evm-private-key>
+```
+Note: aptos account address can be found in .aptos/config.yaml
+## Build and deploy
+
+### Builds the contracts
+```bash
+pnpm run lz:sdk:move:build --lz-config move.layerzero.config.ts --named-addresses oft=$ACCOUNT_ADDRESS,oft_admin=$ACCOUNT_ADDRESS --move-deploy-script deploy-move/MyMoveOFTFA.ts
+```
+
+### Checks for build, builds if not, then deploys the contracts, sets the delegate and initializes
+
+```bash
+pnpm run lz:sdk:move:deploy --lz-config move.layerzero.config.ts --named-addresses oft=$ACCOUNT_ADDRESS,oft_admin=$ACCOUNT_ADDRESS --move-deploy-script deploy/MyMoveOFTFA.ts
+```
+
+## Init - Not OFT Agnostic
+First modify deploy/MyMoveOFTFA.ts and replace the oftMetadata with the following:
+```ts
+const oftType = 'OFT_FA'
+const oftMetadata = {
+    token_name: 'MyMoveOFT',
+    token_symbol: 'MMOFT',
+    icon_uri: '',
+    project_uri: '',
+}
+```
+Before running the wire command, first inside of move.layerzero.config.ts, set the delegate address to your account address.
+```ts
+    contracts: [
+        {
+            contract: bscContract,
+            config: {
+                owner: 'YOUR_EVM_ACCOUNT_ADDRESS',
+                delegate: 'YOUR_EVM_ACCOUNT_ADDRESS',
+            },
+        },
+        {
+            contract: aptosContract,
+            config: {
+                delegate: 'YOUR_APTOS_ACCOUNT_ADDRESS',
+                owner: 'YOUR_APTOS_ACCOUNT_ADDRESS',
+            },
+        },
+    ],
+```
+Then run the following command:
+```bash
+pnpm run lz:sdk:move:init --lz-config move.layerzero.config.ts --move-deploy-script deploy/MyMoveOFTFA.ts
+```
+
+## Wire 
+Then run the wire command:
+
+For EVM:
+```bash
+pnpm run lz:sdk:evm:wire --lz-config move.layerzero.config.ts
+```
+For Move-VM:
+```bash
+pnpm run lz:sdk:move:wire --lz-config move.layerzero.config.ts
+```
+
+## Help
+```bash
+pnpm run lz:sdk:help
+```
+## EVM Deployment
+```bash
+npx hardhat lz:deploy
+```
+Select only the evm networks (DO NOT SELECT APTOS)
+For deploy script tags use: 
---
+++ packages/devtools-move/tasks/evm/wire-evm.ts
@@ -0,0 +1,152 @@
+import fs from 'fs'
+
+import { Contract, ethers } from 'ethers'
+
+import { getDeploymentAddressAndAbi } from '@layerzerolabs/lz-evm-sdk-v2'
+
+import { getEidFromAptosNetwork, getLzNetworkStage, parseYaml } from '../move/utils/aptosNetworkParser'
+import { getMoveVMOftAddress } from '../move/utils/utils'
+import { createEidToNetworkMapping, getConfigConnections, getHHAccountConfig } from '../shared/utils'
+
+import AnvilForkNode from './utils/anvilForkNode'
+import { createSetDelegateTransactions } from './wire/setDelegate'
+import { createSetEnforcedOptionsTransactions } from './wire/setEnforcedOptions'
+import { createSetPeerTransactions } from './wire/setPeer'
+import { createSetReceiveConfigTransactions } from './wire/setReceiveConfig'
+import { createSetReceiveLibraryTransactions } from './wire/setReceiveLibrary'
+import { createSetSendConfigTransactions } from './wire/setSendConfig'
+import { createSetSendLibraryTransactions } from './wire/setSendLibrary'
+import { executeTransactions } from './wire/transactionExecutor'
+
+import type { ContractMetadataMapping, NonEvmOAppMetadata, TxEidMapping } from './utils/types'
+import path from 'path'
+import dotenv from 'dotenv'
+
+/**
+ * @description Handles wiring of EVM contracts with the Aptos OApp
+ * @dev Creates ethers's populated transactions for the various transaction types (setPeer, setDelegate, setEnforcedOptions, setSendLibrary, setReceiveLibrary, setReceiveLibraryTimeout). It then simulates them on a forked network before executing
+ */
+async function wireEvm(args: any) {
+    const envPath = path.resolve(path.join(args.rootDir, '.env'))
+    const env = dotenv.config({ path: envPath })
+    if (!env.parsed || env.error?.message !== undefined) {
+        console.error('Failed to load .env file.')
+        process.exit(1)
+    }
+
+    const privateKey = env.parsed.EVM_PRIVATE_KEY
+
+    if (!privateKey) {
+        console.error('EVM_PRIVATE_KEY is not set in .env file')
+        process.exit(1)
+    }
+
+    const { network } = await parseYaml()
+    const EID_APTOS = getEidFromAptosNetwork('aptos', network)
+    const globalConfigPath = path.resolve(path.join(args.rootDir, args.lz_config))
+    // @todo grow connectionsToWire by taking in non-evm connections instead of only APTOS.
+    const connectionsToWire = await getConfigConnections('to', EID_APTOS, globalConfigPath)
+
+    const accountConfigs = await getHHAccountConfig(globalConfigPath)
+    const networks = await createEidToNetworkMapping('networkName')
+    const rpcUrls = await createEidToNetworkMapping('url')
+
+    // Build a Transaction mapping for each type of transaction. It is further indexed by the eid.
+    const TxTypeEidMapping: TxEidMapping = {
+        setPeer: {},
+        setDelegate: {},
+        setEnforcedOptions: {},
+        setSendLibrary: {},
+        setReceiveLibrary: {},
+        setReceiveLibraryTimeout: {},
+        sendConfig: {},
+        receiveConfig: {},
+    }
+
+    // Indexed by the eid it contains information about the contract, provider, and configuration of the account and oapp.
+    const contractMetaData: ContractMetadataMapping = {}
+
+    // @todo Use this as a primary key for NonEvmOAppWiring in the following code
+    const lzNetworkStage = getLzNetworkStage(network)
+    const APTOS_OAPP_ADDRESS = getMoveVMOftAddress(lzNetworkStage)
+
+    const nonEvmOapp: NonEvmOAppMetadata = {
+        address: APTOS_OAPP_ADDRESS,
+        eid: EID_APTOS.toString(),
+        rpc: rpcUrls[EID_APTOS],
+    }
+
+    /*
+     * Looping through the connections we build out the contractMetaData and TxTypeEidMapping by reading from the deployment files.
+     * contractMetaData contains ethers Contract objects for the OApp and EndpointV2 contracts.
+     */
+    for (const conn of connectionsToWire) {
+        const fromEid = conn.from.eid
+        const fromNetwork = networks[fromEid]
+        const configOapp = conn?.config
+
+        const provider = new ethers.providers.JsonRpcProvider(rpcUrls[fromEid])
+        const signer = new ethers.Wallet(privateKey, provider)
+
+        const OAppDeploymentPath = path.resolve(`deployments/${fromNetwork}/${conn.from.contractName}.json`)
+        const OAppDeploymentData = JSON.parse(fs.readFileSync(OAppDeploymentPath, 'utf8'))
+        const EndpointV2DeploymentData = getDeploymentAddressAndAbi(fromNetwork, 'EndpointV2')
+
+        const { address: oappAddress, abi: oappAbi } = OAppDeploymentData
+        const { address: epv2Address, abi: epv2Abi } = EndpointV2DeploymentData
+
+        const OAppContract = new Contract(oappAddress, oappAbi, signer)
+        const EPV2Contract = new Contract(epv2Address, epv2Abi, signer)
+
+        contractMetaData[fromEid] = {
+            address: {
+                oapp: oappAddress,
+                epv2: epv2Address,
+            },
+            contract: {
+                oapp: OAppContract,
+                epv2: EPV2Contract,
+            },
+            provider: provider,
+            configAccount: accountConfigs[fromEid],
+            configOapp: configOapp,
+        }
+    }
+
+    /*
+     */
+    TxTypeEidMapping.setPeer = await createSetPeerTransactions(contractMetaData, nonEvmOapp)
+    TxTypeEidMapping.setDelegate = await createSetDelegateTransactions(contractMetaData, nonEvmOapp)
+    TxTypeEidMapping.setEnforcedOptions = await createSetEnforcedOptionsTransactions(contractMetaData, nonEvmOapp)
+    TxTypeEidMapping.setSendLibrary = await createSetSendLibraryTransactions(contractMetaData, nonEvmOapp)
+    TxTypeEidMapping.setReceiveLibrary = await createSetReceiveLibraryTransactions(contractMetaData, nonEvmOapp)
+    TxTypeEidMapping.sendConfig = await createSetSendConfigTransactions(contractMetaData, nonEvmOapp)
+    TxTypeEidMapping.receiveConfig = await createSetReceiveConfigTransactions(contractMetaData, nonEvmOapp)
+
+    // TxTypeEidMapping.setReceiveLibraryTimeout = await createSetReceiveLibraryTimeoutTransactions(
+    //     contractMetaData,
+    //     nonEvmOapp
+    // )
+
+    // @todo Clean this up or move to utils
+    const rpcUrlSelfMap: { [eid: string]: string } = {}
+    for (const [eid, eidData] of Object.entries(contractMetaData)) {
+        rpcUrlSelfMap[eid] = eidData.provider.connection.url
+    }
+
+    const anvilForkNode = new AnvilForkNode(rpcUrlSelfMap, 8546)
+
+    try {
+        // const forkRpcMap = await anvilForkNode.startNodes()
+        // await executeTransactions(contractMetaData, TxTypeEidMapping, forkRpcMap, 'dry-run', privateKey)
+        // console.log('\nAll transactions have been SIMULATED on the blockchains.')
+        await executeTransactions(contractMetaData, TxTypeEidMapping, rpcUrlSelfMap, 'broadcast', privateKey)
+        console.log('\nAll transactions have been EXECUTED on the blockchains.')
+    } catch (error) {
+        anvilForkNode.killNodes()
+        throw error
+    }
+    anvilForkNode.killNodes()
+}
+
+export { wireEvm }
