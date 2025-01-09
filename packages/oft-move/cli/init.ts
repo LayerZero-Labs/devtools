ea5f3e3f63a3452ce839db027ed19664297f3db8
shankar <shankar@layerzerolabs.org>
2025-01-09 01:11:16 +0000

    update path
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/oft-move/cli/init.ts
+++ packages/oft-move/cli/init.ts
@@ -5,4 +5,5 @@ export async function attach_oft_move(sdk: AptosEVMCLI) {
     await sdk.extendOperationFromPath(path.join(__dirname, './operations/init-move-oft-fa'))
     await sdk.extendOperationFromPath(path.join(__dirname, './operations/quote-send-move-oft'))
     await sdk.extendOperationFromPath(path.join(__dirname, './operations/init-move-oft-fa-adapter'))
+    await sdk.extendOperationFromPath(path.join(__dirname, './operations/quote-send-oft-fa'))
 }
