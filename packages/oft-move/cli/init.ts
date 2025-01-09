4c849871a59464eee6bc8743990fa4879ec44f92
shankar <shankar@layerzerolabs.org>
2025-01-09 15:56:02 +0000

    remove duplicated path
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/oft-move/cli/init.ts
+++ packages/oft-move/cli/init.ts
@@ -5,5 +5,4 @@ export async function attach_oft_move(sdk: AptosEVMCLI) {
     await sdk.extendOperationFromPath(path.join(__dirname, './operations/init-move-oft-fa'))
     await sdk.extendOperationFromPath(path.join(__dirname, './operations/quote-send-move-oft'))
     await sdk.extendOperationFromPath(path.join(__dirname, './operations/init-move-oft-fa-adapter'))
-    await sdk.extendOperationFromPath(path.join(__dirname, './operations/quote-send-oft-fa'))
 }
