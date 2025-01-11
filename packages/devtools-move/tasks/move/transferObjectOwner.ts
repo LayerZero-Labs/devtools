f37559596eca4ce70ac553579abdeec64eeaf048
alexanderliteplo <alexanderliteplo@gmail.com>
2025-01-10 19:38:50 -0800

    adding transfer ownership functionality

---
+++ packages/devtools-move/tasks/move/transferObjectOwner.ts
@@ -0,0 +1,30 @@
+import { OFT } from '../../sdk/oft'
+
+import { getLzNetworkStage, parseYaml } from './utils/aptosNetworkParser'
+import { createTransferObjectOwnerPayload } from './utils/moveVMOftConfigOps'
+import { getMoveVMOftAddress, sendAllTxs } from './utils/utils'
+import { getChain } from '../../sdk/moveVMConnectionBuilder'
+import { getConnection } from '../../sdk/moveVMConnectionBuilder'
+
+async function transferObjectOwner(newOwner: string) {
+    const { account_address, private_key, network, fullnode, faucet } = await parseYaml()
+
+    const chain = getChain(fullnode)
+    const aptos = getConnection(chain, network, fullnode, faucet)
+
+    const lzNetworkStage = getLzNetworkStage(network)
+    const oftAddress = getMoveVMOftAddress(lzNetworkStage)
+    console.log(`\n👑 Transferring ${chain}-${lzNetworkStage} Object Ownership`)
+    console.log(`\tFor Object at: ${oftAddress}\n`)
+    console.log(`\tNew Owner: ${newOwner}\n`)
+
+    const oft = new OFT(aptos, oftAddress, account_address, private_key)
+
+    const transferOwnerPayload = createTransferObjectOwnerPayload(oftAddress, newOwner)
+
+    const payloads = [transferOwnerPayload]
+
+    sendAllTxs(aptos, oft, account_address, payloads)
+}
+
+export { transferObjectOwner }
