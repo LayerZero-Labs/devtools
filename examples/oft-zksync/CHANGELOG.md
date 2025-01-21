0b6766746c5a1d3ae66281d1e0e53129d7385720
Matthew Krak <krak@layerzerolabs.org>
2025-01-20 17:44:42 -0800

    chore: add oapp and oft zksolc examples

---
+++ examples/oft-zksync/CHANGELOG.md
@@ -0,0 +1,25 @@
+# @layerzerolabs/onft721-zksync-example
+
+## 1.0.3
+
+### Patch Changes
+
+- ce03876: Get latest EndpointIds by bumping lz-definitions
+
+## 1.0.2
+
+### Patch Changes
+
+- 1d2abff: new SDK methods, tests in devtools-ton, upgraded lz-definitions
+
+## 1.0.1
+
+### Patch Changes
+
+- 1bb0524: Upgraded dependency (@layerzerolabs/lz-definitions 3.0.12->3.0.21)
+
+## 1.0.0
+
+### Major Changes
+
+- fc1bc2f: adds a zkSolc compiler friendly example of ONFT721
---
+++ examples/oft-zksync/layerzero.config.ts
@@ -0,0 +1,79 @@
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
+
+import type { OAppEdgeConfig, OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+
+const abstractContract: OmniPointHardhat = {
+    eid: EndpointId.ABSTRACT_V2_TESTNET,
+    contractName: 'MyOFT',
+}
+
+const fujiContract: OmniPointHardhat = {
+    eid: EndpointId.AVALANCHE_V2_TESTNET,
+    contractName: 'MyOFT',
+}
+
+const zksyncContract: OmniPointHardhat = {
+    eid: EndpointId.ZKSYNCSEP_V2_TESTNET,
+    contractName: 'MyOFT',
+}
+
+const DEFAULT_EDGE_CONFIG: OAppEdgeConfig = {
+    // Gas can be profiled and enforced based on your contract's needs
+    enforcedOptions: [
+        {
+            msgType: 1,
+            optionType: ExecutorOptionType.LZ_RECEIVE,
+            gas: 100_000,
+            value: 0,
+        },
+    ],
+}
+
+const config: OAppOmniGraphHardhat = {
+    contracts: [
+        {
+            contract: fujiContract,
+        },
+        {
+            contract: abstractContract,
+        },
+        {
+            contract: zksyncContract,
+        },
+    ],
+    connections: [
+        {
+            from: fujiContract,
+            to: abstractContract,
+            config: DEFAULT_EDGE_CONFIG,
+        },
+        {
+            from: fujiContract,
+            to: zksyncContract,
+            config: DEFAULT_EDGE_CONFIG,
+        },
+        {
+            from: abstractContract,
+            to: fujiContract,
+            config: DEFAULT_EDGE_CONFIG,
+        },
+        {
+            from: abstractContract,
+            to: zksyncContract,
+            config: DEFAULT_EDGE_CONFIG,
+        },
+        {
+            from: zksyncContract,
+            to: abstractContract,
+            config: DEFAULT_EDGE_CONFIG,
+        },
+        {
+            from: zksyncContract,
+            to: fujiContract,
+            config: DEFAULT_EDGE_CONFIG,
+        },
+    ],
+}
+
+export default config
