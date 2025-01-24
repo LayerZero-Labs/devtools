7ad20fc43ba6f30ff4ad207d6e415e46015ff057
Matthew Krak <krak@layerzerolabs.org>
2025-01-24 15:57:32 -0800

    chore: update lockfile

--- examples/lzapp-migration/hardhat.config.ts
+++ examples/lzapp-migration/hardhat.config.ts
@@ -13,10 +13,6 @@ import { HardhatUserConfig, HttpNetworkAccountsUserConfig } from 'hardhat/types'
 // Import LayerZero DevTools functions
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 
-import '@layerzerolabs/devtools-evm-hardhat/type-extensions'
-import '@layerzerolabs/devtools-evm-hardhat/tasks'
-import '@layerzerolabs/ua-devtools-evm-hardhat/tasks'
-
 import './tasks/sendV1Message.ts'
 import './tasks/sendV2Message.ts'
 import './tasks/index.ts'
