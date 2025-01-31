31f67e00454ecbeb7bfda523c9554467f53c0ed9
Alexander Liteplo <65365446+AlexanderLiteplo@users.noreply.github.com>
2025-01-30 16:59:59 -0800

    Aptos-Move OApp example (#1188)
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>
    Signed-off-by: shankar <shankar@layerzerolabs.org>
    Co-authored-by: Austin Baltes <abaltes@layerzerolabs.org>
    Co-authored-by: Matthew Krak <krak@layerzerolabs.org>
    Co-authored-by: Ryan Goulding <goulding@layerzerolabs.org>
    Co-authored-by: shankar <shankar@layerzerolabs.org>

--- packages/devtools-move/cli/operations/transfer-object-owner.ts
+++ packages/devtools-move/cli/operations/transfer-object-owner.ts
@@ -5,10 +5,10 @@ class MoveTransferObjectOwnerOperation implements INewOperation {
     vm = 'move'
     operation = 'transfer-object-owner'
     description = 'Transfer Aptos Move OApp Ownership'
-    reqArgs = ['new_owner']
+    reqArgs = ['new_owner', 'oapp_config']
 
     async impl(args: any): Promise<void> {
-        await transferObjectOwner(args.new_owner)
+        await transferObjectOwner(args.new_owner, args.oapp_config)
     }
 }
 
