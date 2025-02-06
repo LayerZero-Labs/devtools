632ca54419389bf6bb4f5e602c0bd98f1b44bc35
nazreen <nazreen.mohamad@layerzerolabs.org>
2025-02-07 00:18:31 +0800

    can init config

--- examples/lzapp-migration/tasks/common/wire.ts
+++ examples/lzapp-migration/tasks/common/wire.ts
@@ -2,15 +2,27 @@ import { Keypair, PublicKey } from '@solana/web3.js'
 import { subtask, task } from 'hardhat/config'
 
 import { createSignAndSendFlow, firstFactory } from '@layerzerolabs/devtools'
-import { createConnectedContractFactory, createSignerFactory, inheritTask } from '@layerzerolabs/devtools-evm-hardhat'
+import { SUBTASK_LZ_SIGN_AND_SEND, createSignerFactory, inheritTask, types } from '@layerzerolabs/devtools-evm-hardhat'
+import { setTransactionSizeBuffer } from '@layerzerolabs/devtools-solana'
 import { type LogLevel, createLogger } from '@layerzerolabs/io-devtools'
 import { endpointIdToVersion } from '@layerzerolabs/lz-definitions'
-import { createLzAppFactory } from '@layerzerolabs/ua-devtools-evm'
-import { SUBTASK_LZ_OAPP_WIRE_CONFIGURE, TASK_LZ_OAPP_WIRE } from '@layerzerolabs/ua-devtools-evm-hardhat'
+import { IOApp, OAppConfigurator, OAppOmniGraph } from '@layerzerolabs/ua-devtools'
+import {
+    SUBTASK_LZ_OAPP_WIRE_CONFIGURE,
+    SubtaskConfigureTaskArgs,
+    TASK_LZ_OAPP_WIRE,
+} from '@layerzerolabs/ua-devtools-evm-hardhat'
 import { initOFTAccounts } from '@layerzerolabs/ua-devtools-solana'
 
 import { configureLzAppGraph } from './taskHelper'
 import { keyPair, publicKey } from './types'
+import { createSdkFactory, createSolanaConnectionFactory, createSolanaSignerFactory } from './utils'
+
+import type { SignAndSendTaskArgs } from '@layerzerolabs/devtools-evm-hardhat/tasks'
+
+function bigIntReplacer(_key: string, value: any): any {
+    return typeof value === 'bigint' ? value.toString() : value
+}
 
 /**
  * Filters graph connections where `from.eid` is an EndpointV1.
@@ -18,9 +30,13 @@ import { keyPair, publicKey } from './types'
  * @returns Filtered graph with only EndpointV1 connections.
  */
 const filterGraphForEndpointV1 = (graph: { connections: any[]; contracts: any[] }) => {
-    const filteredConnections = graph.connections.filter(
-        ({ vector: { from } }) => endpointIdToVersion(from.eid) === 'v1'
-    )
+    // const filteredConnections = graph.connections.filter(
+    //     ({ vector: { from } }) => endpointIdToVersion(from.eid) === 'v1'
+    // )
+    const filteredConnections = graph.connections.filter(({ vector: { from } }) => {
+        console.log({ from, filterGraphForEndpointV1Res: endpointIdToVersion(from.eid) === 'v1' })
+        return endpointIdToVersion(from.eid) === 'v1'
+    })
 
     return {
         ...graph,
@@ -51,7 +67,10 @@ interface Args {
     logLevel: LogLevel
     solanaProgramId: PublicKey
     solanaSecretKey?: Keypair
+    multisigKey?: PublicKey
     oappConfig: string
+    isSolanaInitConfig: boolean
+    internalConfigurator?: OAppConfigurator
 }
 
 /**
@@ -66,54 +85,157 @@ task(TASK_LZ_OAPP_WIRE)
         true
     )
     .addParam('solanaProgramId', 'The OFT program ID to use', undefined, publicKey, true)
+    .addParam('multisigKey', 'The MultiSig key', undefined, publicKey, true)
+    // We use this argument to get around the fact that we want to both override the task action for the wiring task
+    // and wrap this task with custom configurators
+    //
+    // By default, this argument will be left empty and the default OApp configurator will be used.
+    // The tasks that are using custom configurators will override this argument with the configurator of their choice
+    .addParam('internalConfigurator', 'FOR INTERNAL USE ONLY', undefined, types.fn, true)
+    .addParam('isSolanaInitConfig', 'FOR INTERNAL USE ONLY', undefined, types.boolean, true)
     .setAction(async (args: Args, hre, runSuper) => {
         const logger = createLogger(args.logLevel)
         logger.info('Starting LayerZero OApp wiring task...')
 
-        const evmContractFactory = createConnectedContractFactory()
-        const lzAppFactory = createLzAppFactory(evmContractFactory)
+        //
+        //
+        // ENVIRONMENT SETUP
+        //
+        //
 
-        // Merge EVM and Solana factories into a single SDK factory
-        const sdkFactory = firstFactory(lzAppFactory)
+        // The Solana transaction size estimation algorithm is not very accurate, so we increase its tolerance by 192 bytes
+        setTransactionSizeBuffer(192)
 
-        // Create a default signer factory
-        const createSigner = createSignerFactory()
-        // Override the configure subtask to include the filtered graph
-        subtask(SUBTASK_LZ_OAPP_WIRE_CONFIGURE, 'Configure OApp connections').setAction(
-            async (subtaskArgs, hre, runSuper) => {
-                const { graph } = subtaskArgs
+        //
+        //
+        // USER INPUT
+        //
+        //
 
-                // Filter the graph for EndpointV1 connections
-                const v1Graph = filterGraphForEndpointV1(graph)
+        // TODO: allow for no Solana mode
+        if (args.solanaSecretKey == null) {
+            logger.warn(
+                `Missing --solana-secret-key CLI argument. A random keypair will be generated and interaction with solana programs will not be possible`
+            )
+        }
 
-                logger.info(`[EndpointV1] Checking connections`)
+        // The first step is to create the user Keypair from the secret passed in
+        const wallet = args.solanaSecretKey ?? Keypair.generate()
+        const userAccount = wallet.publicKey
 
-                // Use the custom `configureLzAppTrustedRemotes` logic for the V1 graph
-                const setConfigurationTxs = await configureLzAppGraph(v1Graph, hre)
+        // Then we grab the programId from the args
+        const programId = args.solanaProgramId
 
-                const transactions = [...setConfigurationTxs]
+        if (!programId) {
+            logger.error('Missing --solana-program-id CLI argument')
+            return
+        }
 
-                if (transactions.length > 0) {
-                    logger.info(`Generated ${transactions.length} transactions for OApp configuration.`)
-                    await createSignAndSendFlow({ createSigner })({ transactions })
-                }
+        const configurator = args.internalConfigurator
 
-                if (transactions.length === 0) {
-                    logger.info('The LzApp is wired, no action is necessary')
-                }
+        //
+        //
+        // TOOLING SETUP
+        //
+        //
 
-                // Filter the graph for EndpointV2 connections
-                const v2Graph = filterGraphForEndpointV2(graph)
+        // We'll need a connection factory to be able to query the Solana network
+        //
+        // If you haven't set RPC_URL_SOLANA and/or RPC_URL_SOLANA_TESTNET environment variables,
+        // the factory will use the default public RPC URLs
+        const connectionFactory = createSolanaConnectionFactory()
 
-                logger.info(`[EndpointV2] checking connections`)
+        // We'll need SDKs to be able to use devtools
+        const sdkFactory = createSdkFactory(userAccount, programId, connectionFactory)
 
-                // Pass the filtered V2 graph to the original task logic
-                return runSuper({
-                    ...subtaskArgs,
-                    graph: v2Graph,
+        // We'll also need a signer factory
+        const solanaSignerFactory = createSolanaSignerFactory(wallet, connectionFactory, args.multisigKey)
+
+        //
+        //
+        // SUBTASK OVERRIDES
+        //
+        //
+
+        // We'll need to override the default implementation of the configure subtask
+        // (responsible for collecting the on-chain configuration of the contracts
+        // and coming up with the transactions that need to be sent to the network)
+        //
+        // The only thing we are overriding is the sdkFactory parameter - we supply the SDK factory we created above
+        subtask(
+            SUBTASK_LZ_OAPP_WIRE_CONFIGURE,
+            'Configure OFT',
+            (args: SubtaskConfigureTaskArgs<OAppOmniGraph, IOApp>, hre, runSuper) =>
+                runSuper({
+                    ...args,
+                    configurator: configurator ?? args.configurator,
+                    sdkFactory,
                 })
-            }
         )
+        // We'll also need to override the default implementation of the signAndSend subtask
+        // (responsible for sending transactions to the network and waiting for confirmations)
+        //
+        // In this subtask we need to override the createSigner function so that it uses the Solana
+        // signer for all Solana transactions
+        subtask(SUBTASK_LZ_SIGN_AND_SEND, 'Sign OFT transactions', (args: SignAndSendTaskArgs, _hre, runSuper) =>
+            runSuper({
+                ...args,
+                createSigner: firstFactory(solanaSignerFactory, args.createSigner),
+            })
+        )
+
+        //
+        // end of paste from oft-solana
+        //
+
+        if (!args.isSolanaInitConfig) {
+            // const evmContractFactory = createConnectedContractFactory()
+            // const lzAppFactory = createLzAppFactory(evmContractFactory)
+
+            // // Merge EVM and Solana factories into a single SDK factory
+            // const sdkFactory = firstFactory(lzAppFactory)
+
+            // Create a default signer factory
+            const createSigner = createSignerFactory()
+            // Override the configure subtask to include the filtered graph
+            subtask(SUBTASK_LZ_OAPP_WIRE_CONFIGURE, 'Configure OApp connections').setAction(
+                async (subtaskArgs, hre, runSuper) => {
+                    const { graph } = subtaskArgs
+
+                    // Filter the graph for EndpointV1 connections
+                    const v1Graph = filterGraphForEndpointV1(graph)
+
+                    logger.info(`[EndpointV1] Checking connections`)
+
+                    // Use the custom `configureLzAppTrustedRemotes` logic for the V1 graph
+                    // console.log(JSON.stringify(v1Graph, bigIntReplacer, 2))
+                    const setConfigurationTxs = await configureLzAppGraph(v1Graph, hre)
+
+                    const transactions = [...setConfigurationTxs]
+
+                    if (transactions.length > 0) {
+                        logger.info(`Generated ${transactions.length} transactions for OApp configuration.`)
+                        await createSignAndSendFlow({ createSigner })({ transactions })
+                    }
+
+                    if (transactions.length === 0) {
+                        logger.info('The LzApp is wired, no action is necessary')
+                    }
+
+                    // Filter the graph for EndpointV2 connections
+                    const v2Graph = filterGraphForEndpointV2(graph)
+
+                    logger.info(`[EndpointV2] checking connections`)
+
+                    // Pass the filtered V2 graph to the original task logic
+                    return runSuper({
+                        ...subtaskArgs,
+                        graph: v2Graph,
+                    })
+                }
+            )
+        }
+
         return runSuper(args)
     })
 
@@ -124,5 +246,5 @@ const wireLikeTask = inheritTask(TASK_LZ_OAPP_WIRE)
 wireLikeTask('lz:oapp:init:solana')
     .setDescription('Initialize OFT accounts for Solana')
     .setAction(async (args: Args, hre) =>
-        hre.run(TASK_LZ_OAPP_WIRE, { ...args, internalConfigurator: initOFTAccounts })
+        hre.run(TASK_LZ_OAPP_WIRE, { ...args, isSolanaInitConfig: true, internalConfigurator: initOFTAccounts })
     )
