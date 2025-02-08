86084c9f8c69abb1818bd4cd856957995abe2a81
nazreen <nazreen.mohamad@layerzerolabs.org>
2025-02-08 22:36:07 +0800

    setup endpoint v1 oft

---
+++ examples/lzapp-migration/contracts/MyEndpointV1OFTV2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@layerzerolabs/solidity-examples/contracts/token/oft/v2/OFTV2.sol";
+
+contract MyEndpointV1OFTV2 is OFTV2 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _sharedDecimals,
+        address _lzEndpoint,
+        address _owner
+    ) OFTV2(_name, _symbol, _sharedDecimals, _lzEndpoint) {}
+}
