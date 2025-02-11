15dd4108940a5f3cd6fba427091bb00b01fa859a
nazreen <nazreen.mohamad@layerzerolabs.org>
2025-02-11 16:11:24 +0000

    lint:fix

--- examples/lzapp-migration/contracts/MyEndpointV1OFTV2.sol
+++ examples/lzapp-migration/contracts/MyEndpointV1OFTV2.sol
@@ -6,7 +6,7 @@ import { OFTV2 } from "@layerzerolabs/solidity-examples/contracts/token/oft/v2/O
 /// @title MyEndpointV1OFTV2
 /// @notice **This contract is an EndpointV1 OFT implementation** and should not be used for new OFT deployments.
 /// @dev The name `OFTV2`refers to the V2 implementation of the OFT on **LayerZero EndpointV1** and **not** **LayerZero EndpointV2**
-///      The `solidity-examples` repo is exclusively for EndpointV1 OFT 
+///      The `solidity-examples` repo is exclusively for EndpointV1 OFT
 contract MyEndpointV1OFTV2 is OFTV2 {
     /// @notice Initializes an OFT token with LayerZero's EndpointV1.
     /// @param _name The name of the token.
--- examples/lzapp-migration/contracts/mocks/MyEndpointV1OFTV2Mock.sol
+++ examples/lzapp-migration/contracts/mocks/MyEndpointV1OFTV2Mock.sol
@@ -7,7 +7,7 @@ import { OFTV2 } from "@layerzerolabs/solidity-examples/contracts/token/oft/v2/O
 /// @title MyEndpointV1OFTV2Mock
 /// @notice **This contract is an EndpointV1 OFT implementation** and should not be used for new OFT deployments.
 /// @dev The name `OFTV2`refers to the V2 implementation of the OFT on **LayerZero EndpointV1** and **not** **LayerZero EndpointV2**.
-///      The `solidity-examples` repo is exclusively for EndpointV1 OFT 
+///      The `solidity-examples` repo is exclusively for EndpointV1 OFT
 contract MyEndpointV1OFTV2Mock is OFTV2 {
     constructor(
         address _layerZeroEndpoint,
