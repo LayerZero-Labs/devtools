41b0b1672d16f259c4140387667a819f239454f7
alexanderliteplo <alexanderliteplo@gmail.com>
2025-02-12 13:11:37 -0800

    fixing build issues

--- packages/protocol-devtools-evm/src/uln302/schema.ts
+++ packages/protocol-devtools-evm/src/uln302/schema.ts
@@ -12,7 +12,7 @@ import type { Uln302ExecutorConfigInput, Uln302UlnConfigInput } from './types'
  */
 export const Uln302UlnConfigSchema = Uln302UlnConfigSchemaBase.extend({
     confirmations: BigNumberishBigIntSchema,
-    optionalDVNThreshold: z.number().optional(),
+    optionalDVNThreshold: z.number(),
 }) satisfies z.ZodSchema<Uln302UlnConfig, z.ZodTypeDef, Uln302UlnConfigInput>
 
 /**
