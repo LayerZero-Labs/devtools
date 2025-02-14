8b0159fdc4b45f04a4063b3f3e076f5b146022aa
DanL0 <dan@layerzerolabs.org>
2025-02-14 10:26:39 +0100

    improve initialization logic in upgradeable oft

--- examples/oft-upgradeable/contracts/MyOFTAdapterUpgradeable.sol
+++ examples/oft-upgradeable/contracts/MyOFTAdapterUpgradeable.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.22;
 import { OFTAdapterUpgradeable } from "@layerzerolabs/oft-evm-upgradeable/contracts/oft/OFTAdapterUpgradeable.sol";
 
 contract MyOFTAdapterUpgradeable is OFTAdapterUpgradeable {
-    constructor(address _token, address _lzEndpoint) OFTAdapterUpgradeable(_token, _lzEndpoint) {}
+    constructor(address _token, address _lzEndpoint) OFTAdapterUpgradeable(_token, _lzEndpoint) {
+        _disableInitializers();
+    }
 
     function initialize(address _delegate) public initializer {
         __OFTAdapter_init(_delegate);
