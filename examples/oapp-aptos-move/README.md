912d9c66ec658e3f1f87575e95233886bb4dd25c
alexanderliteplo <alexanderliteplo@gmail.com>
2025-02-18 15:15:03 -0800

    merging initia and movement changes into main

--- examples/oapp-aptos-move/README.md
+++ examples/oapp-aptos-move/README.md
@@ -1,28 +1,53 @@
-## Move-VM OApp Setup and Deployment
+## Move-VM OFT Setup and Deployment
 
-### Connecting to Aptos via CLI
+### connecting to aptos via cli
 
 To install aptos cli, run the following command:
 
-```
+```bash
 brew install aptos
+```
+
+> **Important:** Version requirements:
+>
+> - For deploying to Aptos chain: Use version >= 6.0.1 (installable via brew)
+> - For deploying to Movement chain: Use version <= 3.5.0 (must be built from source following the [Aptos CLI Build Guide](https://aptos.dev/en/network/nodes/building-from-source/))
+
+After installing the aptos cli, you can set the aptos cli path in the .env file. This allows you to have two different aptos cli versions installed on your machine and not have to switch between them for deployments.
+
+For example:
+
+```bash
+# if installed via homebrew, the aptos command will be available globally so this can be set to just aptos:
+APTOS_COMPATIBLE_APTOS_CLI_PATH=aptos
+
+# if installed from source the aptos command path to the aptos binary must be specified:
+MOVEMENT_COMPATIBLE_APTOS_CLI_PATH=/Users/your-username/Documents/aptos-core/target/cli/aptos
 ```
 
 If you need to generate a new key, run the following command:
 
-```
+```bash
 aptos key generate --output-file my_key.pub
 ```
 
 Then initialize the aptos cli and connect to the aptos network:
 
-```
+For Aptos Chain:
+
+```bash
 aptos init --network=testnet --private-key=<your-private-key>
+```
+
+For Movement Chain:
+
+```bash
+aptos init --network=custom --private-key=<your-private-key>
 ```
 
 You can then verify that your initialization was successful by running the following command:
 
-```
+```bash
 cat .aptos/config.yaml
 ```
 
@@ -32,59 +57,50 @@ Note: Your private key is stored in the .aptos/config.yaml file and will be extr
 
 ## Setup
 
-Run `pnpm i` in this folder to install the dependencies.
-
 Create a `.env` file with the following variables:
 
 ```bash
-ACCOUNT_ADDRESS=<your-aptos-account-address>
+# If you are deploying to Movement chain
+MOVEMENT_INDEXER_URL=https://indexer.testnet.movementnetwork.xyz/v1/graphql
+MOVEMENT_FULLNODE_URL=https://aptos.testnet.bardock.movementlabs.xyz/v1
+MOVEMENT_ACCOUNT_ADDRESS=<your-movement-account-address>
+MOVEMENT_PRIVATE_KEY=<your-movement-private-key>
+MOVEMENT_COMPATIBLE_APTOS_CLI_PATH=<path-to-aptos-cli>
+
 EVM_PRIVATE_KEY=<your-evm-private-key>
+MNEMONIC=<your-mnemonic>
+
+# If you are deploying to Aptos chain, the indexer and fullnode urls are fetched by the Aptos SDK and do not need to be specified.
+APTOS_ACCOUNT_ADDRESS=<your-aptos-account-address>
+APTOS_PRIVATE_KEY=<your-aptos-private-key>
+APTOS_COMPATIBLE_APTOS_CLI_PATH=<path-to-aptos-cli>
 ```
 
-Then run `source .env` in order for your values to be mapped to `$APTOS_ACCOUNT_ADDRESS` and `$EVM_PRIVATE_KEY`
+Then run `source .env` in order for your values to be mapped.
 
-Note: aptos account address can be found in .aptos/config.yaml
+Note: the Movement specific values can be found at: https://docs.movementnetwork.xyz/devs/networkEndpoints#movement-bardock-testnet-aptos-environment and currently Bardock testnet is the only Movement testnet with a deployed layerzero endpoint.
 
-## Build and deploy Aptos move modules
+### Build and Deploy the modules
 
-Note: to overwrite previous deploy and build, you can use `--force-build true` for the build script and `--force-deploy true` for the deploy script.
-
-### Build the modules
-
-```bash
-pnpm run lz:sdk:move:build --oapp-config move.layerzero.config.ts --named-addresses oapp=$APTOS_ACCOUNT_ADDRESS,oapp_admin=$APTOS_ACCOUNT_ADDRESS
-```
-
-### Deploy the modules
-
-```bash
-pnpm run lz:sdk:move:deploy --oapp-config move.layerzero.config.ts --address-name oapp --named-addresses oapp=$APTOS_ACCOUNT_ADDRESS,oapp_admin=$APTOS_ACCOUNT_ADDRESS
-```
-
-## Set Delegate
-
-Before running the wire command, first inside of move.layerzero.config.ts, set the delegate address to your account address.
+Before running the deploy and wire commands, first inside of `move.layerzero.config.ts`, set the delegate and owner address to your deployer account address. These can be changed in the future with commands shown later in this README, but for now they should be set to the address you will be running the commands from (deployer account address).
 
 ```ts
     contracts: [
         {
-            contract: bscContract,
+            contract: your_contract_name,
             config: {
-                owner: 'YOUR_EVM_ACCOUNT_ADDRESS',
-                delegate: 'YOUR_EVM_ACCOUNT_ADDRESS',
-            },
-        },
-        {
-            contract: aptosContract,
-            config: {
-                delegate: 'YOUR_APTOS_ACCOUNT_ADDRESS',
-                owner: 'YOUR_APTOS_ACCOUNT_ADDRESS',
+                delegate: 'YOUR_ACCOUNT_ADDRESS',
+                owner: 'YOUR_ACCOUNT_ADDRESS',
             },
         },
     ],
 ```
 
-Then run the following command:
+```bash
+pnpm run lz:sdk:move:deploy --oapp-config move.layerzero.config.ts --address-name oapp --oapp-type oapp
+```
+
+## Set Delegate
 
 ```bash
 pnpm run lz:sdk:move:set-delegate --oapp-config move.layerzero.config.ts
@@ -116,17 +132,27 @@ Ensure that in move.layerzero.config.ts, all of your evm contracts have the owne
     ]
 ```
 
-Commands:
+If you are wiring solana to move-vm, create a file in `deployments/solana-mainnet/MyOFT.json` (or `deployments/solana-testnet/MyOFT.json` if you are using testnet) and add the following field:
 
-```bash
-pnpm run lz:sdk:evm:wire --oapp-config move.layerzero.config.ts [--simulate true] [--mnemonic-index 0]
+```json
+{
+    "address": <oft-store-address-from-solana-deployment-folder>
+}
 ```
 
---simulate <true> and --mnemonic-index <value> are optional.
---mnemonic-index <value> is the index of the mnemonic to use for the EVM account. If not specified, EVM_PRIVATE_KEY from .env is used. else the mnemonic is used along with the index.
---only-calldata <true> is optional. If specified, only the calldata is generated and not the transaction (this is primarily for multisig wallets).
+Commands:
 
-For Move-VM:
+To wire from EVM to Move-VM:
+
+```bash
+pnpm run lz:sdk:evm:wire --oapp-config move.layerzero.config.ts
+```
+
+Note: `--simulate <true>` and `--mnemonic-index <value>` are optional.
+`--mnemonic-index <value>` is the index of the mnemonic to use for the EVM account. If not specified, EVM_PRIVATE_KEY from `.env` is used. Otherwise, the mnemonic is used along with the index.
+If `--only-calldata <true>` is specified, only the calldata is generated and not the transaction (this is primarily for multisig wallets).
+
+To wire from Move-VM to EVM:
 
 ```bash
 pnpm run lz:sdk:move:wire --oapp-config move.layerzero.config.ts
@@ -180,7 +206,7 @@ Note: The object owner has the upgrade authority for the Object.
 pnpm run lz:sdk:help
 ```
 
-Select only the evm networks you want to deploy to(DO NOT SELECT APTOS or MOVEMENT)
+Select only the evm networks (DO NOT SELECT APTOS or MOVEMENT)
 
 ### Verifying successful ownership transfer of your Move-VM OFT:
 
@@ -189,7 +215,7 @@ Run the following command:
 ```bash
 aptos account list \
   --account <OBJECT_ADDRESS> \
-  --url https://fullnode.testnet.aptoslabs.com \
+  --url <your-fullnode-url> \
   --query resources
 ```
 
@@ -222,3 +248,38 @@ For verifying the admin look for the following in the output:
 ```
 
 If the admin is your desired address, then the ownership transfer was successful.
+
+
+## Multisig Transaction Execution
+
+To execute transactions with a multisig account via the aptos CLI, follow these steps:
+
+Run the CLI command and select `(e)xport - save as JSON for multisig execution` when prompted. This will save a JSON file to the transactions folder.
+
+Using Aptos CLI:
+
+1. Create the transaction using:
+
+```bash
+aptos multisig create-transaction \
+    --json-file <path-to-json-file> \
+    --multisig-address <your-multisig-address> \
+    --private-key-file <path-to-private-key> \
+    --assume-yes
+```
+
+2. Approve the transaction:
+
+```bash
+aptos multisig approve \
+    --multisig-address <your-multisig-address> \
+    --sequence-number <your-sequence-number> \
+    --private-key-file <path-to-private-key> \
+    --assume-yes
+```
+
+For more detailed information about multisig transactions, please refer to the [Aptos Multi-Signature Tutorial](https://aptos.dev/en/build/cli/working-with-move-contracts/multi-signature-tutorial#execute-the-governance-parameter-transaction).
+
+Using Rimosafe:
+
+Follow the instructions here: https://docs.rimosafe.com/docs/introduction
--- examples/oft-aptos-move/move.layerzero.config.ts
+++ examples/oft-aptos-move/move.layerzero.config.ts
@@ -13,6 +13,11 @@ const bscContract: OmniPointHardhat = {
     contractName: 'MyOFT',
 }
 
+const movementContract: OmniPointHardhat = {
+    eid: EndpointId.MOVEMENT_V2_TESTNET,
+    contractName: 'MyOFT',
+}
+
 const aptosContract: OmniPointHardhat = {
     eid: EndpointId.APTOS_V2_TESTNET,
     contractName: 'MyOFT',
@@ -28,14 +33,88 @@ const config: OAppOmniGraphHardhat = {
             },
         },
         {
+            contract: movementContract,
+            config: {
+                delegate: '931fa60697f87560ba7cce0dd72e12ac8b797b8b35a823974e9c0fc6dd3b36b6',
+                owner: '931fa60697f87560ba7cce0dd72e12ac8b797b8b35a823974e9c0fc6dd3b36b6',
+            },
+        },
+        {
             contract: aptosContract,
             config: {
-                delegate: '',
-                owner: '',
+                delegate: '58b730d07e98a22f2b357bee721115c986e4dc873c1884763708ee3d4006f74e',
+                owner: '58b730d07e98a22f2b357bee721115c986e4dc873c1884763708ee3d4006f74e',
             },
         },
     ],
     connections: [
+        {
+            from: movementContract,
+            to: bscContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: MsgType.SEND,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
+                        value: 0, // msg.value in wei for EndpointV2.lzReceive
+                    },
+                    {
+                        msgType: MsgType.SEND_AND_CALL,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
+                        value: 0, // msg.value in wei for EndpointV2.lzReceive
+                    },
+                ],
+                sendLibrary: '0xcc1c03aed42e2841211865758b5efe93c0dde2cb7a2a5dc6cf25a4e33ad23690',
+                receiveLibraryConfig: {
+                    // Required Receive Library Address on Aptos
+                    receiveLibrary: '0xcc1c03aed42e2841211865758b5efe93c0dde2cb7a2a5dc6cf25a4e33ad23690',
+                    // Optional Grace Period for Switching Receive Library Address on Aptos
+                    gracePeriod: BigInt(0),
+                },
+                // Optional Receive Library Timeout for when the Old Receive Library Address will no longer be valid on Aptos
+                // receiveLibraryTimeoutConfig: {
+                //     lib: '0xbe533727aebe97132ec0a606d99e0ce137dbdf06286eb07d9e0f7154df1f3f10',
+                //     expiry: BigInt(1000000000),
+                // },
+                sendConfig: {
+                    executorConfig: {
+                        maxMessageSize: 10_000,
+                        // The configured Executor address on Aptos
+                        executor: '0x93353700091200ef9fdc536ce6a86182cc7e62da25f94356be9421c6310b9585',
+                    },
+                    ulnConfig: {
+                        // The number of block confirmations to wait on Aptos before emitting the message from the source chain.
+                        confirmations: BigInt(5),
+                        // The address of the DVNs you will pay to verify a sent message on the source chain.
+                        // The destination tx will wait until ALL `requiredDVNs` verify the message.
+                        requiredDVNs: ['0x756f8ab056688d22687740f4a9aeec3b361170b28d08b719e28c4d38eed1043e'],
+                        // The address of the DVNs you will pay to verify a sent message on the source chain.
+                        // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
+                        optionalDVNs: [],
+                        // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+                        optionalDVNThreshold: 0,
+                    },
+                },
+                // Optional Receive Configuration
+                // @dev Controls how the `from` chain receives messages from the `to` chain.
+                receiveConfig: {
+                    ulnConfig: {
+                        // The number of block confirmations to expect from the `to` chain.
+                        confirmations: BigInt(5),
+                        // The address of the DVNs your `receiveConfig` expects to receive verifications from on the `from` chain.
+                        // The `from` chain's OApp will wait until the configured threshold of `requiredDVNs` verify the message.
+                        requiredDVNs: ['0x756f8ab056688d22687740f4a9aeec3b361170b28d08b719e28c4d38eed1043e'],
+                        // The address of the `optionalDVNs` you expect to receive verifications from on the `from` chain.
+                        // The destination tx will wait until the configured threshold of `optionalDVNs` verify the message.
+                        optionalDVNs: [],
+                        // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+                        optionalDVNThreshold: 0,
+                    },
+                },
+            },
+        },
         {
             from: aptosContract,
             to: bscContract,
@@ -103,6 +182,140 @@ const config: OAppOmniGraphHardhat = {
                 },
             },
         },
+        // {
+        //     from: movementContract,
+        //     to: aptosContract,
+        //     config: {
+        //         enforcedOptions: [
+        //             {
+        //                 msgType: MsgType.SEND,
+        //                 optionType: ExecutorOptionType.LZ_RECEIVE,
+        //                 gas: 5_000, // gas limit in wei for EndpointV2.lzReceive
+        //                 value: 0, // msg.value in wei for EndpointV2.lzReceive
+        //             },
+        //             {
+        //                 msgType: MsgType.SEND_AND_CALL,
+        //                 optionType: ExecutorOptionType.LZ_RECEIVE,
+        //                 gas: 5_000, // gas limit in wei for EndpointV2.lzReceive
+        //                 value: 0, // msg.value in wei for EndpointV2.lzReceive
+        //             },
+        //         ],
+        //         sendLibrary: '0xcc1c03aed42e2841211865758b5efe93c0dde2cb7a2a5dc6cf25a4e33ad23690',
+        //         receiveLibraryConfig: {
+        //             // Required Receive Library Address on Aptos
+        //             receiveLibrary: '0xcc1c03aed42e2841211865758b5efe93c0dde2cb7a2a5dc6cf25a4e33ad23690',
+        //             // Optional Grace Period for Switching Receive Library Address on Aptos
+        //             gracePeriod: BigInt(0),
+        //         },
+        //         // Optional Receive Library Timeout for when the Old Receive Library Address will no longer be valid on Aptos
+        //         // receiveLibraryTimeoutConfig: {
+        //         //     lib: '0xbe533727aebe97132ec0a606d99e0ce137dbdf06286eb07d9e0f7154df1f3f10',
+        //         //     expiry: BigInt(1000000000),
+        //         // },
+        //         sendConfig: {
+        //             executorConfig: {
+        //                 maxMessageSize: 10_000,
+        //                 // The configured Executor address on Aptos
+        //                 executor: '0x93353700091200ef9fdc536ce6a86182cc7e62da25f94356be9421c6310b9585',
+        //             },
+        //             ulnConfig: {
+        //                 // The number of block confirmations to wait on Aptos before emitting the message from the source chain.
+        //                 confirmations: BigInt(5),
+        //                 // The address of the DVNs you will pay to verify a sent message on the source chain.
+        //                 // The destination tx will wait until ALL `requiredDVNs` verify the message.
+        //                 requiredDVNs: ['0x756f8ab056688d22687740f4a9aeec3b361170b28d08b719e28c4d38eed1043e'],
+        //                 // The address of the DVNs you will pay to verify a sent message on the source chain.
+        //                 // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
+        //                 optionalDVNs: [],
+        //                 // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+        //                 optionalDVNThreshold: 0,
+        //             },
+        //         },
+        //         // Optional Receive Configuration
+        //         // @dev Controls how the `from` chain receives messages from the `to` chain.
+        //         receiveConfig: {
+        //             ulnConfig: {
+        //                 // The number of block confirmations to expect from the `to` chain.
+        //                 confirmations: BigInt(5),
+        //                 // The address of the DVNs your `receiveConfig` expects to receive verifications from on the `from` chain.
+        //                 // The `from` chain's OApp will wait until the configured threshold of `requiredDVNs` verify the message.
+        //                 requiredDVNs: ['0x756f8ab056688d22687740f4a9aeec3b361170b28d08b719e28c4d38eed1043e'],
+        //                 // The address of the `optionalDVNs` you expect to receive verifications from on the `from` chain.
+        //                 // The destination tx will wait until the configured threshold of `optionalDVNs` verify the message.
+        //                 optionalDVNs: [],
+        //                 // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+        //                 optionalDVNThreshold: 0,
+        //             },
+        //         },
+        //     },
+        // },
+        // {
+        //     from: aptosContract,
+        //     to: movementContract,
+        //     config: {
+        //         enforcedOptions: [
+        //             {
+        //                 msgType: MsgType.SEND,
+        //                 optionType: ExecutorOptionType.LZ_RECEIVE,
+        //                 gas: 5_000, // gas limit in wei for EndpointV2.lzReceive
+        //                 value: 0, // msg.value in wei for EndpointV2.lzReceive
+        //             },
+        //             {
+        //                 msgType: MsgType.SEND_AND_CALL,
+        //                 optionType: ExecutorOptionType.LZ_RECEIVE,
+        //                 gas: 5_000, // gas limit in wei for EndpointV2.lzReceive
+        //                 value: 0, // msg.value in wei for EndpointV2.lzReceive
+        //             },
+        //         ],
+        //         sendLibrary: '0xcc1c03aed42e2841211865758b5efe93c0dde2cb7a2a5dc6cf25a4e33ad23690',
+        //         receiveLibraryConfig: {
+        //             // Required Receive Library Address on Aptos
+        //             receiveLibrary: '0xcc1c03aed42e2841211865758b5efe93c0dde2cb7a2a5dc6cf25a4e33ad23690',
+        //             // Optional Grace Period for Switching Receive Library Address on Aptos
+        //             gracePeriod: BigInt(0),
+        //         },
+        //         // Optional Receive Library Timeout for when the Old Receive Library Address will no longer be valid on Aptos
+        //         // receiveLibraryTimeoutConfig: {
+        //         //     lib: '0xbe533727aebe97132ec0a606d99e0ce137dbdf06286eb07d9e0f7154df1f3f10',
+        //         //     expiry: BigInt(1000000000),
+        //         // },
+        //         sendConfig: {
+        //             executorConfig: {
+        //                 maxMessageSize: 10_000,
+        //                 // The configured Executor address on Aptos
+        //                 executor: '0x93353700091200ef9fdc536ce6a86182cc7e62da25f94356be9421c6310b9585',
+        //             },
+        //             ulnConfig: {
+        //                 // The number of block confirmations to wait on Aptos before emitting the message from the source chain.
+        //                 confirmations: BigInt(5),
+        //                 // The address of the DVNs you will pay to verify a sent message on the source chain.
+        //                 // The destination tx will wait until ALL `requiredDVNs` verify the message.
+        //                 requiredDVNs: ['0x756f8ab056688d22687740f4a9aeec3b361170b28d08b719e28c4d38eed1043e'],
+        //                 // The address of the DVNs you will pay to verify a sent message on the source chain.
+        //                 // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
+        //                 optionalDVNs: [],
+        //                 // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+        //                 optionalDVNThreshold: 0,
+        //             },
+        //         },
+        //         // Optional Receive Configuration
+        //         // @dev Controls how the `from` chain receives messages from the `to` chain.
+        //         receiveConfig: {
+        //             ulnConfig: {
+        //                 // The number of block confirmations to expect from the `to` chain.
+        //                 confirmations: BigInt(5),
+        //                 // The address of the DVNs your `receiveConfig` expects to receive verifications from on the `from` chain.
+        //                 // The `from` chain's OApp will wait until the configured threshold of `requiredDVNs` verify the message.
+        //                 requiredDVNs: ['0x756f8ab056688d22687740f4a9aeec3b361170b28d08b719e28c4d38eed1043e'],
+        //                 // The address of the `optionalDVNs` you expect to receive verifications from on the `from` chain.
+        //                 // The destination tx will wait until the configured threshold of `optionalDVNs` verify the message.
+        //                 optionalDVNs: [],
+        //                 // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+        //                 optionalDVNThreshold: 0,
+        //             },
+        //         },
+        //     },
+        // },
         {
             from: bscContract,
             to: aptosContract,
@@ -143,7 +356,54 @@ const config: OAppOmniGraphHardhat = {
                 },
                 receiveConfig: {
                     ulnConfig: {
-                        confirmations: BigInt(260),
+                        confirmations: BigInt(5),
+                        requiredDVNs: ['0x0eE552262f7B562eFcED6DD4A7e2878AB897d405'],
+                        optionalDVNThreshold: 0,
+                    },
+                },
+            },
+        },
+        {
+            from: bscContract,
+            to: movementContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: MsgType.SEND,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 5_000, // gas limit in wei for EndpointV2.lzReceive
+                        value: 0, // msg.value in wei for EndpointV2.lzReceive
+                    },
+                    {
+                        msgType: MsgType.SEND_AND_CALL,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 5_000, // gas limit in wei for EndpointV2.lzCompose
+                        value: 0, // msg.value in wei for EndpointV2.lzCompose
+                    },
+                ],
+                sendLibrary: '0x55f16c442907e86D764AFdc2a07C2de3BdAc8BB7',
+                receiveLibraryConfig: {
+                    receiveLibrary: '0x188d4bbCeD671A7aA2b5055937F79510A32e9683',
+                    gracePeriod: BigInt(0),
+                },
+                // receiveLibraryTimeoutConfig: {
+                //     lib: '0x188d4bbCeD671A7aA2b5055937F79510A32e9683',
+                //     expiry: BigInt(67323472),
+                // },
+                sendConfig: {
+                    executorConfig: {
+                        maxMessageSize: 10_000,
+                        executor: '0x31894b190a8bAbd9A067Ce59fde0BfCFD2B18470',
+                    },
+                    ulnConfig: {
+                        confirmations: BigInt(5),
+                        requiredDVNs: ['0x0eE552262f7B562eFcED6DD4A7e2878AB897d405'],
+                        optionalDVNThreshold: 0,
+                    },
+                },
+                receiveConfig: {
+                    ulnConfig: {
+                        confirmations: BigInt(5),
                         requiredDVNs: ['0x0eE552262f7B562eFcED6DD4A7e2878AB897d405'],
                         optionalDVNThreshold: 0,
                     },
