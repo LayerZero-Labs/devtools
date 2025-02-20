922acea52f0ad23077651af16365a187aa528721
alexanderliteplo <alexanderliteplo@gmail.com>
2025-02-20 12:31:13 -0800

    fixing hardhat configs

--- examples/oft-adapter-aptos-move/hardhat.config.ts
+++ examples/oft-adapter-aptos-move/hardhat.config.ts
@@ -52,19 +52,9 @@ const config: HardhatUserConfig = {
         ],
     },
     networks: {
-        'movement-testnet': {
-            eid: EndpointId.MOVEMENT_V2_TESTNET,
-            url: process.env.RPC_URL_MOVEMENT_TESTNET || 'https://rpc.ankr.com/http/movement_testnet/v1[1]',
-            accounts,
-        },
         'bsc-testnet': {
             eid: EndpointId.BSC_V2_TESTNET,
             url: process.env.RPC_URL_BSC || 'https://bsc-testnet.public.blastapi.io',
-            accounts,
-        },
-        'aptos-testnet': {
-            eid: EndpointId.APTOS_V2_TESTNET,
-            url: process.env.RPC_URL_APTOS_TESTNET || 'https://rpc.ankr.com/http/aptos_testnet/v1[1]',
             accounts,
         },
         'eth-testnet': {
