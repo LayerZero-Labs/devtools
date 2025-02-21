389d6c923844a40eaaf6ffea291200a7a0270db8
Shankar Subramanian <shankar@layerzerolabs.org>
2025-02-21 13:16:38 -0800

    🧹 oft  upgradeable example deploy script (#1317)
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-upgradeable/deploy/MyOFTUpgradeable.ts
+++ examples/oft-upgradeable/deploy/MyOFTUpgradeable.ts
@@ -1,6 +1,6 @@
-import { Contract } from 'ethers'
 import { type DeployFunction } from 'hardhat-deploy/types'
 
+import { EndpointId, endpointIdToNetwork } from '@layerzerolabs/lz-definitions'
 import { getDeploymentAddressAndAbi } from '@layerzerolabs/lz-evm-sdk-v2'
 
 const contractName = 'MyOFTUpgradeable'
@@ -10,12 +10,14 @@ const deploy: DeployFunction = async (hre) => {
     const signer = (await hre.ethers.getSigners())[0]
     console.log(`deploying ${contractName} on network: ${hre.network.name} with ${signer.address}`)
 
-    const { address, abi } = getDeploymentAddressAndAbi(hre.network.name, 'EndpointV2')
-    const endpointV2Deployment = new Contract(address, abi, signer)
+    const eid = hre.network.config.eid as EndpointId
+    const lzNetworkName = endpointIdToNetwork(eid)
+
+    const { address } = getDeploymentAddressAndAbi(lzNetworkName, 'EndpointV2')
 
     await deploy(contractName, {
         from: signer.address,
-        args: [endpointV2Deployment.address],
+        args: [address],
         log: true,
         waitConfirmations: 1,
         skipIfAlreadyDeployed: false,
