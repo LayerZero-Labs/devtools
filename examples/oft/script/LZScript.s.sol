b6f6e3cfa35f9d54bbf02d27a05e558dc8455278
shankar <shankar@layerzerolabs.org>
2025-02-28 00:46:12 +0000

    remove base foundry Script import
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft/script/LZScript.s.sol
+++ examples/oft/script/LZScript.s.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 
 import { LZScripts } from "@layerzerolabs/script-devtools-evm-foundry/script/LZScripts.s.sol";
 
-contract LZScript is Script, LZScripts {
+contract LZScript is LZScripts {
     function run() public pure override {
         console.log("Hello, World!");
     }
