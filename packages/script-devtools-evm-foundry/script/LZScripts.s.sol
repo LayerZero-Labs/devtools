ee65560c7b29cc2556cce96431ac42af03e1a6fb
shankar <shankar@layerzerolabs.org>
2025-02-28 00:33:38 +0000

    make run() virtual
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/script-devtools-evm-foundry/script/LZScripts.s.sol
+++ packages/script-devtools-evm-foundry/script/LZScripts.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { SimulateUtils } from "./SimulateReceive/utils.sol";
+import { SimulateScript } from "./SimulateReceive/utils.sol";
 
 abstract contract LZScripts is SimulateUtils {
     function IS_WORKING() public pure returns (bool) {
