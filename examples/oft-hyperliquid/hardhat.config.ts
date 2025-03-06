03d843035a6d56128e4adf00df72917ea577705c
shankar <shankar@layerzerolabs.org>
2025-03-06 00:59:00 +0000

    using the new hyperliquid package
    Signed-off-by: shankar <shankar@layerzerolabs.org>

---
+++ examples/oft-hyperliquid/hardhat.config.ts
@@ -0,0 +1,72 @@
+// Get the environment configuration from .env file
+//
+// To make use of automatic environment setup:
+// - Duplicate .env.example file and name it .env
+// - Fill in the environment variables
+import 'dotenv/config'
+
+import 'hardhat-deploy'
+import 'hardhat-contract-sizer'
+import '@nomiclabs/hardhat-ethers'
+import '@layerzerolabs/toolbox-hardhat'
+import { HardhatUserConfig, HttpNetworkAccountsUserConfig } from 'hardhat/types'
+
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+
+// Set your preferred authentication method
+//
+// If you prefer using a mnemonic, set a MNEMONIC environment variable
+// to a valid mnemonic
+// const MNEMONIC = process.env.MNEMONIC
+
+// If you prefer to be authenticated using a private key, set a PRIVATE_KEY environment variable
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+if (PRIVATE_KEY == null) {
+    throw new Error('PRIVATE_KEY is not set in .env file')
+}
+
+const accounts: HttpNetworkAccountsUserConfig | undefined = [PRIVATE_KEY]
+
+if (accounts == null) {
+    console.warn(
+        'Could not find MNEMONIC or PRIVATE_KEY environment variables. It will not be possible to execute transactions in your example.'
+    )
+}
+
+const config: HardhatUserConfig = {
+    paths: {
+        cache: 'cache/hardhat',
+    },
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.22',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+    },
+    networks: {
+        'hyperliquid-testnet': {
+            eid: EndpointId.HYPERLIQUID_V2_TESTNET,
+            url: process.env.RPC_HYPEREVM_TESTNET || 'https://rpc.hyperliquid-testnet.xyz/evm',
+            accounts,
+        },
+        hardhat: {
+            // Need this for testing because TestHelperOz5.sol is exceeding the compiled contract size limit
+            allowUnlimitedContractSize: true,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // wallet address of index[0], of the mnemonic in .env
+        },
+    },
+}
+
+export default config
