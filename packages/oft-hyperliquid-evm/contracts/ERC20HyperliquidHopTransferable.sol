4b231a7aa89b243588cd830f401c9a47d4ddb9fc
shankar <shankar@layerzerolabs.org>
2025-03-06 20:51:24 +0000

    make ERC20HyperliquidHopTransferable abstract Ownable
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/oft-hyperliquid-evm/contracts/ERC20HyperliquidHopTransferable.sol
+++ packages/oft-hyperliquid-evm/contracts/ERC20HyperliquidHopTransferable.sol
@@ -2,27 +2,28 @@
 pragma solidity ^0.8.20;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 import { IERC20HyperliquidHopTransferable } from "./interfaces/IERC20HyperliquidHopTransferable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title ERC20Extension
 ///
 /// @dev This contract is a wrapper around the ERC20 contract that allows for the transfer of tokens to the HyperLiquid L1 contract
 /// @dev This contract is designed to be used as a replacement for the ERC20 contract such that you can simply replace the import of the ERC20 with this contract
 /// @dev Hyperliquid L1 listens to token transfers to the HL_NATIVE_TRANSFER address. The token should be linked to an L1 Native Spot address before the transfer is made
-abstract contract ERC20HyperliquidHopTransferable is ERC20, Ownable, IERC20HyperliquidHopTransferable {
+abstract contract ERC20HyperliquidHopTransferable is ERC20, IERC20HyperliquidHopTransferable, Ownable {
     address public constant HL_NATIVE_TRANSFER = 0x2222222222222222222222222222222222222222;
     mapping(address approvedCaller => bool) public approvedCallers;
 
     /// @notice Constructor for the HyperLiquidERC20Extended contract
     ///
     /// @dev This constructor is called by the `HyperLiquidOFT` contract
-    /// @dev The `owner` is by default the `msg.sender` of the constructor (the deployer of the contract)
+    /// @dev The `onlyErc20Admin` is by default the `msg.sender` of the constructor (the deployer of the contract)
     ///
     /// @param _name The name of the token
     /// @param _symbol The symbol of the token
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable(msg.sender) {}
+    /// @param _delegate The delegate owner of the contract
+    constructor(string memory _name, string memory _symbol, address _delegate) ERC20(_name, _symbol) {}
 
     /// @notice Transfers tokens to the HyperLiquid L1 contract
     ///
@@ -60,6 +61,15 @@ abstract contract ERC20HyperliquidHopTransferable is ERC20, Ownable, IERC20Hyper
     function removeApprovedCaller(address _caller) external onlyOwner {
         approvedCallers[_caller] = false;
         emit HyperLiquidL1CallerRemoved(_caller);
+    }
+
+    /// @notice Transfers the delegate owner role to a new address
+    ///
+    /// @dev This function is called by the `delegateOwner` to transfer the role to a new address
+    ///
+    /// @param _newDelegateOwner The address of the new delegate owner
+    function transferDelegateOwner(address _newDelegateOwner) external onlyOwner {
+        _transferOwnership(_newDelegateOwner);
     }
 
     /// @notice Returns true if the contract implements the `IHyperLiquidERC20Extended` interface
