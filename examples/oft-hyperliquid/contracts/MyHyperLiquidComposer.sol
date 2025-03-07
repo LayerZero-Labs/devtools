874ca1eb7aac1f9b18e3ec4f74cb5cc76b1f1ca2
shankar <shankar@layerzerolabs.org>
2025-03-07 22:12:52 +0000

    using the new deposit system address
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-hyperliquid/contracts/MyHyperLiquidComposer.sol
+++ examples/oft-hyperliquid/contracts/MyHyperLiquidComposer.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8.22;
 import { HyperLiquidComposer } from "@layerzerolabs/oft-hyperliquid-evm/contracts/HyperLiquidComposer.sol";
 
 contract MyHyperLiquidComposer is HyperLiquidComposer {
-    constructor(address _lzEndpoint, address _oft) HyperLiquidComposer(_lzEndpoint, _oft) {}
+    constructor(
+        address _lzEndpoint,
+        address _oft,
+        uint256 _hlIndexId
+    ) HyperLiquidComposer(_lzEndpoint, _oft, _hlIndexId) {}
 }
--- examples/oft-hyperliquid/test/foundry/MyHyperliquidOFT.t.sol
+++ examples/oft-hyperliquid/test/foundry/MyHyperliquidOFT.t.sol
@@ -36,6 +36,7 @@ contract MyHyperLiquidOFTTest is TestHelperOz5 {
 
     address public userA = makeAddr("userA");
     address public userB = makeAddr("userB");
+    uint256 public hlIndexId = 9999;
 
     uint256 public initialBalance = 100 ether;
     uint256 public initialNativeBalance = 1000 ether;
@@ -51,7 +52,7 @@ contract MyHyperLiquidOFTTest is TestHelperOz5 {
         srcOFT = new MyHyperLiquidOFT(SRC_OFT_NAME, SRC_OFT_SYMBOL, address(endpoints[SRC_EID]), address(this));
         dstOFT = new MyHyperLiquidOFT(DST_OFT_NAME, DST_OFT_SYMBOL, address(endpoints[DST_EID]), address(this));
 
-        dstLZComposer = new MyHyperLiquidComposer(address(endpoints[DST_EID]), address(dstOFT));
+        dstLZComposer = new MyHyperLiquidComposer(address(endpoints[DST_EID]), address(dstOFT), hlIndexId);
 
         // config and wire the ofts
         address[] memory ofts = new address[](2);
@@ -128,12 +129,12 @@ contract MyHyperLiquidOFTTest is TestHelperOz5 {
 
         // Expect the Transfer event to be emitted
         vm.expectEmit(address(dstOFT));
-        emit IERC20.Transfer(address(userB), dstOFT.HL_NATIVE_TRANSFER(), oftReceipt.amountReceivedLD);
+        emit IERC20.Transfer(address(userB), dstLZComposer.HL_NATIVE_TRANSFER(), oftReceipt.amountReceivedLD);
         this.lzCompose(dstEid_, from_, options_, guid_, to_, composerMsg_);
 
         // Assert the post state
         assertEq(dstOFT.balanceOf(address(userB)), 0);
-        assertEq(dstOFT.balanceOf(dstOFT.HL_NATIVE_TRANSFER()), oftReceipt.amountReceivedLD);
+        assertEq(dstOFT.balanceOf(dstLZComposer.HL_NATIVE_TRANSFER()), oftReceipt.amountReceivedLD);
     }
 
     function send_oft_AB(
