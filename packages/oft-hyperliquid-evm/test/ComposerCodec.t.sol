87a58e8e8edab0c4ae62831a0d1d1397e694ce79
shankar <shankar@layerzerolabs.org>
2025-03-10 17:17:07 +0000

    lint fix
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/oft-hyperliquid-evm/test/ComposerCodec.t.sol
+++ packages/oft-hyperliquid-evm/test/ComposerCodec.t.sol
@@ -8,13 +8,13 @@ import { Test, console } from "forge-std/Test.sol";
 contract ComposerCodecTest is Test {
     function setUp() public {}
 
-    function test_into_assetBridgeAddress() public {
+    function test_into_assetBridgeAddress() public pure {
         uint256 coreIndexId = 1;
         address assetBridgeAddress = HyperLiquidComposerCodec.into_assetBridgeAddress(coreIndexId);
         assertEq(assetBridgeAddress, 0x2000000000000000000000000000000000000001);
     }
 
-    function test_into_tokenId() public {
+    function test_into_tokenId() public pure {
         address assetBridgeAddress = 0x2000000000000000000000000000000000000001;
         uint256 tokenId = HyperLiquidComposerCodec.into_tokenId(assetBridgeAddress);
         assertEq(tokenId, 1);
