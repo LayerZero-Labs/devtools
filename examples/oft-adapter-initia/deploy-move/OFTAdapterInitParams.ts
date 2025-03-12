55d423db33c947e28d5e04b179e3d0213290e59c
alexanderliteplo <alexanderliteplo@gmail.com>
2025-03-12 15:19:59 -0700

    adding move vm specific printout and typed params

--- examples/oft-adapter-initia/deploy-move/OFTAdapterInitParams.ts
+++ examples/oft-adapter-initia/deploy-move/OFTAdapterInitParams.ts
@@ -1,6 +1,8 @@
-const oftMetadata = {
-    move_vm_fa_address: '0x892c83d36770bf99d5e1a480874b6395a27bac3014a96d02865ad34669387cd9',
+import { OFTAdapterFaInitParams } from '@layerzerolabs/oft-move'
+
+const oftMetadata: OFTAdapterFaInitParams = {
+    move_vm_fa_address: '',
     shared_decimals: 6,
 }
 
-export { oftMetadata }
+export default oftMetadata
