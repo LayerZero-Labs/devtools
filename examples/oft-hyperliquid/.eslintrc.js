0438156aa476e92fb228fa76fb89e5059d481303
shankar <shankar@layerzerolabs.org>
2025-03-18 23:33:45 +0000

    ignore import path for @layerzerolabs/oft-hyperliquid-evm
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-hyperliquid/.eslintrc.js
+++ examples/oft-hyperliquid/.eslintrc.js
@@ -1,11 +1,29 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-    root: true,
+    root: true, // Add this line to prevent ESLint from looking up the directory tree
     extends: ['@layerzerolabs/eslint-config-next/recommended'],
     rules: {
         // @layerzerolabs/eslint-config-next defines rules for turborepo-based projects
         // that are not relevant for this particular project
         'turbo/no-undeclared-env-vars': 'off',
+        // Explicitly disable no-unresolved for LayerZero packages
+        'import/no-unresolved': [
+            'error',
+            {
+                ignore: ['@layerzerolabs/oft-hyperliquid-evm'],
+            },
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+            node: {
+                moduleDirectory: ['node_modules', '.'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
     },
 };
