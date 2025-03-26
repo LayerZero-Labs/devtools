aa336b06404483ef6897b494caf4aa95dba55f7f
shankar <shankar@layerzerolabs.org>
2025-03-26 21:19:26 +0000

    feat: i/o for core spot (HIP-1)  deployments
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/oft-hyperliquid-evm/src/io/parser.ts
+++ packages/oft-hyperliquid-evm/src/io/parser.ts
@@ -1,49 +1,119 @@
 import fs from 'fs'
 import path from 'path'
 
-import { NativeSpotDeployment } from '@/types'
+import { CoreSpotDeployment, TxData } from '@/types'
+import { importDefault, Logger } from '@layerzerolabs/io-devtools'
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+import type { OAppOmniGraphHardhat } from '@layerzerolabs/toolbox-hardhat'
+import 'hardhat/register'
+import type { HardhatUserConfig, NetworkUserConfig } from 'hardhat/types'
 
-const DEPLOY_DIR = path.join('deploy', 'native-spots')
+function getFullPath(index: string, isTestnet: boolean): string {
+    return path.join(process.cwd(), 'deployments', `hypercore-${isTestnet ? 'testnet' : 'mainnet'}`, `${index}.json`)
+}
 
-export function getNativeSpot(key: string): NativeSpotDeployment {
-    const nativeSpotPath = path.join(process.cwd(), DEPLOY_DIR, `${key}.json`)
-    const nativeSpot = fs.readFileSync(nativeSpotPath, 'utf8')
+export function getCoreSpotDeployment(index: string, isTestnet: boolean, logger?: Logger): CoreSpotDeployment {
+    const fullPath = getFullPath(index, isTestnet)
+    const nativeSpot = fs.readFileSync(fullPath, 'utf8')
     if (!nativeSpot) {
-        throw new Error(
-            `Native spot ${key} not found - make sure the native spot for the token ${key} is found at ${nativeSpotPath}`
-        )
+        const errMsg = `Native spot ${index} not found - make sure the native spot for the token ${index} is found at ${fullPath}`
+        logger?.error(errMsg)
+        throw new Error(errMsg)
     }
-    return JSON.parse(nativeSpot) as NativeSpotDeployment
+    return JSON.parse(nativeSpot) as CoreSpotDeployment
 }
 
-export function writeUpdatedNativeSpots(
-    key: string,
-    tokenAddress: string,
-    tokenName: string,
-    txHash: string,
-    nonce: number,
-    from: string,
-    connected: boolean,
-    weiDiff?: number
+export function writeCoreSpotDeployment(
+    index: string,
+    isTestnet: boolean,
+    coreSpotDeployment: CoreSpotDeployment,
+    logger?: Logger
 ) {
-    const spot = getNativeSpot(key)
-    spot.nativeSpot.evmContract = tokenAddress
-    spot.nativeSpot.fullName = tokenName
-    spot.txData.txHash = txHash
-    spot.txData.nonce = nonce
-    spot.txData.from = from
-    spot.txData.connected = connected
-    spot.txData.weiDiff = weiDiff
+    const fullPath = getFullPath(index, isTestnet)
 
-    fs.writeFileSync(path.join(process.cwd(), DEPLOY_DIR, `${key}.json`), JSON.stringify(spot, null, 2))
-    console.log(`Updated native spot ${key}`)
+    const dir = path.dirname(fullPath)
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true })
+    }
+
+    fs.writeFileSync(fullPath, JSON.stringify(coreSpotDeployment, null, 2))
+    logger?.info(`Wrote deployment ${fullPath}`)
 }
 
-export function writeNativeSpotConnected(key: string, connected: boolean, weiDiff: number) {
-    const spot = getNativeSpot(key)
+export function writeUpdatedCoreSpotDeployment(
+    index: string,
+    isTestnet: boolean,
+    tokenName: string,
+    tokenAddress: string,
+    txData: TxData,
+    logger?: Logger
+) {
+    const fullPath = getFullPath(index, isTestnet)
+
+    const spot = getCoreSpotDeployment(index, isTestnet, logger)
+    spot.nativeSpot.evmContract = {
+        address: tokenAddress,
+        evm_extra_wei_decimals: txData.weiDiff ?? 0,
+    }
+    spot.nativeSpot.fullName = tokenName
+    spot.txData.txHash = txData.txHash
+    spot.txData.nonce = txData.nonce
+    spot.txData.from = txData.from
+    spot.txData.connected = txData.connected
+    spot.txData.weiDiff = txData.weiDiff
+
+    fs.writeFileSync(fullPath, JSON.stringify(spot, null, 2))
+    logger?.info(`Updated core spot ${index}`)
+}
+
+export function writeNativeSpotConnected(
+    index: string,
+    isTestnet: boolean,
+    connected: boolean,
+    weiDiff: number,
+    logger?: Logger
+) {
+    const fullPath = getFullPath(index, isTestnet)
+
+    const spot = getCoreSpotDeployment(index, isTestnet, logger)
     spot.txData.connected = connected
     spot.txData.weiDiff = weiDiff
 
-    fs.writeFileSync(path.join(process.cwd(), DEPLOY_DIR, `${key}.json`), JSON.stringify(spot, null, 2))
-    console.log(`Updated native spot ${key}`)
+    fs.writeFileSync(fullPath, JSON.stringify(spot, null, 2))
+    logger?.info(`Updated core spot ${index}`)
+}
+
+export async function getHyperEVMOAppDeployment(
+    oapp_config: string,
+    network: string,
+    logger?: Logger
+): Promise<{ contractName: string; deployment: string }> {
+    const targetEid =
+        network === 'testnet'
+            ? EndpointId.HYPERLIQUID_V2_TESTNET.valueOf()
+            : EndpointId.HYPERLIQUID_V2_MAINNET.valueOf()
+
+    const lzConfig = await importDefault(path.join(process.cwd(), oapp_config))
+    const contracts = (lzConfig as OAppOmniGraphHardhat).contracts
+    const contractName = contracts.find((contract) => contract.contract.eid === targetEid)?.contract.contractName
+    if (!contractName) {
+        throw new Error(`HyperEVM deployment not found for ${targetEid}`)
+    }
+
+    const hardhatConfig = await importDefault(path.join(process.cwd(), 'hardhat.config.ts'))
+    const networks = (hardhatConfig as HardhatUserConfig).networks
+    const networkKey = Object.keys(networks as NetworkUserConfig).find((key) => networks?.[key]?.eid === targetEid)
+    if (!networkKey) {
+        throw new Error(`Network not found for ${targetEid}`)
+    }
+
+    const deploymentFilePath = path.join(process.cwd(), 'deployments', networkKey, `${contractName}.json`)
+    logger?.info(`HyperEVM-${network} deployment file: ${deploymentFilePath}`)
+
+    const deploymentFile = JSON.parse(fs.readFileSync(deploymentFilePath, 'utf8'))
+
+    return {
+        contractName: contractName,
+        deployment: deploymentFile,
+    }
 }
