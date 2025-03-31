fc639f5b00306ab53fd36d290f0d7900a522dd3a
shankar <shankar@layerzerolabs.org>
2025-03-31 23:17:41 +0000

    decoding sender and receiver address to zero if not evm (bytes20)
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/oft-hyperliquid-evm/contracts/library/HyperLiquidComposerCodec.sol
+++ packages/oft-hyperliquid-evm/contracts/library/HyperLiquidComposerCodec.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { IHyperLiquidComposer } from "../interfaces/IHyperLiquidComposer.sol";
-import { IHyperLiquidComposerErrors, ErrorMessage } from "../interfaces/IHyperLiquidComposerErrors.sol";
+import { ErrorMessagePayload, IHyperLiquidComposerErrors } from "../interfaces/IHyperLiquidComposerErrors.sol";
 import { IHyperAsset, IHyperAssetAmount } from "../interfaces/IHyperLiquidComposerCore.sol";
 import { OFTComposeMsgCodec } from "@layerzerolabs/oft-evm/contracts/libs/OFTComposeMsgCodec.sol";
+
 library HyperLiquidComposerCodec {
+    /// @dev Valid compose message lengths for the HyperLiquidComposer - can be abi.encodePacked(address) or abi.encode(address)
+    /// @dev If we are in abi.encodePacked(address) mode, the length is 20 bytes because addresses are 20 bytes
+    uint256 public constant VALID_COMPOSE_MESSAGE_LENGTH_PACKED = 20;
+    /// @dev If we are in abi.encode(address) mode, the length is 32 bytes
+    uint256 public constant VALID_COMPOSE_MESSAGE_LENGTH_ENCODE = 32;
+
     /// @dev The base asset bridge address is the address of the HyperLiquid L1 contract
     /// @dev This is the address that the OFT contract will transfer the tokens to when we want to send tokens to HyperLiquid L1
     /// @dev https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/hyperevm/hypercore-less-than-greater-than-hyperevm-transfers#system-addresses
@@ -41,21 +47,26 @@ library HyperLiquidComposerCodec {
     /// @param _asset The asset to convert
     ///
     /// @return IHyperAssetAmount memory - The evm amount, core amount, and dust
-    function into_hyper_asset_amount(
+    function into_hyperAssetAmount(
         uint256 _amount,
         uint64 _maxTransferableAmount,
         IHyperAsset memory _asset
     ) internal returns (IHyperAssetAmount memory) {
         uint256 scale = 10 ** _asset.decimalDiff;
+        uint256 dust;
+        uint256 amountEVM;
 
-        uint256 dust = _amount % scale;
-        uint256 amountEVM = _amount - dust;
+        unchecked {
+            dust = _amount % scale;
+            amountEVM = _amount - dust;
+        }
 
-        // _amount / scale is guaranteed to be smaller than 2 ** 64
+        /// @dev _amount / scale is guaranteed to be smaller than 2 ** 64
         uint64 amountCore = uint64(_amount / scale);
 
         if (amountCore > _maxTransferableAmount) {
             emit OverflowDetected(amountCore, _maxTransferableAmount);
+
             uint256 overflowEVM = (amountCore - _maxTransferableAmount) * scale;
             amountCore = _maxTransferableAmount;
             amountEVM = amountEVM - overflowEVM;
@@ -65,11 +76,64 @@ library HyperLiquidComposerCodec {
         return IHyperAssetAmount({ evm: amountEVM, dust: dust, core: amountCore });
     }
 
+    /// @notice Converts a bytes32 to an evm address
+    /// @notice This function is called by the HyperLiquidComposer contract
+    ///
+    /// @param _senderAsBytes32 The bytes32 to convert
+    ///
+    /// @return _evmAddress Non zero address if the bytes32 is a valid evm address, otherwise zero address
+    function into_evmAddress_or_zero(bytes32 _senderAsBytes32) internal pure returns (address) {
+        uint256 senderAsUint160 = uint256(_senderAsBytes32);
+        if (senderAsUint160 <= type(uint160).max) {
+            return address(uint160(senderAsUint160));
+        }
+        return address(0);
+    }
+
+    /// @notice Converts a bytes to an evm address
+    /// @notice This function is called by the HyperLiquidComposer contract
+    ///
+    /// @param _senderAsBytes The bytes to convert
+    ///
+    /// @return _evmAddress Non zero address if the bytes are a valid evm address, otherwise zero address
+    function into_evmAddress_or_zero(bytes memory _senderAsBytes) internal pure returns (address) {
+        uint256 length = _senderAsBytes.length;
+        if (length == VALID_COMPOSE_MESSAGE_LENGTH_PACKED) {
+            return address(bytes20(_senderAsBytes));
+        } else if (length == VALID_COMPOSE_MESSAGE_LENGTH_ENCODE) {
+            bytes32 senderAsBytes32 = bytes32(_senderAsBytes);
+            return into_evmAddress_or_zero(senderAsBytes32);
+        }
+        return address(0);
+    }
+
+    /// @notice Extracts the error payload from a bytes array
+    /// @notice This function is called by the HyperLiquidComposer contract
+    /// @dev Strips out the revert message to extract the payload ErrorMsg(bytes errorMessage) - ('0x' + 32 bytes) * 2 = 64 bytes
+    ///
+    /// @param _err The bytes array to extract the error payload from
+    ///
+    /// @return _errorPayload The error payload
+    function extractErrorPayload(bytes calldata _err) external pure returns (bytes memory) {
+        return _err[64 + 4:];
+    }
+
+    /// @notice Creates an error message payload
+    /// @notice This function is called by the HyperLiquidComposer contract
+    ///
+    /// @param _errorMessage The error message to create
+    /// @param _sender The sender of the error message
+    /// @param _amountLD The amount of the error message
+    ///
+    /// @return _errorMessagePayload The error message payload
     function createErrorMessage(
+        bytes memory _errorMessage,
         address _sender,
-        uint256 _amountLD,
-        bytes memory _errorMessage
+        uint256 _amountLD
     ) internal pure returns (bytes memory) {
-        return abi.encode(ErrorMessage({ refundTo: _sender, refundAmount: _amountLD, errorMessage: _errorMessage }));
+        return
+            abi.encode(
+                ErrorMessagePayload({ refundTo: _sender, refundAmount: _amountLD, errorMessage: _errorMessage })
+            );
     }
 }
