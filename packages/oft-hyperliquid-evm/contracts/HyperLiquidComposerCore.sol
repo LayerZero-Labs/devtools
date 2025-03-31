97eaae03308f463973520e371b000c9a1c410313
shankar <shankar@layerzerolabs.org>
2025-03-31 23:21:51 +0000

    decompose validateAndDecodeMessage into pre-checks for valid byte arrays
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/oft-hyperliquid-evm/contracts/HyperLiquidComposerCore.sol
+++ packages/oft-hyperliquid-evm/contracts/HyperLiquidComposerCore.sol
@@ -1,17 +1,21 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import { IOFT } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { HyperLiquidComposerCodec } from "./library/HyperLiquidComposerCodec.sol";
-import { IHyperLiquidComposerErrors, ErrorMessage } from "./interfaces/IHyperLiquidComposerErrors.sol";
+import { IHyperLiquidComposerErrors, ErrorMessagePayload } from "./interfaces/IHyperLiquidComposerErrors.sol";
 import { IHyperLiquidWritePrecompile } from "./interfaces/IHyperLiquidWritePrecompile.sol";
 import { IHyperLiquidReadPrecompile } from "./interfaces/IHyperLiquidReadPrecompile.sol";
 import { OFTComposeMsgCodec } from "@layerzerolabs/oft-evm/contracts/libs/OFTComposeMsgCodec.sol";
 import { IHyperLiquidComposerCore, IHyperAsset, IHyperAssetAmount } from "./interfaces/IHyperLiquidComposerCore.sol";
 
 contract HyperLiquidComposerCore is IHyperLiquidComposerCore {
+    using HyperLiquidComposerCodec for bytes32;
+    using HyperLiquidComposerCodec for bytes;
+    using HyperLiquidComposerCodec for uint256;
+
     modifier onlyComposer() {
         if (msg.sender != address(this)) {
             revert IHyperLiquidComposerErrors.HyperLiquidComposer_InvalidCall_NotComposer(msg.sender);
@@ -19,67 +23,60 @@ contract HyperLiquidComposerCore is IHyperLiquidComposerCore {
         _;
     }
 
-    /// @dev The length of the message that is valid for the HyperLiquidComposer
-    /// @dev This is 20 bytes because addresses are 20 bytes
-    /// @dev We are in encodePacked mode, if we are in encode mode, the length is 32 bytes
+    /// @dev Valid compose message lengths for the HyperLiquidComposer - can be abi.encodePacked(address) or abi.encode(address)
+    /// @dev If we are in abi.encodePacked(address) mode, the length is 20 bytes because addresses are 20 bytes
     uint256 public constant VALID_COMPOSE_MESSAGE_LENGTH_PACKED = 20;
+    /// @dev If we are in abi.encode(address) mode, the length is 32 bytes
     uint256 public constant VALID_COMPOSE_MESSAGE_LENGTH_ENCODE = 32;
 
-    address public constant L1WritePrecompileAddress = 0x3333333333333333333333333333333333333333;
-    address public constant L1ReadPrecompileAddress_SpotBalance = 0x0000000000000000000000000000000000000801;
+    address public constant HLP_PRECOMPILE_WRITE = 0x3333333333333333333333333333333333333333;
+    address public constant HLP_PRECOMPILE_READ_SPOT_BALANCE = 0x0000000000000000000000000000000000000801;
 
     address public immutable endpoint;
+
     IOFT public immutable oft;
     IERC20 public immutable token;
 
     IHyperAsset public oftAsset;
     IHyperAsset public hypeAsset;
 
-    /// @notice Validates the compose message and decodes it into an address and amount
-    /// @notice This function is called by the HyperLiquidComposer contract
-    ///
-    /// @param _composeMessage The compose message to validate and decode
-    ///
-    /// @return _receiver The address of the receiver
-    /// @return _amountLD The amount of tokens to send
-    function validateAndDecodeMessage(
-        bytes calldata _composeMessage
-    ) external pure returns (address _receiver, uint256 _amountLD) {
-        bytes memory message = OFTComposeMsgCodec.composeMsg(_composeMessage);
+    function validate_payload(bytes calldata _composeMessage) external pure returns (uint256, bytes32, bytes memory) {
+        /// @dev Revert type : out of bounds or type cast error
+        /// @dev Reason: Trying to slice the bytes object when it isn't of the form created by a OFTComposeMsgCodec.encode()
+        uint256 amountLD = OFTComposeMsgCodec.amountLD(_composeMessage);
+        bytes32 maybeSenderBytes32 = OFTComposeMsgCodec.composeFrom(_composeMessage);
 
-        _amountLD = OFTComposeMsgCodec.amountLD(_composeMessage);
+        /// @dev This is an unbounded slice range without type casting thereby having the least chance of erroring
+        bytes memory maybeReceiver = OFTComposeMsgCodec.composeMsg(_composeMessage);
 
-        // Addresses in EVM are 20 bytes when packed or 32 bytes when encoded
-        // So if the message's length is not 20 bytes, we can pre-emptively revert
-        if (
-            message.length != VALID_COMPOSE_MESSAGE_LENGTH_PACKED &&
-            message.length != VALID_COMPOSE_MESSAGE_LENGTH_ENCODE
-        ) {
-            bytes memory errMsg = abi.encodeWithSelector(
-                IHyperLiquidComposerErrors.HyperLiquidComposer_Codec_InvalidMessage_UnexpectedLength.selector,
-                message,
-                message.length
-            );
-            /// @dev Handling the case when the transaction is from 1 bytes-20 address and we can prevent the funds from being stuck in the composer
-            /// @dev We can optimisitcally assume that the bytes20 address is an evm-address
-            /// @dev If this is a non-evm address (i.e. a bytes32 on aptos move, or solana) then this abi.decode will revert and the following revert will not be thrown
-            address sender = abi.decode(bytes.concat(OFTComposeMsgCodec.composeFrom(_composeMessage)), (address));
-            /// @dev THrow the following error message ONLY if the sender is bytes20. It causes lzCompose to refund the sender and then emit the error message
-            revert IHyperLiquidComposerErrors.ErrorMsg(
-                HyperLiquidComposerCodec.createErrorMessage(sender, _amountLD, errMsg)
-            );
-        }
-
-        // Since we are encodePacked, we can just decode the first 20 bytes as an address
-        if (message.length == VALID_COMPOSE_MESSAGE_LENGTH_PACKED) {
-            _receiver = address(bytes20(message));
-        } else {
-            _receiver = abi.decode(message, (address));
-        }
+        return (amountLD, maybeSenderBytes32, maybeReceiver);
     }
 
-    /// @notice Quotes the amount of tokens that will be sent to HyperCore
-    /// @notice This function is externally callable
+    function validate_addresses_or_refund(
+        bytes memory _maybeReceiver,
+        bytes32 _senderBytes32,
+        uint256 _amountLD
+    ) external pure returns (address) {
+        /// @dev Test the conversion of the below (i.e. bytes and bytes32 into address)
+        /// @dev This function returns address(0) if the sender is not a valid evm address
+        address sender = _senderBytes32.into_evmAddress_or_zero();
+        address receiver = _maybeReceiver.into_evmAddress_or_zero();
+
+        /// @dev Initiate refund if the receiver is not a valid evm adress
+        /// @dev Handling the if sender is not a valid evm address in the refund function
+        if (receiver == address(0)) {
+            bytes memory errMsg = abi.encodeWithSelector(
+                IHyperLiquidComposerErrors.HyperLiquidComposer_Codec_InvalidMessage_UnexpectedLength.selector,
+                _maybeReceiver,
+                _maybeReceiver.length
+            );
+            revert IHyperLiquidComposerErrors.ErrorMsg(errMsg.createErrorMessage(sender, _amountLD));
+        }
+
+        return receiver;
+    }
+
+    /// @notice External function to quote the conversion of evm tokens to hypercore tokens
     ///
     /// @param _amount The amount of tokens to send
     /// @param _isOFT Whether the amount is an OFT amount or a HYPE amount
@@ -97,27 +94,33 @@ contract HyperLiquidComposerCore is IHyperLiquidComposerCore {
             maxTransferableAmount = _balanceOfHyperCore(hypeAsset.assetBridgeAddress, hypeAsset.coreIndexId);
         }
 
-        return HyperLiquidComposerCodec.into_hyper_asset_amount(_amount, maxTransferableAmount, asset);
+        return _amount.into_hyperAssetAmount(maxTransferableAmount, asset);
     }
 
+    /// @notice External function to read the balance of the user in the hypercore
+    ///
+    /// @param _user The address of the user
+    /// @param _tokenId The token id of the hypercore
+    ///
+    /// @return The balance of the user in the hypercore
     function balanceOfHyperCore(address _user, uint64 _tokenId) external view returns (uint64) {
         return _balanceOfHyperCore(_user, _tokenId);
     }
 
+    /// @notice Internal function to read the balance of the user in the hypercore
+    ///
+    /// @param _user The address of the user
+    /// @param _tokenId The token id of the hypercore
+    ///
+    /// @return The balance of the user in the hypercore
     function _balanceOfHyperCore(address _user, uint64 _tokenId) internal view returns (uint64) {
         bool success;
         bytes memory result;
-        (success, result) = L1ReadPrecompileAddress_SpotBalance.staticcall(abi.encode(_user, _tokenId));
-        require(success, "SpotBalance precompile call failed");
+        (success, result) = HLP_PRECOMPILE_READ_SPOT_BALANCE.staticcall(abi.encode(_user, _tokenId));
+        if (!success) {
+            revert IHyperLiquidComposerErrors.HyperLiquidComposerCore_SpotBalanceRead_Failed(_user, _tokenId);
+        }
         return abi.decode(result, (IHyperLiquidReadPrecompile.SpotBalance)).total;
-    }
-
-    function getOFTAsset() external view returns (IHyperAsset memory) {
-        return oftAsset;
-    }
-
-    function getHypeAsset() external view returns (IHyperAsset memory) {
-        return hypeAsset;
     }
 
     /// @notice Refunds the tokens to the sender
@@ -126,23 +129,28 @@ contract HyperLiquidComposerCore is IHyperLiquidComposerCore {
     /// @dev The function refunds the oft token or msg.value to the sender if the error message contains a refund amount
     /// @dev This is applicable in cases when we would normally revert the transaction but can't due to the composer being the intermediate recipient of the minted tokens
     ///
+    /// @param _err The error message
+    ///
     /// @return errMsg.errorMessage The error message
-    function refundTokens(bytes calldata err) external payable onlyComposer returns (bytes memory) {
-        if (err.length == 0) {
-            // This is the only non-custom revert - failure @ abi.decode(bytes, (address))
-            return abi.encode("Error: Not logic based - malformed receiver address with a non-evm sender");
-        }
-        // All error messages beyond this point as of the form ErrorMessage(address refundTo, uint256 refundAmount, bytes errorMessage)
-        // Here we strip out the revert message to extract the payload ErrorMsg(bytes errorMessage) - ('0x' + 32 bytes) * 2 = 64 bytes
-        bytes memory encodedErrorMessage = err[64 + 4:];
-        ErrorMessage memory errMsg = abi.decode(encodedErrorMessage, (ErrorMessage));
+    function completeRefund(bytes memory _err) internal returns (bytes memory) {
+        // All error messages beyond this point are of the form ErrorMessage(address refundTo, uint256 refundAmount, bytes errorMessage)
+
+        bytes memory encodedErrorMessage = _err.extractErrorPayload();
+        ErrorMessagePayload memory errMsg = abi.decode(encodedErrorMessage, (ErrorMessagePayload));
+
         // The refund amount can vary based on partial refunds
-        if (errMsg.refundAmount > 0) {
-            token.transfer(errMsg.refundTo, errMsg.refundAmount);
-            emit errorRefund(errMsg.refundTo, errMsg.refundAmount);
+        try this.refundERC20(errMsg.refundTo, errMsg.refundAmount) {} catch {
+            emit ErrorERC20_Refund(errMsg.refundTo, errMsg.refundAmount);
         }
+
+        // Try to refund the native tokens and if this fails, we fallback to the tx.origin
         try this.refundNativeTokens{ value: msg.value }(errMsg.refundTo) {} catch {
-            emit errorNativeRefund_Failed(errMsg.refundTo, msg.value);
+            (bool success, ) = tx.origin.call{ value: msg.value }("");
+            if (!success) {
+                emit ErrorHYPE_Refund(tx.origin, msg.value);
+            }
+
+            emit ErrorHYPE_Refund(errMsg.refundTo, msg.value);
         }
         return errMsg.errorMessage;
     }
@@ -150,14 +158,45 @@ contract HyperLiquidComposerCore is IHyperLiquidComposerCore {
     /// @notice Refunds the native tokens to the refund address
     /// @notice This function is called by the refundTokens function
     ///
-    /// @dev It is possible that the address is a contract without fallback or receive functions - in that case the transfer fails and tokens will be locked in the contract.
-    /// @dev Since msg.value is used as the amount to refund, and msg.value reduces during tx execution, it isn't possible to partial transfer and then get the entire amount back.
-    /// @dev The refund amount in this case would be the amount of the tx minus the amount of the error refund.
+    /// @dev If the refund address is set to the zero address - it means that the transaction sender is a non-evm address and the receiver is malformed.
+    /// @dev In this case, the tokens are locked in the composer.
     ///
-    /// @param refundAddress The address to refund the native tokens to
-    function refundNativeTokens(address refundAddress) external payable onlyComposer {
-        if (msg.value > 0) {
-            token.transfer(refundAddress, msg.value);
+    /// @param _refundAddress The address to refund the native tokens to
+    /// @param _amount The amount of tokens to refund
+    function refundERC20(address _refundAddress, uint256 _amount) external payable onlyComposer {
+        if (_amount > 0 && _refundAddress != address(0)) {
+            token.transfer(_refundAddress, _amount);
         }
+    }
+
+    /// @notice Refunds the native tokens to the refund address
+    /// @notice This function is called by the refundTokens function
+    ///
+    /// @dev It is possible that the refund address is a contract without fallback or receive functions - in that case the transfer fails and tokens will be locked in the contract.
+    /// @dev Since this is an external function - the msg.value can be different to the msg.value sent to the lzCompose function by tx.origin
+    /// @dev It is different in the case of a partial refund where the call is:
+    /// @dev `this.refundNativeTokens{ value: amounts.dust }(_receiver)`
+    /// @dev In this case, the msg.value is the amount of the dust and not the msg.value sent to the lzCompose function by tx.origin
+    ///
+    /// @param _refundAddress The address to refund the native tokens to
+    function refundNativeTokens(address _refundAddress) external payable onlyComposer {
+        if (msg.value > 0) {
+            /// @dev If the refund address is the zero address, we refund to the tx.origin
+            /// @dev This is to emulate the behavior on a revert - where the msg.value is returned to the transaction sender
+            address refundAddress = _refundAddress == address(0) ? tx.origin : _refundAddress;
+
+            (bool success, ) = refundAddress.call{ value: msg.value }("");
+            if (!success) {
+                revert IHyperLiquidComposerErrors.HyperLiquidComposer_FailedToRefund_HYPE(refundAddress, msg.value);
+            }
+        }
+    }
+
+    function getOFTAsset() external view returns (IHyperAsset memory) {
+        return oftAsset;
+    }
+
+    function getHypeAsset() external view returns (IHyperAsset memory) {
+        return hypeAsset;
     }
 }
