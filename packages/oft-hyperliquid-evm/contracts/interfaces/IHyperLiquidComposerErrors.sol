80ac183e4bdd0944c9bbfcdbdbd479dd8c2d4537
shankar <shankar@layerzerolabs.org>
2025-04-01 00:06:22 +0000

    capitalize events and constants and sort alphabetically
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/oft-hyperliquid-evm/contracts/interfaces/IHyperLiquidComposerErrors.sol
+++ packages/oft-hyperliquid-evm/contracts/interfaces/IHyperLiquidComposerErrors.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-struct ErrorMessage {
+struct ErrorMessagePayload {
     address refundTo;
     uint256 refundAmount;
     bytes errorMessage;
@@ -10,10 +10,16 @@ struct ErrorMessage {
 interface IHyperLiquidComposerErrors {
     error ErrorMsg(bytes errorMessage);
 
-    error HyperLiquidComposer_Codec_InvalidMessage_UnexpectedLength(bytes message, uint256 length);
     error HyperLiquidComposer_InvalidCall_NotEndpoint(address notEndpointAddress, address expectedEndpointAddress);
     error HyperLiquidComposer_InvalidCall_NotOFT(address internalOFTAddress, address receivedOFTAddress);
-    error HyperLiquidComposer_FailedToSend_HYPE(uint256 amount);
-    error HyperLiquidComposer_FailedToReturn_HYPE_Dust(address to, uint256 amount);
     error HyperLiquidComposer_InvalidCall_NotComposer(address notComposerAddress);
+
+    error HyperLiquidComposer_InvalidComposeMessage(bytes errorMessage);
+
+    error HyperLiquidComposer_FailedToRefund_HYPE(address to, uint256 amount);
+    error HyperLiquidComposer_FailedToSend_HYPE(uint256 amount);
+
+    error HyperLiquidComposer_Codec_InvalidMessage_UnexpectedLength(bytes message, uint256 length);
+
+    error HyperLiquidComposerCore_SpotBalanceRead_Failed(address user, uint64 tokenId);
 }
