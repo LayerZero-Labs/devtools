49118237faacee23a7bcb875573a3b15ea86ef04
Nazreen <nazreen.mohamad@layerzerolabs.org>
2025-04-04 02:02:09 +0800

    🚀 feat(oft-solana): remove need for manual solana endpoint ID input (#1427)

--- examples/oft-solana/tasks/solana/utils.ts
+++ examples/oft-solana/tasks/solana/utils.ts
@@ -1,5 +1,15 @@
 import { Connection, PublicKey } from '@solana/web3.js'
 import { backOff } from 'exponential-backoff'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+import { ChainType, EndpointId, endpointIdToChainType } from '@layerzerolabs/lz-definitions'
+import { OAppOmniGraph } from '@layerzerolabs/ua-devtools'
+import {
+    OAppOmniGraphHardhatSchema,
+    SUBTASK_LZ_OAPP_CONFIG_LOAD,
+    SubtaskLoadConfigTaskArgs,
+    TASK_LZ_OAPP_CONFIG_GET,
+} from '@layerzerolabs/ua-devtools-evm-hardhat'
 
 /**
  * Assert that the account is initialized on the Solana blockchain.  Due to eventual consistency, there is a race
@@ -23,4 +33,45 @@ export const assertAccountInitialized = async (connection: Connection, publicKey
             startingDelay: 5000,
         }
     )
+}
+
+export const findSolanaEndpointIdInGraph = async (
+    hre: HardhatRuntimeEnvironment,
+    oappConfig: string
+): Promise<EndpointId> => {
+    if (!oappConfig) throw new Error('Missing oappConfig')
+
+    let graph: OAppOmniGraph
+    try {
+        graph = await hre.run(SUBTASK_LZ_OAPP_CONFIG_LOAD, {
+            configPath: oappConfig,
+            schema: OAppOmniGraphHardhatSchema,
+            task: TASK_LZ_OAPP_CONFIG_GET,
+        } satisfies SubtaskLoadConfigTaskArgs)
+    } catch (error) {
+        if (error instanceof Error) {
+            throw new Error(`Failed to load OApp configuration: ${error.message}`)
+        } else {
+            throw new Error('Failed to load OApp configuration: Unknown error')
+        }
+    }
+
+    let solanaEid: EndpointId | null = null
+
+    const checkSolanaEndpoint = (eid: EndpointId) => {
+        if (endpointIdToChainType(eid) === ChainType.SOLANA) {
+            if (solanaEid && solanaEid !== eid) {
+                throw new Error(`Multiple Solana Endpoint IDs found: ${solanaEid}, ${eid}`)
+            }
+            solanaEid = eid
+        }
+    }
+
+    for (const { vector } of graph.connections) {
+        checkSolanaEndpoint(vector.from.eid)
+        checkSolanaEndpoint(vector.to.eid)
+        if (solanaEid) return solanaEid
+    }
+
+    throw new Error('No Solana Endpoint ID found. Ensure your OApp configuration includes a valid Solana endpoint.')
 }
