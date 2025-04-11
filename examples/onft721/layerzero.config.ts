001170b8570e871bff68ea3d704bfb5a3158076f
Nazreen <nazreen.mohamad@layerzerolabs.org>
2025-04-11 18:31:19 +0800

    🚀 feat(examples/onft721): apply simple config generator (#1443)

--- examples/onft721/layerzero.config.ts
+++ examples/onft721/layerzero.config.ts
@@ -1,85 +1,73 @@
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
+import { TwoWayConfig, generateConnectionsConfig } from '@layerzerolabs/metadata-tools'
+import { OAppEnforcedOption } from '@layerzerolabs/toolbox-hardhat'
 
-import type { OAppEdgeConfig, OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import type { OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
 
-const sepoliaContract: OmniPointHardhat = {
-    eid: EndpointId.SEPOLIA_V2_TESTNET,
+const optimismContract: OmniPointHardhat = {
+    eid: EndpointId.OPTSEP_V2_TESTNET,
     contractName: 'MyONFT721',
 }
 
-const fujiContract: OmniPointHardhat = {
+const avalancheContract: OmniPointHardhat = {
     eid: EndpointId.AVALANCHE_V2_TESTNET,
     contractName: 'MyONFT721',
 }
 
-const amoyContract: OmniPointHardhat = {
-    eid: EndpointId.AMOY_V2_TESTNET,
+const arbitrumContract: OmniPointHardhat = {
+    eid: EndpointId.ARBSEP_V2_TESTNET,
     contractName: 'MyONFT721',
 }
 
-const DEFAULT_EDGE_CONFIG: OAppEdgeConfig = {
-    enforcedOptions: [
-        {
-            msgType: 1,
-            optionType: ExecutorOptionType.LZ_RECEIVE,
-            gas: 100_000,
-            value: 0,
-        },
-        {
-            msgType: 2,
-            optionType: ExecutorOptionType.COMPOSE,
-            index: 0,
-            gas: 100_000,
-            value: 0,
-        },
-    ],
-}
+// To connect all the above chains to each other, we need the following pathways:
+// Optimism <-> Avalanche
+// Optimism <-> Arbitrum
+// Avalanche <-> Arbitrum
 
-const config: OAppOmniGraphHardhat = {
-    contracts: [
-        {
-            contract: fujiContract,
-        },
-        {
-            contract: sepoliaContract,
-        },
-        {
-            contract: amoyContract,
-        },
-    ],
-    connections: [
-        {
-            from: fujiContract,
-            to: sepoliaContract,
-            config: DEFAULT_EDGE_CONFIG,
-        },
-        {
-            from: fujiContract,
-            to: amoyContract,
-            config: DEFAULT_EDGE_CONFIG,
-        },
-        {
-            from: sepoliaContract,
-            to: fujiContract,
-            config: DEFAULT_EDGE_CONFIG,
-        },
-        {
-            from: sepoliaContract,
-            to: amoyContract,
-            config: DEFAULT_EDGE_CONFIG,
-        },
-        {
-            from: amoyContract,
-            to: sepoliaContract,
-            config: DEFAULT_EDGE_CONFIG,
-        },
-        {
-            from: amoyContract,
-            to: fujiContract,
-            config: DEFAULT_EDGE_CONFIG,
-        },
-    ],
-}
+// For this example's simplicity, we will use the same enforced options values for sending to all chains
+// For production, you should ensure `gas` is set to the correct value through profiling the gas usage of calling OFT._lzReceive(...) on the destination chain
+// To learn more, read https://docs.layerzero.network/v2/concepts/applications/oapp-standard#execution-options-and-enforced-settings
+const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
+    {
+        msgType: 1,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 100_000,
+        value: 0,
+    },
+]
 
-export default config
+// With the config generator, pathways declared are automatically bidirectional
+// i.e. if you declare A,B there's no need to declare B,A
+const pathways: TwoWayConfig[] = [
+    [
+        optimismContract, // Chain A contract
+        avalancheContract, // Chain B contract
+        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+        [1, 1], // [A to B confirmations, B to A confirmations]
+        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
+    ],
+    [
+        optimismContract, // Chain A contract
+        arbitrumContract, // Chain C contract
+        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+        [1, 1], // [A to B confirmations, B to A confirmations]
+        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain C enforcedOptions, Chain A enforcedOptions
+    ],
+    [
+        avalancheContract, // Chain B contract
+        arbitrumContract, // Chain C contract
+        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+        [1, 1], // [A to B confirmations, B to A confirmations]
+        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain C enforcedOptions, Chain B enforcedOptions
+    ],
+]
+
+export default async function () {
+    // Generate the connections config based on the pathways
+    const connections = await generateConnectionsConfig(pathways)
+    return {
+        contracts: [{ contract: optimismContract }, { contract: avalancheContract }, { contract: arbitrumContract }],
+        connections,
+    }
+}
