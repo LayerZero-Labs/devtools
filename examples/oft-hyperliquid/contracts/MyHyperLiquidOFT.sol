037743eb62cfdbc1a21e0c141db9c309af6ecab9
shankar <shankar@layerzerolabs.org>
2025-04-23 17:03:48 +0000

    remove exposed mint() and burn() functions from example
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-hyperliquid/contracts/MyHyperLiquidOFT.sol
+++ examples/oft-hyperliquid/contracts/MyHyperLiquidOFT.sol
@@ -11,12 +11,4 @@ contract MyHyperLiquidOFT is OFT {
         address _lzEndpoint,
         address _delegate
     ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
-
-    function mint(address _to, uint256 _amount) public virtual {
-        _mint(_to, _amount);
-    }
-
-    function burn(address _from, uint256 _amount) public virtual {
-        _burn(_from, _amount);
-    }
 }
