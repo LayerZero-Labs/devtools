874c07264ec5b0d9e173a1662c98cc25aa0dcd5f
shankar <shankar@layerzerolabs.org>
2025-04-23 21:13:09 +0000

    update lz config in examples
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-hyperliquid/layerzero.config.ts
+++ examples/oft-hyperliquid/layerzero.config.ts
@@ -1,5 +1,4 @@
 import { EndpointId } from '@layerzerolabs/lz-definitions'
-import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
 
 import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
 
@@ -39,92 +38,10 @@ const config: OAppOmniGraphHardhat = {
         {
             from: bscTestnetContract,
             to: hyperliquidTestnetContract,
-            config: {
-                enforcedOptions: [
-                    {
-                        msgType: MsgType.SEND,
-                        optionType: ExecutorOptionType.LZ_RECEIVE,
-                        gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
-                        value: 0, // msg.value in wei for EndpointV2.lzReceive
-                    },
-                    {
-                        msgType: MsgType.SEND_AND_CALL,
-                        optionType: ExecutorOptionType.LZ_RECEIVE,
-                        gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
-                        value: 0, // msg.value in wei for EndpointV2.lzReceive
-                    },
-                ],
-                sendLibrary: '',
-                receiveLibraryConfig: {
-                    receiveLibrary: '',
-                    gracePeriod: BigInt(0),
-                },
-                sendConfig: {
-                    executorConfig: {
-                        maxMessageSize: 10_000,
-                        executor: '',
-                    },
-                    ulnConfig: {
-                        confirmations: BigInt(5),
-                        requiredDVNs: [''],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
-                    },
-                },
-                receiveConfig: {
-                    ulnConfig: {
-                        confirmations: BigInt(5),
-                        requiredDVNs: [''],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
-                    },
-                },
-            },
         },
         {
             from: hyperliquidTestnetContract,
             to: bscTestnetContract,
-            config: {
-                enforcedOptions: [
-                    {
-                        msgType: MsgType.SEND,
-                        optionType: ExecutorOptionType.LZ_RECEIVE,
-                        gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
-                        value: 0, // msg.value in wei for EndpointV2.lzReceive
-                    },
-                    {
-                        msgType: MsgType.SEND_AND_CALL,
-                        optionType: ExecutorOptionType.LZ_RECEIVE,
-                        gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
-                        value: 0, // msg.value in wei for EndpointV2.lzReceive
-                    },
-                ],
-                sendLibrary: '',
-                receiveLibraryConfig: {
-                    receiveLibrary: '',
-                    gracePeriod: BigInt(0),
-                },
-                sendConfig: {
-                    executorConfig: {
-                        maxMessageSize: 10_000,
-                        executor: '',
-                    },
-                    ulnConfig: {
-                        confirmations: BigInt(5),
-                        requiredDVNs: [''],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
-                    },
-                },
-                receiveConfig: {
-                    ulnConfig: {
-                        confirmations: BigInt(5),
-                        requiredDVNs: [''],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
-                    },
-                },
-            },
         },
     ],
 }
