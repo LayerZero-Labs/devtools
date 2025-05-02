111fa2d0a433902aef6e8043a5b9a2e244b2a0c9
shankar <shankar@layerzerolabs.org>
2025-05-02 16:03:07 +0000

    remove: unused variables
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-hyperliquid/layerzero.config.ts
+++ examples/oft-hyperliquid/layerzero.config.ts
@@ -2,11 +2,6 @@ import { EndpointId } from '@layerzerolabs/lz-definitions'
 
 import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
 
-enum MsgType {
-    SEND = 1,
-    SEND_AND_CALL = 2,
-}
-
 const bscTestnetContract: OmniPointHardhat = {
     eid: EndpointId.BSC_V2_TESTNET,
     contractName: 'MyOFT',
