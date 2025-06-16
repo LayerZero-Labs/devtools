33fe02a2d05e42584aa17752fe3e3cbd37f125fc
shankar <shankar@layerzerolabs.org>
2025-06-16 19:31:31 +0000

    chore: ovault stores the share and asset OFTs
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/ovault-composer/contracts/OVault.sol
+++ packages/ovault-composer/contracts/OVault.sol
@@ -7,9 +7,15 @@ import { ERC4626Adapter } from "./ERC4626Adapter.sol";
 import { IOVault } from "./interfaces/IOVault.sol";
 
 contract OVault is ERC4626Adapter, IOVault {
+    address public immutable ASSET_OFT;
+    address public immutable SHARE_OFT;
+
     constructor(IOFT _asset, IOFT _share) ERC4626Adapter(_asset.token(), _share.token()) {
         if (!IERC20MintBurnExtension(_share.token()).ERC4626AdapterCompliant()) {
             revert ShareNotERC4626AdapterCompliant();
         }
+
+        ASSET_OFT = address(_asset);
+        SHARE_OFT = address(_share);
     }
 }
--- packages/ovault-composer/contracts/interfaces/IOVault.sol
+++ packages/ovault-composer/contracts/interfaces/IOVault.sol
@@ -3,4 +3,7 @@ pragma solidity ^0.8.20;
 
 interface IOVault {
     error ShareNotERC4626AdapterCompliant();
+
+    function ASSET_OFT() external view returns (address);
+    function SHARE_OFT() external view returns (address);
 }
