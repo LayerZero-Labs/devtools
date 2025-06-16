0261fb5319da2afbb42c94b9018a279fa8cb0e6c
shankar <shankar@layerzerolabs.org>
2025-06-16 19:33:05 +0000

    test: erc20 equivalent solmate
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/ovault-composer/test/utils/mocks/MockERC20MintBurn.sol
+++ packages/ovault-composer/test/utils/mocks/MockERC20MintBurn.sol
@@ -5,4 +5,12 @@ import { ERC20MintBurn } from "../../../contracts/ERC20MintBurn.sol";
 
 contract MockERC20MintBurn is ERC20MintBurn {
     constructor(string memory _name, string memory _symbol) ERC20MintBurn(_name, _symbol) {}
+
+    function mint(address to, uint256 amount) external override {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external override {
+        _burn(from, amount);
+    }
 }
