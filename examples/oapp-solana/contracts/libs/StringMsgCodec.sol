488ad1ccaa9f7211a3fe976a1b7b379b2fd4e3da
nazreen <nazreen.mohamad@layerzerolabs.org>
2025-06-17 18:54:22 +0200

    prefer custom errors over revert strings

--- examples/oapp-solana/contracts/libs/StringMsgCodec.sol
+++ examples/oapp-solana/contracts/libs/StringMsgCodec.sol
@@ -2,12 +2,15 @@
 
 pragma solidity ^0.8.22;
 
+error MsgTooShort();
+error InvalidStringValueLength();
+
 library StringMsgCodec {
     uint8 public constant VANILLA_TYPE = 1;
 
     /// @notice Reconstructs `stringValue` from `_msg`.
     function decode(bytes calldata _msg) internal pure returns (string memory stringValue) {
-        require(_msg.length >= 32, "msg too short");
+        if (_msg.length < 32) revert MsgTooShort();
 
         // 1) Grab the first 32 bytes and ABI-decode as uint256, then cast down to u32
         uint256 lenRaw = abi.decode(_msg[:32], (uint256));
@@ -15,7 +18,7 @@ library StringMsgCodec {
         uint256 N = uint256(strLen);
 
         // 2) Bounds check
-        require(_msg.length >= 32 + N, "invalid stringValue length");
+        if (_msg.length < 32 + N) revert InvalidStringValueLength();
 
         // 3) Extract the UTF-8 string
         stringValue = string(_msg[32:32 + N]);
