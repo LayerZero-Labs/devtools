8795d05488c601211d7cf1f4c45cc9a6df6cac77
Shankar Subramanian <shankar@layerzerolabs.org>
2025-06-19 16:25:11 -0700

    feat: ovault composer (#1583)
    Signed-off-by: shankar <shankar@layerzerolabs.org>

---
+++ packages/ovault-composer-evm/README.md
@@ -0,0 +1,31 @@
+<p align="center">
+  <a href="https://layerzero.network">
+    <img alt="LayerZero" style="max-width: 500px" src="https://d3a2dpnnrypp5h.cloudfront.net/bridge-app/lz.png"/>
+  </a>
+</p>
+
+<h1 align="center">@layerzerolabs/ovault-composer</h1>
+
+<!-- The badges section -->
+<p align="center">
+  <!-- Shields.io NPM published package version -->
+  <a href="https://www.npmjs.com/package/@layerzerolabs/ovault-composer"><img alt="NPM Version" src="https://img.shields.io/npm/v/@layerzerolabs/ovault-composer"/></a>
+  <!-- Shields.io NPM downloads -->
+  <a href="https://www.npmjs.com/package/@layerzerolabs/ovault-composer"><img alt="Downloads" src="https://img.shields.io/npm/dm/@layerzerolabs/ovault-composer"/></a>
+  <!-- Shields.io license badge -->
+  <a href="https://www.npmjs.com/package/@layerzerolabs/ovault-composer"><img alt="NPM License" src="https://img.shields.io/npm/l/@layerzerolabs/ovault-composer"/></a>
+</p>
+
+## Installation
+
+```bash
+pnpm install @layerzerolabs/ovault-composer
+```
+
+```bash
+yarn install @layerzerolabs/ovault-composer
+```
+
+```bash
+npm install @layerzerolabs/ovault-composer
+```
---
+++ packages/ovault-composer-evm/contracts/interfaces/IOVaultComposer.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import { IOAppComposer } from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppComposer.sol";
+import { IOFT, SendParam, MessagingFee } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
+
+struct FailedMessage {
+    address oft;
+    SendParam sendParam;
+    address refundOFT;
+    SendParam refundSendParam;
+}
+
+enum FailedState {
+    NotFound,
+    CanOnlyRefund,
+    CanOnlyRetry,
+    CanRetryWithSwap
+}
+
+interface IOVaultComposer is IOAppComposer {
+    /// ========================== EVENTS =====================================
+    event DecodeFailed(bytes32 indexed guid, address indexed oft, bytes message);
+    event Sent(bytes32 indexed guid, address indexed oft);
+    event SentOnHub(address indexed receiver, address indexed oft, uint256 amountLD);
+    event SendFailed(bytes32 indexed guid, address indexed oft);
+    event Refunded(bytes32 indexed guid, address indexed oft);
+    event Retried(bytes32 indexed guid, address indexed oft);
+    event GenericError(bytes32 indexed guid, address indexed oft, bytes errMsg);
+    event NoPeer(bytes32 indexed guid, address indexed oft, uint32 dstEid);
+
+    /// ========================== Error Messages =====================================
+    error ShareOFTShouldBeLockboxAdapter(address share);
+
+    error OnlyEndpoint(address caller);
+    error OnlySelf(address caller);
+    error OnlyOFT(address oft);
+    error OnlyAsset(address asset);
+    error OnlyShare(address share);
+    error CanNotRefund(bytes32 guid);
+    error CanNotRetry(bytes32 guid);
+    error CanNotWithdraw(bytes32 guid);
+    error NotEnoughTargetTokens(uint256 amountLD, uint256 minAmountLD);
+
+    /// ========================== GLOBAL VARIABLE FUNCTIONS =====================================
+    function ASSET_OFT() external view returns (address);
+    function SHARE_OFT() external view returns (address);
+    function ENDPOINT() external view returns (address);
+
+    /// ========================== FUNCTIONS =====================================
+    function executeOVaultAction(
+        address _oft,
+        uint256 _amount,
+        SendParam calldata _sendParam
+    ) external returns (uint256 vaultAmount);
+
+    function refund(bytes32 guid, bytes memory extraOptions) external payable;
+    function retry(bytes32 guid, bytes memory extraOptions) external payable;
+    function retryWithSwap(bytes32 guid, bytes memory extraOptions) external payable;
+    function send(address _oft, SendParam calldata _sendParam) external payable;
+
+    function failedGuidState(bytes32 guid) external view returns (FailedState);
+
+    receive() external payable;
+}
