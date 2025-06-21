c49355089a75504ee0e15dd650cdbcff248c5dc7
shankar <shankar@layerzerolabs.org>
2025-06-21 00:38:33 +0000

    using eslintrc from oft-evm
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/ovault-evm/.eslintrc.js
+++ examples/ovault-evm/.eslintrc.js
@@ -1,29 +1,12 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-    root: true, // Add this line to prevent ESLint from looking up the directory tree
+    root: true,
     extends: ['@layerzerolabs/eslint-config-next/recommended'],
     rules: {
         // @layerzerolabs/eslint-config-next defines rules for turborepo-based projects
         // that are not relevant for this particular project
         'turbo/no-undeclared-env-vars': 'off',
-        // Explicitly disable no-unresolved for LayerZero packages
-        'import/no-unresolved': [
-            'error',
-            {
-                ignore: ['@layerzerolabs/hyperliquid-composer'],
-            },
-        ],
-    },
-    settings: {
-        'import/resolver': {
-            typescript: {
-                project: './tsconfig.json',
-            },
-            node: {
-                moduleDirectory: ['node_modules', '.'],
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
+        'import/no-unresolved': 'warn', // Updated to warn to avoid erratic popping up of this particular error for '@layerzerolabs/metadata-tools' even when the package has been installed correctly
     },
 };
