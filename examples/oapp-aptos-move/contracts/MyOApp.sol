08b767c8673d6a3578f1598caa5738ca5da08f11
alexanderliteplo <alexanderliteplo@gmail.com>
2025-06-23 16:04:48 -0700

    correcting licenses

--- examples/oapp-aptos-move/contracts/MyOApp.sol
+++ examples/oapp-aptos-move/contracts/MyOApp.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
 import { OApp, Origin, MessagingFee } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
--- examples/oft-hyperliquid/contracts/MyOFT.sol
+++ examples/oft-hyperliquid/contracts/MyOFT.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
