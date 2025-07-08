404d28cb0a5640c4d3eeccf479d93164cb403630
ravinagill15 <ravina@layerzerolabs.org>
2025-07-08 10:29:25 -0700

    removed redundant script package

--- packages/script-devtools-evm-foundry/script/GasProfiling/README.md
+++
@@ -1,5 +0,0 @@
-# GasProfiling
-
-Can be used to profile the gas usage of an OFT for its `lzReceive` and `lzCompose` functions.
-
-Refer: [OFTProfilerExample.s.sol](./OFTProfilerExample.s.sol)
--- packages/script-devtools-evm-foundry/script/SimulateReceive/utils.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.22;
-
-import { SimulateReceive } from "./SimulateReceive.s.sol";
-
-abstract contract SimulateScript is SimulateReceive {}
