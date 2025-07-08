cf98cb63a06e1831d603e14213346677da08cc05
ravinagill15 <ravina@layerzerolabs.org>
2025-07-08 10:27:41 -0700

    renamed dir to scripts

---
+++ packages/script-devtools-evm-foundry/scripts/GasProfiling/utils.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+import { GasProfilerScript } from "./GasProfiler.s.sol";
+
+abstract contract GasProfilingScript is GasProfilerScript {}
