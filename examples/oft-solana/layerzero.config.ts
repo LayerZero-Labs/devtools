12b4572bb4c4d6146ff03bbdb74503208a548754
nazreen <10964594+nazreen@users.noreply.github.com>
2025-07-14 17:44:16 +0200

    specify exact messaeg value for when transferring to Solana

--- examples/oft-solana/layerzero.config.ts
+++ examples/oft-solana/layerzero.config.ts
@@ -31,7 +31,9 @@ const SOLANA_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
         msgType: 1,
         optionType: ExecutorOptionType.LZ_RECEIVE,
         gas: 200000,
-        value: 2500000,
+        value: 2039280, // This figure represents lamports on Solana. When sending OFTs to Solana, SOL is needed (for rent) to initialize the recipient's token account. This value is the exact rent value needed for SPL token accounts (0.00203928 SOL).
+        // For Token2022 token accounts, you will need to increase `value` to a higher amount, which depends on the token account size, which in turns depends on the extentions that you enable.
+        // To learn more about Solana's rent concept, refer to https://solana.com/docs/core/accounts#rent
     },
 ]
 
