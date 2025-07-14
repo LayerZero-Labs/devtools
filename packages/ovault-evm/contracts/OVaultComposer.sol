c1718c1633a79045b4442d8ff8e00b0f8ff7b4da
shankar <shankar@layerzerolabs.org>
2025-07-14 21:38:49 +0000

    issue-12: remove duplicated safeERC20 import and library usage
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/ovault-evm/contracts/OVaultComposer.sol
+++ packages/ovault-evm/contracts/OVaultComposer.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.22;
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
@@ -19,7 +18,6 @@ import { IOVaultComposer, FailedMessage, FailedState } from "./interfaces/IOVaul
 contract OVaultComposer is IOVaultComposer, ReentrancyGuard {
     using OFTComposeMsgCodec for bytes;
     using OFTComposeMsgCodec for bytes32;
-    using SafeERC20 for IERC20;
     using SafeERC20 for IERC20;
 
     address public immutable ASSET_OFT; // any OFT
