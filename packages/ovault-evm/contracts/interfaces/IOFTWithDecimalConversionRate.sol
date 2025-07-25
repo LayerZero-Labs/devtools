1b1e3419b9c53e3d074ac7cab0f5d1969a9e1bfc
Shankar Subramanian <shankar@layerzerolabs.org>
2025-07-25 13:33:25 -0700

    ovault atomic synchronous composer (#1629)
    Signed-off-by: shankar <shankar@layerzerolabs.org>
    Co-authored-by: Blockchain Warlock <39101443+TRileySchwarz@users.noreply.github.com>

--- packages/ovault-evm/contracts/interfaces/IOFTWithDecimalConversionRate.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
-
-import { IOFT } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
-
-interface IOFTWithDecimalConversionRate is IOFT {
-    function decimalConversionRate() external view returns (uint256);
-}
