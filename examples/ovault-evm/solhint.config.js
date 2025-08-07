ed8c24e081c84903bfe5690e08433ee554ddd346
shankar <shankar@layerzerolabs.org>
2025-08-07 22:32:15 +0000

    solhint: ignore 2 contracts in MyOVaultComposer.sol
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/ovault-evm/solhint.config.js
+++ examples/ovault-evm/solhint.config.js
@@ -1,1 +1,9 @@
-module.exports = require('@layerzerolabs/solhint-config');
+const config = require('@layerzerolabs/solhint-config');
+
+module.exports = {
+    ...config,
+    rules: {
+        ...config.rules,
+        'one-contract-per-file': 'off',
+    },
+};
