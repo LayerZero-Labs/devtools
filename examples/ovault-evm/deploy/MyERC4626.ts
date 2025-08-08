e4cebd60529ea3b02ec9af82c37568266e11719c
shankar <shankar@layerzerolabs.org>
2025-08-08 22:46:45 +0000

    feat: simplified deploy scripts
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/ovault-evm/deploy/MyERC4626.ts
+++ examples/ovault-evm/deploy/MyERC4626.ts
@@ -1,15 +1,17 @@
 import assert from 'assert'
 
+import { ethers } from 'ethers'
 import { type DeployFunction } from 'hardhat-deploy/types'
 
-import { assetOFT, oVault, shareOFT } from './tokenConfig'
 import {
-    TokenDeployConfig,
-    checkAddressOrGetDeployment,
-    isContractAddress,
-    isVaultChain,
-    onlyDeployVault,
-} from './types'
+    TokenMetadata,
+    assetConfig,
+    deployContract,
+    getConfigType,
+    oVaultConfig,
+    shareConfig,
+    shouldDeployOnNetwork,
+} from '../devtools'
 
 /// @dev It is possible to ONLY deploy the vault by setting the environment variable `ONLY_VAULT=1`
 /// ex: // `ONLY_VAULT=1 npx hardhat deploy --tags ovault`
@@ -24,45 +26,74 @@ const deploy: DeployFunction = async (hre) => {
     console.log(`Network: ${hre.network.name}`)
     console.log(`Deployer: ${deployer}`)
 
+    const networkEid = hre.network.config?.eid
+    assert(networkEid, `Network ${hre.network.name} is missing 'eid' in config`)
+    const endpointV2Deployment = await hre.deployments.get('EndpointV2')
+
     // Validate that vault contracts are deployed only on hub chain
-    if (!isVaultChain(hre.network.config)) {
+    if (oVaultConfig.eid != hre.network.config.eid) {
         console.error(
             `Vault contracts can only be deployed on hub chain. Network '${hre.network.name}' is configured as spoke chain (isHubChain: false). Please deploy on a hub chain.`
         )
         return
     }
 
-    const assetOFTAddress = await checkAddressOrGetDeployment(hre, assetOFT)
+    let assetOFTAddress: string
+    let vaultAddress: string
 
-    const shareOFTConfig = shareOFT as TokenDeployConfig
-    const { address: ovaultAddress } = await deploy(oVault.vault, {
+    if (getConfigType(assetConfig) === 'OFTAddress') {
+        // The above asserts that assetConfig is a string
+        assetOFTAddress = ethers.utils.getAddress(assetConfig.oft as string)
+        console.log(`Using pre-deployed asset OFT at address: ${assetOFTAddress}`)
+    } else if (shouldDeployOnNetwork(assetConfig, networkEid)) {
+        const assetOFTMetadata = assetConfig.oft as TokenMetadata
+        const deployment = await deployContract(hre, assetOFTMetadata.contractName, deployer, [
+            assetOFTMetadata.tokenName,
+            assetOFTMetadata.tokenSymbol,
+            endpointV2Deployment.address,
+            deployer,
+        ])
+        assetOFTAddress = deployment.address
+    } else {
+        throw new Error(`Skipping asset OFT deployment on network: ${hre.network.name}`)
+    }
+
+    if (ethers.utils.isAddress(oVaultConfig.vault)) {
+        vaultAddress = oVaultConfig.vault
+    } else {
+        const deployment = await deployContract(hre, oVaultConfig.vault, deployer, [
+            shareConfig.oft.tokenName,
+            shareConfig.oft.tokenSymbol,
+            assetOFTAddress,
+        ])
+
+        vaultAddress = deployment.address
+    }
+
+    const shareAdapterContractName = oVaultConfig.shareAdapterContractName
+
+    const deployment = await deploy(shareAdapterContractName, {
         from: deployer,
-        args: [shareOFTConfig.tokenName, shareOFTConfig.tokenSymbol, assetOFTAddress],
+        args: [vaultAddress, endpointV2Deployment.address, deployer],
         log: true,
         skipIfAlreadyDeployed: true,
     })
 
-    console.log(`Deployed contract: ${oVault.vault}, network: ${hre.network.name}, address: ${ovaultAddress}`)
+    const shareOFTAdapterAddress = deployment.address
 
-    // Early exit if we only want to deploy the vault
-    // Executing the same without 'ONLY_VAULT=1' will resume the share OFT adapter deployment.
-    if (onlyDeployVault()) return
+    console.log(
+        `Deployed contract: ${shareAdapterContractName}, network: ${hre.network.name}, address: ${shareOFTAdapterAddress}`
+    )
 
-    const shareAdapterContractName = oVault.shareAdapterContractName
-    if (isContractAddress(shareAdapterContractName)) {
-        throw new Error(`Should be a contract name. Found: ${shareAdapterContractName}`)
-    }
-
-    const endpointV2Deployment = await hre.deployments.get('EndpointV2')
-    const { address: shareOFTAdapterAddress } = await deploy(shareAdapterContractName, {
+    const { address: composerAddress } = await deploy(oVaultConfig.composer, {
         from: deployer,
-        args: [ovaultAddress, endpointV2Deployment.address, deployer],
+        args: [vaultAddress, assetOFTAddress, shareOFTAdapterAddress],
         log: true,
         skipIfAlreadyDeployed: true,
     })
 
     console.log(
-        `Deployed contract: ${shareAdapterContractName}, network: ${hre.network.name}, address: ${shareOFTAdapterAddress}`
+        `Deployed contract: ${oVaultConfig.composer}, network: ${hre.network.name}, address: ${composerAddress}`
     )
 }
 
