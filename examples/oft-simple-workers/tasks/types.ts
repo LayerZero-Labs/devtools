1753b4b7fb9fd9e3d9aa472a49eb47467dfacbe5
shankar <shankar@layerzerolabs.org>
2025-08-16 02:53:46 +0000

    seperate lz config for simple worker
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- examples/oft-simple-workers/tasks/types.ts
+++
@@ -1,6 +0,0 @@
-export interface SendResult {
-    txHash: string // EVM: receipt.transactionHash
-    scanLink: string // LayerZeroScan link for cross-chain tracking
-    outboundNonce: string // Outbound nonce for SimpleDVN processing (converted from Number)
-    extraOptions?: string // Hex-encoded options used in the transaction. Only used in --simple-workers mode.
-}
--- examples/oft/CHANGELOG.md
+++ examples/oft/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @layerzerolabs/oft-example
 
+## 0.7.3
+
+### Patch Changes
+
+- f974ed4: Update LayerZero OFT example to support simple config custom DVNs and Executors.
+
 ## 0.7.2
 
 ### Patch Changes
