f8e683eaa24b6855b67e013de1c8e03593a0df37
shankar <shankar@layerzerolabs.org>
2025-08-21 23:45:57 +0000

    test: fee token extension tests
    Signed-off-by: shankar <shankar@layerzerolabs.org>

---
+++ packages/hyperliquid-composer/test/mocks/FeeTokenMock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import { FeeToken } from "../../contracts/extensions/FeeToken.sol";
+import { HyperLiquidComposer } from "../../contracts/HyperLiquidComposer.sol";
+
+contract FeeTokenMock is FeeToken {
+    constructor(
+        address _oft,
+        uint64 _coreIndexId,
+        int64 _assetDecimalDiff,
+        address _REFUND_ADDRESS
+    ) HyperLiquidComposer(_oft, _coreIndexId, _assetDecimalDiff, _REFUND_ADDRESS) {}
+
+    function createRawActionPayloadERC20(address _to, uint64 _coreAmount) public view returns (bytes memory payload) {
+        bytes memory action = abi.encode(_to, oftAsset.coreIndexId, _coreAmount);
+        payload = abi.encodePacked(SPOT_SEND_HEADER, action);
+    }
+}
