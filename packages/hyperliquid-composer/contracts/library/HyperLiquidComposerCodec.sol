6445b2d33a86fedf12d5937b5eeeb5cb02ce7587
shankar <shankar@layerzerolabs.org>
2025-08-21 18:09:39 +0000

    docs: natspec and cleanup
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/hyperliquid-composer/contracts/library/HyperLiquidComposerCodec.sol
+++ packages/hyperliquid-composer/contracts/library/HyperLiquidComposerCodec.sol
@@ -5,6 +5,11 @@ import { ErrorMessagePayload, IHyperLiquidComposerErrors } from "../interfaces/I
 import { IHyperAsset, IHyperAssetAmount } from "../interfaces/IHyperLiquidComposerCore.sol";
 import { OFTComposeMsgCodec } from "@layerzerolabs/oft-evm/contracts/libs/OFTComposeMsgCodec.sol";
 
+/**
+ * @title HyperLiquidComposerCodec
+ * @author LayerZero Labs (@shankars99)
+ * @notice Library for computing hyperliquid asset bridges, and converting between EVM and HyperCore amounts
+ */
 library HyperLiquidComposerCodec {
     /// @dev This is the largest possible token supply on HyperCore
     uint64 public constant EVM_MAX_TRANSFERABLE_INTO_CORE_PER_TX = type(uint64).max;
@@ -15,34 +20,34 @@ library HyperLiquidComposerCodec {
     address public constant BASE_ASSET_BRIDGE_ADDRESS = 0x2000000000000000000000000000000000000000;
     uint256 public constant BASE_ASSET_BRIDGE_ADDRESS_UINT256 = uint256(uint160(BASE_ASSET_BRIDGE_ADDRESS));
 
-    /// @notice Converts a core index id to an asset bridge address
-    /// @notice This function is called by the HyperLiquidComposer contract
-    ///
-    /// @param _coreIndexId The core index id to convert
-    ///
-    /// @return _assetBridgeAddress The asset bridge address
+    /**
+     * @notice Converts a core index id to an asset bridge address
+     * @notice This function is called by the HyperLiquidComposer contract
+     * @param _coreIndexId The core index id to convert
+     * @return _assetBridgeAddress The asset bridge address
+     */
     function into_assetBridgeAddress(uint256 _coreIndexId) internal pure returns (address) {
         return address(uint160(BASE_ASSET_BRIDGE_ADDRESS_UINT256 + _coreIndexId));
     }
 
-    /// @notice Converts an asset bridge address to a core index id
-    /// @notice This function is called by the HyperLiquidComposer contract
-    ///
-    /// @param _assetBridgeAddress The asset bridge address to convert
-    ///
-    /// @return _coreIndexId The core index id
+    /**
+     * @notice Converts an asset bridge address to a core index id
+     * @notice This function is called by the HyperLiquidComposer contract
+     * @param _assetBridgeAddress The asset bridge address to convert
+     * @return _coreIndexId The core index id
+     */
     function into_tokenId(address _assetBridgeAddress) internal pure returns (uint256) {
         return uint256(uint160(_assetBridgeAddress)) - BASE_ASSET_BRIDGE_ADDRESS_UINT256;
     }
 
-    /// @notice Converts an amount and an asset to a evm amount, core amount, and dust
-    /// @notice This function is called by the HyperLiquidComposer contract
-    ///
-    /// @param _amount The amount to convert
-    /// @param _assetBridgeSupply The maximum amount transferable capped by the number of tokens located on the HyperCore's side of the asset bridge
-    /// @param _asset The asset to convert
-    ///
-    /// @return IHyperAssetAmount memory - The evm amount, core amount, and dust
+    /**
+     * @notice Converts an amount and an asset to a evm amount, core amount, and dust
+     * @notice This function is called by the HyperLiquidComposer contract
+     * @param _amount The amount to convert
+     * @param _assetBridgeSupply The maximum amount transferable capped by the number of tokens located on the HyperCore's side of the asset bridge
+     * @param _asset The asset to convert
+     * @return IHyperAssetAmount memory - The evm amount, core amount, and dust
+     */
     function into_hyperAssetAmount(
         uint256 _amount,
         uint64 _assetBridgeSupply,
@@ -54,13 +59,9 @@ library HyperLiquidComposerCodec {
 
         uint64 maxTransferableCoreAmount = min_u64(_assetBridgeSupply, EVM_MAX_TRANSFERABLE_INTO_CORE_PER_TX);
 
-        /// @dev The general functioning of these function calls are:
-        /// @dev 1. Compute the max evm sendable amount from the max core amount computed above as maxTransferableCoreAmount
-        /// @dev 2. This gives us evmMaxTransferable which is a u256 scaling of the core amount by 10.pow(decimalDiff)
-        /// @dev 3. evmMaxTransferable is the maximal number of tokens that we can send across in a single SendSpot transaction
-        /// @dev 4. If amount > evmMaxTransferable then only send evmMaxTransferable and refund the difference by accumulating it in dust
-        /// @dev 5. The above step bounds evm amount to be an input that is guaranteed to, on hypercore, have a maximum of u64.
-        /// @dev 6. Compute amountCore from evmMaxTransferable by evmMaxTransferable / 10.pow(decimalDiff)
+        /// @dev HyperLiquid decimal conversion: Scale EVM (u256,evmDecimals) -> Core (u64,coreDecimals)
+        /// @dev Dust contains the "dust" from scaling and is used for refund.
+        /// @dev Core amount is guaranteed to be within u64 range.
         if (_asset.decimalDiff > 0) {
             (amountEVM, dust, amountCore) = into_hyperAssetAmount_decimal_difference_gt_zero(
                 _amount,
@@ -78,16 +79,16 @@ library HyperLiquidComposerCodec {
         return IHyperAssetAmount({ evm: amountEVM, dust: dust, core: amountCore });
     }
 
-    /// @notice Computes hyperAssetAmount when EVM decimals > Core decimals
-    /// @notice This function is called by the HyperLiquidComposer contract
-    ///
-    /// @param _amount The amount to convert
-    /// @param _maxTransferableCoreAmount The maximum transferrable amount capped by the asset bridge has range [0,u64.max]
-    /// @param _extraWeiDecimals The decimal difference between HyperEVM and HyperCore
-    ///
-    /// @return amountEVM The EVM amount
-    /// @return dust The dust amount
-    /// @return amountCore The core amount
+    /**
+     * @notice Computes hyperAssetAmount when EVM decimals > Core decimals
+     * @notice This function is called by the HyperLiquidComposer contract
+     * @param _amount The amount to convert
+     * @param _maxTransferableCoreAmount The maximum transferrable amount capped by the asset bridge has range [0,u64.max]
+     * @param _extraWeiDecimals The decimal difference between HyperEVM and HyperCore
+     * @return amountEVM The EVM amount
+     * @return dust The dust amount
+     * @return amountCore The core amount
+     */
     function into_hyperAssetAmount_decimal_difference_gt_zero(
         uint256 _amount,
         uint64 _maxTransferableCoreAmount,
@@ -109,16 +110,16 @@ library HyperLiquidComposerCodec {
         amountCore = uint64(amountEVM / scale);
     }
 
-    /// @notice Computes hyperAssetAmount when EVM decimals < Core decimals and 0
-    /// @notice This function is called by the HyperLiquidComposer contract
-    ///
-    /// @param _amount The amount to convert
-    /// @param _maxTransferableCoreAmount The maximum transferrable amount capped by the asset bridge
-    /// @param _extraWeiDecimals The decimal difference between HyperEVM and HyperCore
-    ///
-    /// @return amountEVM The EVM amount
-    /// @return dust The dust amount
-    /// @return amountCore The core amount
+    /**
+     * @notice Computes hyperAssetAmount when EVM decimals < Core decimals and 0
+     * @notice This function is called by the HyperLiquidComposer contract
+     * @param _amount The amount to convert
+     * @param _maxTransferableCoreAmount The maximum transferrable amount capped by the asset bridge
+     * @param _extraWeiDecimals The decimal difference between HyperEVM and HyperCore
+     * @return amountEVM The EVM amount
+     * @return dust The dust amount
+     * @return amountCore The core amount
+     */
     function into_hyperAssetAmount_decimal_difference_leq_zero(
         uint256 _amount,
         uint64 _maxTransferableCoreAmount,
@@ -135,36 +136,6 @@ library HyperLiquidComposerCodec {
 
         /// @dev Guaranteed to be in the range of [0, u64.max] because it is uppoerbounded by uint64 _maxTransferableCoreAmount
         amountCore = uint64(amountEVM * scale);
-    }
-
-    /// @notice Extracts the error payload from a bytes array
-    /// @notice This function is called by the HyperLiquidComposer contract
-    /// @dev Strips out the revert message to extract the payload ErrorMsg(bytes errorMessage) - ('0x' + 32 bytes) * 2 = 64 bytes
-    ///
-    /// @param _err The bytes array to extract the error payload from
-    ///
-    /// @return _errorPayload The error payload
-    function extractErrorPayload(bytes calldata _err) internal pure returns (bytes memory) {
-        return _err[64 + 4:];
-    }
-
-    /// @notice Creates an error message payload
-    /// @notice This function is called by the HyperLiquidComposer contract
-    ///
-    /// @param _errorMessage The error message to create
-    /// @param _sender The sender of the error message
-    /// @param _amountLD The amount of the error message
-    ///
-    /// @return _errorMessagePayload The error message payload
-    function createErrorMessage(
-        bytes memory _errorMessage,
-        address _sender,
-        uint256 _amountLD
-    ) internal pure returns (bytes memory) {
-        return
-            abi.encode(
-                ErrorMessagePayload({ refundTo: _sender, refundAmount: _amountLD, errorMessage: _errorMessage })
-            );
     }
 
     function min_u256(uint256 a, uint256 b) internal pure returns (uint256) {
