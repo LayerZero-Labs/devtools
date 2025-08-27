45b17be5283bb470b2bee07a3961c26000d047cc
shankar <shankar@layerzerolabs.org>
2025-08-27 20:32:27 +0000

    remove unrequired variables
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/hyperliquid-composer/contracts/HyperLiquidCore.sol
+++ packages/hyperliquid-composer/contracts/HyperLiquidCore.sol
@@ -5,8 +5,8 @@ import { ICoreWriter } from "./interfaces/ICoreWriter.sol";
 
 struct SpotBalance {
     uint64 total;
-    uint64 hold;
-    uint64 entryNtl;
+    uint64 hold; // Unused in this implementation
+    uint64 entryNtl; // Unused in this implementation
 }
 
 struct CoreUserExists {
@@ -16,7 +16,6 @@ struct CoreUserExists {
 abstract contract HyperLiquidCore {
     // Chain IDs
     // https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/hyperevm#mainnet
-    uint256 internal constant HYPE_CHAIN_ID_TESTNET = 998;
     uint256 internal constant HYPE_CHAIN_ID_MAINNET = 999;
 
     // Core Indexes
@@ -25,35 +24,30 @@ abstract contract HyperLiquidCore {
     // https://app.hyperliquid.xyz/explorer/token/0x0d01dc56dcaaca66ad901c959b4011ec
     uint64 internal constant HYPE_CORE_INDEX_MAINNET = 150;
 
-    // Decimal Constants - Pre-computed for gas efficiency
-    uint8 internal constant HYPE_EVM_DECIMALS = 18;
-    uint8 internal constant HYPE_CORE_DECIMALS = 8;
-    int64 internal constant HYPE_DECIMAL_DIFF = 10; // Pre-computed: 18 - 8
+    /// @dev Decimal Constants - Pre-computed for gas efficiency
+    /// @dev uint8 HYPE_EVM_DECIMALS = 18;
+    /// @dev uint8 HYPE_CORE_DECIMALS = 8;
+    int64 internal constant HYPE_DECIMAL_DIFF = 10;
     address internal constant HYPE_ASSET_BRIDGE = 0x2222222222222222222222222222222222222222;
-    uint256 internal constant HYPE_SCALE_FACTOR = 10 ** 10; // Pre-computed: 10^(18-8)
 
     // Precompile Addresses
     address internal constant HLP_CORE_WRITER = 0x3333333333333333333333333333333333333333;
     address internal constant SPOT_BALANCE_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000000801;
     address internal constant CORE_USER_EXISTS_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000000810;
 
-    // Pre-computed headers for gas efficiency
-    bytes1 public constant CORE_WRITER_VERSION = 0x01;
-    bytes3 public constant SPOT_SEND_ACTION_ID = 0x000006;
+    /// @dev Pre-computed headers for gas efficiency
+    /// @dev bytes1 CORE_WRITER_VERSION = 0x01;
+    /// @dev bytes3 SPOT_SEND_ACTION_ID = 0x000006;
     bytes4 public constant SPOT_SEND_HEADER = 0x01000006; // Pre-computed concatenation
 
     function spotBalance(address user, uint64 token) public view returns (SpotBalance memory) {
-        bool success;
-        bytes memory result;
-        (success, result) = SPOT_BALANCE_PRECOMPILE_ADDRESS.staticcall(abi.encode(user, token));
+        (bool success, bytes memory result) = SPOT_BALANCE_PRECOMPILE_ADDRESS.staticcall(abi.encode(user, token));
         require(success, "SpotBalance precompile call failed");
         return abi.decode(result, (SpotBalance));
     }
 
     function coreUserExists(address user) public view returns (CoreUserExists memory) {
-        bool success;
-        bytes memory result;
-        (success, result) = CORE_USER_EXISTS_PRECOMPILE_ADDRESS.staticcall(abi.encode(user));
+        (bool success, bytes memory result) = CORE_USER_EXISTS_PRECOMPILE_ADDRESS.staticcall(abi.encode(user));
         require(success, "Core user exists precompile call failed");
         return abi.decode(result, (CoreUserExists));
     }
