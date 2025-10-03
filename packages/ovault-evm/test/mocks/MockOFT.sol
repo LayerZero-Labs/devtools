3f3e8258929e15fe6c87e3ad24d7af3e96ebd4a3
shankar <shankar@layerzerolabs.org>
2025-10-03 23:23:40 +0000

    test: mock contracts and interfaces
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/ovault-evm/test/mocks/MockOFT.sol
+++ packages/ovault-evm/test/mocks/MockOFT.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.22;
 
 import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 import { OFTAdapter } from "@layerzerolabs/oft-evm/contracts/OFTAdapter.sol";
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @dev The Path struct contains the bus base fare multiplier bps and the credit in the same slot for gas saving.
+struct Path {
+    uint64 credit; // available credit for the path, in SD
+}
 
 contract MockOFT is OFT {
     constructor(
@@ -29,4 +34,19 @@ contract MockOFTAdapter is OFTAdapter {
         address _lzEndpoint,
         address _delegate
     ) OFTAdapter(_token, _lzEndpoint, _delegate) Ownable(msg.sender) {}
+}
+
+contract StargatePool is OFT {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _lzEndpoint,
+        address _delegate
+    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
+
+    mapping(uint32 eid => Path path) public paths;
+}
+
+interface IStargatePool {
+    function paths(uint32 eid) external view returns (Path memory);
 }
