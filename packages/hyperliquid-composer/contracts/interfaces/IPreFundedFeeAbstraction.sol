631c5ffdf23ef9160234f173cc19715a4b75268e
shankar <shankar@layerzerolabs.org>
2025-10-15 19:50:02 +0000

    review changes
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/hyperliquid-composer/contracts/interfaces/IPreFundedFeeAbstraction.sol
+++ packages/hyperliquid-composer/contracts/interfaces/IPreFundedFeeAbstraction.sol
@@ -19,15 +19,21 @@ struct TokenInfo {
 
 interface IPreFundedFeeAbstraction {
     error InvalidSpot();
-    error ActivationCostTooLow();
-    error ActivationFeeTooHigh();
+    error MinUSDAmtGreaterThanU64Max();
     error NoFeesToConvert();
 
     event FeeCollected(uint256 amount);
 
     /// Immutable variables
     function SPOT_ID() external view returns (uint64);
+    function QUOTE_ASSET_INDEX() external view returns (uint64);
+    function QUOTE_ASSET_WEI_DECIMALS() external view returns (uint8);
+    function SPOT_PRICE_DECIMALS() external view returns (uint64);
+    function ACTIVATION_COST() external view returns (uint64);
 
-    function getAccruedFeeUsdValue() external view returns (uint64);
+    function getAccruedFeeUsdValue() external view returns (uint256);
     function retrieveAccruedFees(uint64 _coreAmount, address _to) external;
+
+    function MIN_USD_PRE_FUND_WEI_VALUE() external view returns (uint64);
+    function MIN_USD_PRE_FUND_AMOUNT() external view returns (uint64);
 }
