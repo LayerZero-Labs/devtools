31522741810bd1c29626f276f1a6f468dddf7340
Tino Martínez Molina <tino@martinezmolina.es>
2025-10-15 17:20:43 -0700

    chore: hl prefunded fee abstraction composer extension suggestions (#1762)

--- packages/hyperliquid-composer/contracts/interfaces/IPreFundedFeeAbstraction.sol
+++ packages/hyperliquid-composer/contracts/interfaces/IPreFundedFeeAbstraction.sol
@@ -18,16 +18,23 @@ struct TokenInfo {
 }
 
 interface IPreFundedFeeAbstraction {
-    error InvalidSpot();
+    /// @notice Spot pair base asset does not match ERC20 core asset.
+    error InvalidSpotPair();
+    /// @notice Minimum USD amount greater than `uint64` max.
     error MinUSDAmtGreaterThanU64Max();
-    error NoFeesToConvert();
+    /// @notice Activation overhead fee must be greater than 0.
+    error ZeroActivationOverheadFee();
 
-    event FeeCollected(uint256 amount);
+    /**
+     * @notice Emitted when activation fee is collected.
+     * @param user Address to be activated in HyperCore.
+     * @param amount Activation fee collected in quote asset decimals.
+     */
+    event FeeCollected(address indexed user, uint256 amount);
 
-    /// Immutable variables
+    function ACTIVATION_COST() external view returns (uint64);
     function SPOT_PAIR_ID() external view returns (uint64);
     function QUOTE_ASSET_INDEX() external view returns (uint64);
     function QUOTE_ASSET_DECIMALS() external view returns (uint64);
     function SPOT_PRICE_DECIMALS() external view returns (uint64);
-    function ACTIVATION_COST() external view returns (uint64);
 }
