37b174e1835423285d9c543aa0bccfaa05ef8344
shankar <shankar@layerzerolabs.org>
2025-11-04 22:27:01 +0000

    chore: refactor to avoid double precompile calls
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- packages/hyperliquid-composer/contracts/extensions/PreFundedFeeAbstraction.sol
+++ packages/hyperliquid-composer/contracts/extensions/PreFundedFeeAbstraction.sol
@@ -121,14 +121,12 @@ abstract contract PreFundedFeeAbstraction is FeeToken, RecoverableComposer, IPre
             _amountLD
         );
 
-        bool shouldActivate = !coreUserExists(_to).exists;
-
         if (amounts.evm != 0) {
             uint64 originalAmount = amounts.core;
             uint64 coreAmount = _getFinalCoreAmount(_to, originalAmount);
 
-            /// @dev When user is activated originalAmount = coreAmount, so no fee is collected.
-            if (shouldActivate) {
+            /// @dev When user is not activated, originalAmount > coreAmount, so fee is collected.
+            if (originalAmount > coreAmount) {
                 /// @dev If fee is withdrawn on a block revert all activations
                 /// @dev Transactions executed before the fee withdrawal tx will be activated
                 if (block.number == feeWithdrawalBlockNumber) revert CannotActivateOnFeeWithdrawalBlock();
@@ -146,7 +144,8 @@ abstract contract PreFundedFeeAbstraction is FeeToken, RecoverableComposer, IPre
             IERC20(ERC20).safeTransfer(ERC20_ASSET_BRIDGE, amounts.evm);
             _submitCoreWriterTransfer(_to, ERC20_CORE_INDEX_ID, coreAmount);
         } else {
-            if (msg.value > 0 && shouldActivate) revert HYPEActivationNotAllowed();
+            ///@dev revert when the user is trying to activate with native tokens
+            if (msg.value > 0 && !coreUserExists(_to).exists) revert HYPEActivationNotAllowed();
         }
     }
 
