057f68df4ee80069bad54154ceed0d43f7b44904
Shankar Subramanian <shankar@layerzerolabs.org>
2025-11-06 10:08:24 -0800

    feat/native-sync-vault-composer (#1747)
    Signed-off-by: shankar <shankar@layerzerolabs.org>
    Co-authored-by: Blockchain Warlock <39101443+TRileySchwarz@users.noreply.github.com>
    Co-authored-by: Tino Martínez Molina <tino.molina@layerzerolabs.org>
    Co-authored-by: Tino Martínez Molina <tino@martinezmolina.es>
    Co-authored-by: arekLZ <arek@layerzerolabs.org>

--- packages/ovault-evm/test/vault-sync/VaultComposerSync_Unit.t.sol
+++ packages/ovault-evm/test/vault-sync/VaultComposerSync_Unit.t.sol
@@ -4,34 +4,38 @@ pragma solidity ^0.8.20;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
-import { IOFT } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
-import { IOAppCore } from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppCore.sol";
 import { OptionsBuilder } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
 import { SendParam } from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
 import { Errors } from "@layerzerolabs/lz-evm-protocol-v2/contracts/libs/Errors.sol";
 
 import { IVaultComposerSync } from "../../contracts/interfaces/IVaultComposerSync.sol";
-import { VaultComposerSync } from "../../contracts/VaultComposerSync.sol";
 import { VaultComposerSyncBaseTest } from "./VaultComposerSync_Base.t.sol";
-
-import { console } from "forge-std/console.sol";
 
 contract VaultComposerSyncUnitTest is VaultComposerSyncBaseTest {
     using OptionsBuilder for bytes;
+
+    function _feedAssets(address _addr, uint256 _amount) internal virtual {
+        assetToken_arb.mint(_addr, _amount);
+    }
+
+    function _getUndustedAssetAmount(uint256 _amount) internal pure virtual returns (uint256) {
+        (uint256 sentUndusted, ) = _removeDust(_amount);
+        return sentUndusted;
+    }
 
     function setUp() public virtual override {
         super.setUp();
     }
 
     function test_deployment() public view {
-        assertEq(address(VaultComposerSyncArb.VAULT()), address(vault_arb));
-        assertEq(VaultComposerSyncArb.SHARE_OFT(), address(shareOFT_arb));
-        assertEq(VaultComposerSyncArb.ASSET_OFT(), address(assetOFT_arb));
+        assertEq(address(vaultComposer.VAULT()), address(vault_arb));
+        assertEq(vaultComposer.SHARE_OFT(), address(shareOFT_arb));
+        assertEq(vaultComposer.ASSET_OFT(), address(assetOFT_arb));
     }
 
-    function test_onlyEndpoint() public {
+    function test_onlyEndpoint() public virtual {
         vm.expectRevert(abi.encodeWithSelector(IVaultComposerSync.OnlyEndpoint.selector, address(this)));
-        VaultComposerSyncArb.lzCompose(address(assetOFT_arb), _randomGUID(), "", userA, "");
+        vaultComposer.lzCompose(address(assetOFT_arb), _randomGUID(), "", userA, "");
     }
 
     function test_onlyOFT(address _oft) public {
@@ -39,50 +43,84 @@ contract VaultComposerSyncUnitTest is VaultComposerSyncBaseTest {
 
         vm.expectRevert(abi.encodeWithSelector(IVaultComposerSync.OnlyValidComposeCaller.selector, _oft));
         vm.prank(arbEndpoint);
-        VaultComposerSyncArb.lzCompose{ value: 1 ether }(_oft, _randomGUID(), "", arbExecutor, "");
+        vaultComposer.lzCompose{ value: 1 ether }(_oft, _randomGUID(), "", arbExecutor, "");
     }
 
-    function test_lzCompose_pass_dst_not_hub() public {
+    function test_lzCompose_pass_dst_not_hub_deposit() public virtual {
         bytes32 guid = _randomGUID();
-        assetOFT_arb.mint(address(VaultComposerSyncArb), TOKENS_TO_SEND);
+        _feedAssets(address(vaultComposer), TOKENS_TO_SEND);
 
         SendParam memory internalSendParam = SendParam(POL_EID, addressToBytes32(userA), TOKENS_TO_SEND, 0, "", "", "");
 
         bytes memory composeMsg = _createComposePayload(ETH_EID, internalSendParam, 1 ether, TOKENS_TO_SEND, userA);
 
-        vm.expectEmit(true, true, true, true, address(assetOFT_arb));
-        emit IERC20.Transfer(address(VaultComposerSyncArb), address(vault_arb), TOKENS_TO_SEND);
+        vm.expectEmit(true, true, true, true, address(assetToken_arb));
+        emit IERC20.Transfer(address(vaultComposer), address(vault_arb), TOKENS_TO_SEND);
 
         vm.expectEmit(true, true, true, true, address(vault_arb));
-        emit IERC20.Transfer(address(0), address(VaultComposerSyncArb), TOKENS_TO_SEND);
+        emit IERC20.Transfer(address(0), address(vaultComposer), TOKENS_TO_SEND);
 
         vm.expectEmit(true, true, true, true, address(vault_arb));
-        emit IERC4626.Deposit(
-            address(VaultComposerSyncArb),
-            address(VaultComposerSyncArb),
-            TOKENS_TO_SEND,
-            TOKENS_TO_SEND
-        );
+        emit IERC4626.Deposit(address(vaultComposer), address(vaultComposer), TOKENS_TO_SEND, TOKENS_TO_SEND);
 
-        vm.expectEmit(true, true, true, true, address(VaultComposerSyncArb));
+        vm.expectEmit(true, true, true, true, address(vaultComposer));
         emit IVaultComposerSync.Sent(guid);
 
-        assertEq(assetOFT_arb.totalSupply(), assetOFT_arb.balanceOf(address(VaultComposerSyncArb)), TOKENS_TO_SEND);
+        assertEq(assetToken_arb.totalSupply(), assetToken_arb.balanceOf(address(vaultComposer)), TOKENS_TO_SEND);
         assertEq(vault_arb.totalSupply(), 0);
 
         vm.prank(arbEndpoint);
-        VaultComposerSyncArb.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
+        vaultComposer.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
 
-        assertEq(assetOFT_arb.totalSupply(), assetOFT_arb.balanceOf(address(vault_arb)), TOKENS_TO_SEND);
+        assertEq(assetToken_arb.totalSupply(), assetToken_arb.balanceOf(address(vault_arb)), TOKENS_TO_SEND);
         assertEq(vault_arb.totalSupply(), vault_arb.balanceOf(address(shareOFT_arb)), TOKENS_TO_SEND);
     }
 
-    function test_lzCompose_pass_dst_is_hub() public {
+    function test_lzCompose_pass_dst_not_hub_withdraw() public virtual {
         bytes32 guid = _randomGUID();
-        assetOFT_arb.mint(address(VaultComposerSyncArb), TOKENS_TO_SEND);
+        vault_arb.mint(address(vaultComposer), TOKENS_TO_SEND);
+
+        _setTradeRatioAssetToShare(TOKENS_TO_SEND, TOKENS_TO_SEND);
+
+        SendParam memory internalSendParam = SendParam(POL_EID, addressToBytes32(userA), TOKENS_TO_SEND, 0, "", "", "");
+
+        bytes memory composeMsg = _createComposePayload(ETH_EID, internalSendParam, 1 ether, TOKENS_TO_SEND, userA);
+
+        vm.expectEmit(true, true, true, true, address(vault_arb));
+        emit IERC20.Transfer(address(vaultComposer), address(0), TOKENS_TO_SEND);
+
+        vm.expectEmit(true, true, true, true, address(assetToken_arb));
+        emit IERC20.Transfer(address(vault_arb), address(vaultComposer), TOKENS_TO_SEND - 1);
+
+        vm.expectEmit(true, true, true, true, address(vault_arb));
+        emit IERC4626.Withdraw(
+            address(vaultComposer),
+            address(vaultComposer),
+            address(vaultComposer),
+            TOKENS_TO_SEND - 1, /// @dev Due to ERC4626 rounding.
+            TOKENS_TO_SEND
+        );
+
+        vm.expectEmit(true, true, true, true, address(assetToken_arb));
+        emit IERC20.Transfer(address(vaultComposer), address(0), _getUndustedAssetAmount(TOKENS_TO_SEND - 1));
+
+        vm.expectEmit(true, true, true, true, address(vaultComposer));
+        emit IVaultComposerSync.Sent(guid);
+
+        assertEq(vault_arb.balanceOf(address(vaultComposer)), TOKENS_TO_SEND);
+
+        vm.prank(arbEndpoint);
+        vaultComposer.lzCompose{ value: 1 ether }(address(shareOFT_arb), guid, composeMsg, arbExecutor, "");
+
+        assertEq(vault_arb.balanceOf(address(vaultComposer)), 0);
+    }
+
+    function test_lzCompose_pass_dst_is_hub() public virtual {
+        bytes32 guid = _randomGUID();
+        _feedAssets(address(vaultComposer), TOKENS_TO_SEND);
 
         SendParam memory internalSendParam = SendParam(
-            VaultComposerSyncArb.VAULT_EID(),
+            vaultComposer.VAULT_EID(),
             addressToBytes32(userA),
             TOKENS_TO_SEND,
             0,
@@ -93,80 +131,41 @@ contract VaultComposerSyncUnitTest is VaultComposerSyncBaseTest {
 
         bytes memory composeMsg = _createComposePayload(ETH_EID, internalSendParam, 0, TOKENS_TO_SEND, userA);
 
-        vm.expectEmit(true, true, true, true, address(assetOFT_arb));
-        emit IERC20.Transfer(address(VaultComposerSyncArb), address(vault_arb), TOKENS_TO_SEND);
+        vm.expectEmit(true, true, true, true, address(assetToken_arb));
+        emit IERC20.Transfer(address(vaultComposer), address(vault_arb), TOKENS_TO_SEND);
 
         vm.expectEmit(true, true, true, true, address(vault_arb));
-        emit IERC20.Transfer(address(0), address(VaultComposerSyncArb), TOKENS_TO_SEND);
+        emit IERC20.Transfer(address(0), address(vaultComposer), TOKENS_TO_SEND);
 
         vm.expectEmit(true, true, true, true, address(vault_arb));
-        emit IERC4626.Deposit(
-            address(VaultComposerSyncArb),
-            address(VaultComposerSyncArb),
-            TOKENS_TO_SEND,
-            TOKENS_TO_SEND
-        );
+        emit IERC4626.Deposit(address(vaultComposer), address(vaultComposer), TOKENS_TO_SEND, TOKENS_TO_SEND);
 
-        vm.expectEmit(true, true, true, true, address(VaultComposerSyncArb));
+        vm.expectEmit(true, true, true, true, address(vaultComposer));
         emit IVaultComposerSync.Sent(guid);
 
-        assertEq(assetOFT_arb.totalSupply(), assetOFT_arb.balanceOf(address(VaultComposerSyncArb)), TOKENS_TO_SEND);
+        assertEq(assetToken_arb.totalSupply(), assetToken_arb.balanceOf(address(vaultComposer)), TOKENS_TO_SEND);
         assertEq(vault_arb.totalSupply(), 0);
 
         vm.prank(arbEndpoint);
-        VaultComposerSyncArb.lzCompose(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
+        vaultComposer.lzCompose(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
 
-        assertEq(assetOFT_arb.totalSupply(), assetOFT_arb.balanceOf(address(vault_arb)), TOKENS_TO_SEND);
+        assertEq(assetToken_arb.totalSupply(), assetToken_arb.balanceOf(address(vault_arb)), TOKENS_TO_SEND);
         assertEq(vault_arb.totalSupply(), vault_arb.balanceOf(address(userA)), TOKENS_TO_SEND);
-    }
-
-    function test_lzCompose_pass_dst_is_hub_no_msgValue_causes_refund() public {
-        bytes32 guid = _randomGUID();
-        assetOFT_arb.mint(address(VaultComposerSyncArb), TOKENS_TO_SEND);
-        uint256 userABalanceEth = assetOFT_eth.balanceOf(userA);
-
-        SendParam memory internalSendParam = SendParam(
-            VaultComposerSyncArb.VAULT_EID(),
-            addressToBytes32(userA),
-            TOKENS_TO_SEND,
-            0,
-            "",
-            "",
-            ""
-        );
-
-        bytes memory composeMsg = _createComposePayload(ETH_EID, internalSendParam, 1 wei, TOKENS_TO_SEND, userA);
-
-        /// @dev Internal revert on try...catch
-        /// vm.expectRevert(abi.encodeWithSelector(IVaultComposerSync.NoMsgValueExpected.selector));
-
-        vm.expectEmit(true, true, true, true, address(VaultComposerSyncArb));
-        emit IVaultComposerSync.Refunded(guid);
-
-        vm.prank(arbEndpoint);
-        VaultComposerSyncArb.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
-
-        verifyPackets(ETH_EID, address(assetOFT_eth));
-        assertEq(
-            assetOFT_eth.balanceOf(userA),
-            userABalanceEth + TOKENS_TO_SEND,
-            "userA should have received refund on Ethereum"
-        );
     }
 
     function test_lzCompose_fail_invalid_payload_auto_refunds() public {
         bytes32 guid = _randomGUID();
-        assetOFT_arb.mint(address(VaultComposerSyncArb), TOKENS_TO_SEND);
+        _feedAssets(address(vaultComposer), TOKENS_TO_SEND);
         uint256 userABalanceEth = assetOFT_eth.balanceOf(userA);
 
         bytes memory invalidComposeMsg = abi.encode(bytes("0x1234"), 5);
         bytes memory composeMsg = _createComposePayload(ETH_EID, invalidComposeMsg, TOKENS_TO_SEND, userA);
 
-        vm.expectEmit(true, true, true, true, address(VaultComposerSyncArb));
+        vm.expectEmit(true, true, true, true, address(vaultComposer));
         emit IVaultComposerSync.Refunded(guid);
 
         vm.prank(arbEndpoint);
-        VaultComposerSyncArb.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
+        vaultComposer.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
 
         verifyPackets(ETH_EID, address(assetOFT_eth));
         assertEq(
@@ -176,9 +175,9 @@ contract VaultComposerSyncUnitTest is VaultComposerSyncBaseTest {
         );
     }
 
-    function test_lzCompose_slippage_causes_refund() public {
+    function test_lzCompose_slippage_causes_refund() public virtual {
         bytes32 guid = _randomGUID();
-        assetOFT_arb.mint(address(VaultComposerSyncArb), TOKENS_TO_SEND);
+        _feedAssets(address(vaultComposer), TOKENS_TO_SEND);
         uint256 userABalanceEth = assetOFT_eth.balanceOf(userA);
 
         SendParam memory internalSendParam = SendParam(
@@ -194,14 +193,14 @@ contract VaultComposerSyncUnitTest is VaultComposerSyncBaseTest {
         bytes memory composePayload = abi.encode(internalSendParam);
         bytes memory composeMsg = _createComposePayload(ETH_EID, composePayload, TOKENS_TO_SEND, userA);
 
-        vm.expectEmit(true, true, true, true, address(VaultComposerSyncArb));
+        vm.expectEmit(true, true, true, true, address(vaultComposer));
         emit IVaultComposerSync.Refunded(guid);
 
-        assertEq(assetOFT_arb.totalSupply(), assetOFT_arb.balanceOf(address(VaultComposerSyncArb)), TOKENS_TO_SEND);
+        assertEq(assetToken_arb.totalSupply(), assetToken_arb.balanceOf(address(vaultComposer)), TOKENS_TO_SEND);
         assertEq(vault_arb.totalSupply(), 0);
 
         vm.prank(arbEndpoint);
-        VaultComposerSyncArb.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
+        vaultComposer.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
 
         // The operation should be refunded since slippage was too high
         verifyPackets(ETH_EID, address(assetOFT_eth));
@@ -215,7 +214,7 @@ contract VaultComposerSyncUnitTest is VaultComposerSyncBaseTest {
 
     function test_InsufficientMsgValue_causes_revert_no_refund() public {
         bytes32 guid = _randomGUID();
-        assetOFT_arb.mint(address(VaultComposerSyncArb), TOKENS_TO_SEND);
+        _feedAssets(address(vaultComposer), TOKENS_TO_SEND);
 
         SendParam memory internalSendParam = SendParam(POL_EID, addressToBytes32(userB), TOKENS_TO_SEND, 0, "", "", "");
         bytes memory composeMsg = _createComposePayload(ETH_EID, internalSendParam, 10 ether, msgValueToFail, userA);
@@ -223,18 +222,18 @@ contract VaultComposerSyncUnitTest is VaultComposerSyncBaseTest {
         vm.expectRevert(abi.encodeWithSelector(IVaultComposerSync.InsufficientMsgValue.selector, 10 ether, 1 ether));
 
         vm.prank(arbEndpoint);
-        VaultComposerSyncArb.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
+        vaultComposer.lzCompose{ value: 1 ether }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
     }
 
     function test_lzCompose_fail_not_enough_value_for_refund_complete_revert() public {
         bytes32 guid = _randomGUID();
-        assetOFT_arb.mint(address(VaultComposerSyncArb), TOKENS_TO_SEND);
+        _feedAssets(address(vaultComposer), TOKENS_TO_SEND);
 
         SendParam memory internalSendParam = SendParam(POL_EID, addressToBytes32(userA), TOKENS_TO_SEND, 0, "", "", "");
         bytes memory composeMsg = _createComposePayload(ETH_EID, internalSendParam, 0, TOKENS_TO_SEND, userA);
 
         vm.expectRevert(abi.encodeWithSelector(Errors.LZ_InsufficientFee.selector, 200110716, 1, 0, 0)); /// @dev 200110716 is quoteSend value
         vm.prank(arbEndpoint);
-        VaultComposerSyncArb.lzCompose{ value: 1 wei }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
+        vaultComposer.lzCompose{ value: 1 wei }(address(assetOFT_arb), guid, composeMsg, arbExecutor, "");
     }
 }
