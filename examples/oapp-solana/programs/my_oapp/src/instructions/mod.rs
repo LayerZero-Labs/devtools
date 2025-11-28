323a019780f09389b2d9d6456531da17afac6d19
nazreen <10964594+nazreen@users.noreply.github.com>
2025-11-28 01:06:58 -0300

    delete

--- examples/oapp-solana/programs/my_oapp/src/instructions/mod.rs
+++ examples/oapp-solana/programs/my_oapp/src/instructions/mod.rs
@@ -1,7 +1,6 @@
 pub mod send;
 pub mod init_store;
 pub mod lz_receive;
-pub mod lz_receive_types;
 pub mod lz_receive_types_info;
 pub mod lz_receive_types_v2;
 pub mod quote_send;
@@ -11,7 +10,6 @@ pub mod set_peer_config;
 pub use send::*;
 pub use init_store::*;
 pub use lz_receive::*;
-pub use lz_receive_types::*;
 pub use lz_receive_types_info::*;
 pub use lz_receive_types_v2::*;
 pub use quote_send::*;
