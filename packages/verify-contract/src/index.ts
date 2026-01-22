cd397e25716a5b691043d7366b8c9d3ef2560e35
nazreen <10964594+nazreen@users.noreply.github.com>
2026-01-21 17:36:41 -0800

    export getDefaultChainID

--- packages/verify-contract/src/index.ts
+++ packages/verify-contract/src/index.ts
@@ -2,3 +2,5 @@ export {
     verifyTarget as verifyHardhatDeployTarget,
     verifyNonTarget as verifyHardhatDeployNonTarget,
 } from './hardhat-deploy/verify'
+
+export { getDefaultChainId } from './common/networks-loader'
